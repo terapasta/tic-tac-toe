8712110beadac42d1526c8912aa1b71311cbd8d0
a.harada <redhornet96@gmail.com>
2016-05-28 17:19:25 +0900

    チャットUI実装

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -5,10 +5,38 @@ class TopsController < ApplicationController
   end
 
   def create
+    # HACK リファクタリング
     @answer_ids = params[:answer_ids]
     @texts = params[:texts] || []
+
+    answer_ids = @answer_ids
+    while answer_ids.count < 2
+      answer_ids.unshift(0)
+      @texts.unshift('')
+    end
     @texts << params[:text]
-    # TODO ここで予測を実行して、answer_idsに追加する
+
+    #binding.pry
+    text = Shellwords.escape(params[:text])
+    Rails.logger.debug(text)
+
+    cmd = "cd learning; python main_predict.py #{answer_ids[-2..-1].join(' ')} #{params[:text]}"
+    Rails.logger.debug(cmd)
+    output = `#{cmd}`
+    Rails.logger.debug("output: #{output}")
+
+    answer_id = output.split("\n").last
+    @answer_ids << answer_id
+
+
+    # if !$?.success?
+    #   @result = "診断に失敗しました"
+    # elsif is_getwild == 0
+    #   @result = "「#{training_set.text}」はGetWildではありません。"
+    # elsif is_getwild == 1
+    #   @result = "「#{training_set.text}」はGetWildです。"
+    # end
+
     render :show
   end
 end

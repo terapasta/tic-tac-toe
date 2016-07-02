77d13da8fc663ab821e73aef3f61eb7960ebf0d9
a.harada <redhornet96@gmail.com>
2016-07-02 19:25:46 +0900

    最初のデータをfixtureにしておく

--- db/fixtures/trainings.rb
+++ db/fixtures/trainings.rb
@@ -1,4 +1,24 @@
+# DO NOT MODIFY THIS FILE, it was auto-generated.
+#
+# Date: 2016/07/02 19:25:19
+# Seeding Training
+# Written with the command:
+#
+#   bin/rails 
+#
 Training.seed(:id,
-  { id: 1 },
-  { id: 2 },
+  {"id"=>1},
+  {"id"=>2},
+  {"id"=>11},
+  {"id"=>12},
+  {"id"=>13},
+  {"id"=>14},
+  {"id"=>15},
+  {"id"=>16},
+  {"id"=>17},
+  {"id"=>18},
+  {"id"=>19},
+  {"id"=>20},
+  {"id"=>21}
 )
+# End auto-generated file.

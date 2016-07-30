1e5dc10c0f2ef7b269cbb7bfd80bb507feff5a6d
a.harada <redhornet96@gmail.com>
2016-07-30 15:23:22 +0900

    コメント追記

--- app/controllers/chats/messages_controller.rb
+++ app/controllers/chats/messages_controller.rb
@@ -1,6 +1,7 @@
 class Chats::MessagesController < ApplicationController
   before_action :set_chat
 
+  # TODO apiの方の実装に統合したい
   def create
     @message_guest = @chat.messages.build(message_params)
     @message_guest.speaker = 'guest'

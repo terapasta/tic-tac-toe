5315e272a66a63d867ec7b9d2f00c8a3f291f588
a.harada <redhornet96@gmail.com>
2016-08-15 18:03:59 +0900

    ヘルプデスクチャットは認証なしで利用できる

--- app/controllers/helpdesks_controller.rb
+++ app/controllers/helpdesks_controller.rb
@@ -1,20 +1,8 @@
 class HelpdesksController < ApplicationController
-  before_action :authenticate_user!
-  # before_action :set_chat, only: [:show, :destroy]
   before_action :set_bot, only: [:new, :create, :destroy]
-  # before_action :set_guest_key
   before_action :check_have_start_message, only: :new
-  #
-  # def show
-  #   session[:embed] = params[:embed] if params[:embed]
-  #   redirect_to new_chats_path if @chat.nil?
-  # end
-  #
+
   def new
-    # @chat = @bot.chats.new(guest_key: session[:guest_key])
-    # @chat.messages << @chat.build_start_message
-    # @chat.save!
-    # render :show
   end
 
   def create
@@ -30,26 +18,12 @@ class HelpdesksController < ApplicationController
     end
   end
 
-  #
-  # def destroy
-  #   flash[:notice] = 'クリアしました'
-  #   redirect_to new_bot_chats_path(@bot)
-  # end
-  #
   private
     def set_bot
       # HACK URL内のIDを変更すれば、他社のbotも表示出来てしまうため、いずれはトークンなどによるbot識別をする必要がある
       @bot = Bot.find(params[:bot_id])
     end
-  #
-  #   def set_chat
-  #     @chat = Chat.where(guest_key: session[:guest_key]).last
-  #   end
-  #
-  #   def set_guest_key
-  #     session[:guest_key] ||= SecureRandom.hex(64)
-  #   end
-  #
+
     # TODO DRYにしたい
     def check_have_start_message
       if @bot.start_answer.blank?
@@ -57,8 +31,4 @@ class HelpdesksController < ApplicationController
         redirect_to :back
       end
     end
-  #
-  #   def message_params
-  #     params.require(:message).permit(:body)
-  #   end
 end

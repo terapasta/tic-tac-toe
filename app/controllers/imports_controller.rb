abad8e64f72effca51f1530913ff049239aba975
a.harada <redhornet96@gmail.com>
2016-09-25 16:46:36 +0900

    データインポート処理を実装

---
+++ app/controllers/imports_controller.rb
@@ -0,0 +1,22 @@
+class ImportsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_bot
+
+  def new
+  end
+
+  def create
+    DataImporter.import(params[:file], @bot)
+    flash[:notice] = '登録しました'
+    render :new
+  end
+
+  private
+    def set_bot
+      @bot = current_user.bots.find(params[:bot_id])
+    end
+
+    # def bot_params
+    #   params.require(:bot).permit(:name, :image)
+    # end
+end

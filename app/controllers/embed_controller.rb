8763b5b0b25fab433bdbc333458f332b4b60876d
a.harada <redhornet96@gmail.com>
2016-10-07 07:05:21 +0900

    embed_controllerは不要なので削除

--- app/controllers/embed_controller.rb
+++
@@ -1,8 +0,0 @@
-class EmbedController < ApplicationController
-  def show
-    @bot = Bot.find_by(token: params[:token])
-    # @chat = @bot.chats.new(guest_key: session[:guest_key])
-    # @chat.messages << @chat.build_start_message
-    # @chat.save!
-  end
-end

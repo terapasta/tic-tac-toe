bd4cc0828cb3730829b16a1609fef721a1d0acd4
a.harada <redhornet96@gmail.com>
2016-10-09 17:17:48 +0900

    CSVエクスポートの実装途中

--- app/models/training_message.rb
+++ app/models/training_message.rb
@@ -10,21 +10,31 @@ class TrainingMessage < ActiveRecord::Base
     training
   end
 
+  # TODO リファクタリング(訓練セット用のテーブルなどを用意したほうがいいかも)
   def self.to_csv(bot)
     CSV.generate do |csv|
+      arr = []
       bot.trainings.each do |training|
         guest_body, bot_body = ''
         training.training_messages.each do |training_message|
-          # binding.pry
-          # binding.pry
           if training_message.guest?
             guest_body = training_message.body
           elsif training_message.bot?
             bot_body = training_message.body
-            csv << [guest_body, bot_body] if guest_body.present?
+            if guest_body.present?
+              flag = arr.each_with_index do |row, idx|
+                if row[0] == guest_body
+                  row = [guest_body, bot_body]
+                  arr[idx] = row
+                  break false
+                end
+              end
+              arr << [guest_body, bot_body] if flag
+            end
           end
         end
       end
+      arr.each {|row| csv << row }
     end.encode(Encoding::SJIS)
   end
 end

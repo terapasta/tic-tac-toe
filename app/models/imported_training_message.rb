6dee60394d5af89e47bb2b6228e37dc930aa3abf
a.harada <redhornet96@gmail.com>
2016-10-11 16:24:09 +0900

    分岐含めたインポートが出来る

--- app/models/imported_training_message.rb
+++ app/models/imported_training_message.rb
@@ -2,11 +2,13 @@ class ImportedTrainingMessage < ActiveRecord::Base
   belongs_to :answer
 
   def self.import_csv(file, bot)
+    bot.imported_training_messages.destroy_all
+
     imported_training_messages = []
     CSV.foreach(file.path, encoding: 'Shift_JIS:UTF-8', skip_blanks: true) do |row|
       answer = bot.answers.find_or_initialize_by(body: row[1]) {|answer| answer.bot = bot }
-      imported_training_message = bot.imported_training_messages.find_or_initialize_by(question: row[0])
-      imported_training_message.answer = answer
+      imported_training_message = bot.imported_training_messages.build(question: row[0], answer: answer)
+      imported_training_message.underlayer = row[2..-1].compact if row.compact.count > 2
       imported_training_message.save!
     end
     true

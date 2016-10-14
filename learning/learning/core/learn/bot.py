7d331f90d74dd9cd851027f613b7aabffaaeed5a
a.harada <redhornet96@gmail.com>
2016-10-14 19:16:43 +0900

    logger共通化の試行錯誤

--- learning/learning/core/learn/bot.py
+++ learning/learning/core/learn/bot.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -
-import logging
 import dataset
 import numpy as np
 import pandas as pd
 import MySQLdb
+from ... import log
 from sklearn.svm import SVC
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import KFold
@@ -11,15 +11,15 @@ from sklearn.externals import joblib
 from sklearn import linear_model
 from ..training_set.training_message import TrainingMessage
 from ..plotter import Plotter
-from config.config import Config
+from ...config.config import Config
 
 class Bot:
     def __init__(self, bot_id):
         self.bot_id = bot_id
 
     def learn(self):
-        logging.basicConfig(filename="example.log",level=logging.DEBUG)
-        logging.debug('Bot.learn start')
+        # logging.basicConfig(filename="example.log",level=logging.DEBUG)
+        logger.debug('Bot.learn start')
 
         dbconfig = Config().get('database')
         db = dataset.connect(dbconfig['endpoint'])
@@ -29,13 +29,13 @@ class Bot:
 
         print(training_set.y)
 
-        logging.debug('Bot.learn fit start')
+        logger.debug('Bot.learn fit start')
         estimator = linear_model.LogisticRegression(C=1e5)
         estimator.fit(training_set.x, training_set.y)
 
         # print "estimator.score: %s " % estimator.score  # accuracy
 
-        logging.debug('Bot.learn dump start')
+        logger.debug('Bot.learn dump start')
         joblib.dump(estimator, "learning/models/%s_logistic_reg_model" % self.bot_id)
         test_scores_mean = Plotter().plot(estimator, training_set.x, training_set.y)
         logging.debug('Bot.learn end')

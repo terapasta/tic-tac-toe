dc07938dfc3bb6cc16ce2f7f1bcf9c2cb608bfb0
a.harada <redhornet96@gmail.com>
2016-10-20 16:06:16 +0900

    bot.start_answerなしで正しく動くようにする

--- db/fixtures/bots.rb
+++ db/fixtures/bots.rb
@@ -1,5 +1,5 @@
 Bot.seed(:id,
-  { id: 1, user_id: 1, name: 'どんうさぎbot(社内slack用)', start_answer_id: 1, token: SecureRandom.hex(32) },
+  { id: 1, user_id: 1, name: 'どんうさぎbot(社内slack用)', token: SecureRandom.hex(32) },
   { id: 2, user_id: 1, name: 'ハナコ（ヘルプデスクbot）', token: SecureRandom.hex(32) },
   { id: 3, user_id: 1, name: 'ヘヤ子さん', token: SecureRandom.hex(32) },
 )

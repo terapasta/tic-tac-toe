aba05e4731e173fe9d935d9f5f44b1efca2367f8
a.harada <redhornet96@gmail.com>
2016-10-31 14:40:01 +0900

    回答の出方を調整

--- spec/features/conversations_spec.rb
+++ spec/features/conversations_spec.rb
@@ -21,4 +21,21 @@ feature '意図した通りにBotとの対話が出来る' do
       expect(conversation_bot.reply[0].body).to be_include('地区予選の参加可能な地区数について、参加区分「コンペティション」としての')
     end
   end
+
+  context '「ピティナってなに」とポストされた場合' do
+    let(:message) { chat.messages.build(speaker: 'guest', body: 'ピティナってなに') }
+    scenario do
+      conversation_bot = Conversation::Bot.new(bot, message)
+      expect(conversation_bot.reply[0].body).to be_include('ピティナ（一般社団法人全日本ピアノ指導者協会）は、')
+      # expect(conversation_bot.other_answers.map(&:headline)).to be_include('ピティナ申込にあたる料金について')
+    end
+  end
+
+  context '「ピティナとは」とポストされた場合' do
+    let(:message) { chat.messages.build(speaker: 'guest', body: 'ピティナとは') }
+    scenario do
+      conversation_bot = Conversation::Bot.new(bot, message)
+      expect(conversation_bot.reply[0].body).to be_include('ピティナ（一般社団法人全日本ピアノ指導者協会）は、')
+    end
+  end
 end

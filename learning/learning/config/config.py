6d691791ee07f0bf88c8009f14f37fa3e2cb2a05
a.harada <redhornet96@gmail.com>
2016-11-05 15:05:06 +0900

    Revert "Revert "Revert "Revert "python側で環境ごとにことなるDBにアクセス出来るようにする""""
    This reverts commit fe5ff9c4d14584e4c333721bd854ece45d628d3a.

--- learning/learning/config/config.py
+++ learning/learning/config/config.py
@@ -1,10 +1,19 @@
 import yaml
+import sys
 
 class Config:
 
     def __init__(self):
         with open("learning/config/config.yml", 'r') as ymlfile:
             self.config = yaml.load(ymlfile)
+        self.__set_env()
 
     def get(self, key):
-        return self.config[key]
+        print(self.config[self.env])
+        return self.config[self.env][key]
+
+    def __set_env(self):
+        args = sys.argv
+        self.env = 'development'
+        if len(args) > 1:
+            self.env = args[1]

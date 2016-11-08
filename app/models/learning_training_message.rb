4bd0f0aa5a5ad3ae34ecff65f62aa77f750965a9
a.harada <redhornet96@gmail.com>
2016-11-08 17:45:30 +0900

    LearningTrainingMessageのクラスメソッドをまとめる

--- app/models/learning_training_message.rb
+++ app/models/learning_training_message.rb
@@ -5,43 +5,44 @@ class LearningTrainingMessage < ActiveRecord::Base
   validates :answer_body, length: { maximum: 10000 }
 
   # TODO クラス・メソッドをselfではなく囲う形にする
+  class << self
+    def to_csv(bot)
+      CSV.generate do |csv|
+        bot.learning_training_messages.find_each do |learning_training_message|
+          base = [
+            learning_training_message.question,
+          ]
+          recursive_put(csv, base, learning_training_message.answer)
+        end
+      end.encode(Encoding::SJIS, replace: '') # FIXME 〜などが変換時に情報落ちしてしまう
+    end
 
-  def self.to_csv(bot)
-    CSV.generate do |csv|
-      bot.learning_training_messages.find_each do |learning_training_message|
-        base = [
-          learning_training_message.question,
-        ]
-        recursive_put(csv, base, learning_training_message.answer)
-      end
-    end.encode(Encoding::SJIS, replace: '') # FIXME 〜などが変換時に情報落ちしてしまう
-  end
-
-  def self.recursive_put(csv, base, answer)
-    row = base.dup
-    row2 = nil
-    row << answer.body
-    if answer.decision_branches.present?
-       answer.decision_branches.each do |decision_branch|
-         row2 = row.dup
-         row2 << decision_branch.body
-         if decision_branch.next_answer.present?
-           recursive_put(csv, row2, decision_branch.next_answer)
+    def recursive_put(csv, base, answer)
+      row = base.dup
+      row2 = nil
+      row << answer.body
+      if answer.decision_branches.present?
+         answer.decision_branches.each do |decision_branch|
+           row2 = row.dup
+           row2 << decision_branch.body
+           if decision_branch.next_answer.present?
+             recursive_put(csv, row2, decision_branch.next_answer)
+           end
          end
-       end
-    end
-    csv << (row2 || row)
-  end
-
-  def self.amp!(bot)
-    arr = []
-    bot.learning_training_messages.each do |learning_training_message|
-      WordMapping.variations_of(learning_training_message.question).each do |sentence|
-        copy_model = learning_training_message.dup
-        copy_model.question = sentence
-        arr << copy_model
       end
+      csv << (row2 || row)
     end
-    LearningTrainingMessage.import!(arr)
+
+    def amp!(bot)
+      arr = []
+      bot.learning_training_messages.each do |learning_training_message|
+        WordMapping.variations_of(learning_training_message.question).each do |sentence|
+          copy_model = learning_training_message.dup
+          copy_model.question = sentence
+          arr << copy_model
+        end
+      end
+      LearningTrainingMessage.import!(arr)
+    end
   end
 end

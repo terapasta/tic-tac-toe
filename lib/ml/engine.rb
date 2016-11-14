c0404a5ee3d06d6305a30b47ecedb5badb395917
a.harada <redhornet96@gmail.com>
2016-11-14 15:21:48 +0900

    engineのtimeoutを伸ばす

--- lib/ml/engine.rb
+++ lib/ml/engine.rb
@@ -1,7 +1,7 @@
 class Ml::Engine
 
   def initialize(bot)
-    @client = MessagePack::RPC::Client.new('127.0.0.1', 6000)
+    @client = MessagePack::RPC::Client.new('127.0.0.1', 12000)
     @bot = bot
   end
 

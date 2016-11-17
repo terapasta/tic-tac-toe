a176c76a1e4f01b2569cc0723a323053d92b298f
a.harada <redhornet96@gmail.com>
2016-11-17 17:08:42 +0900

    実際にPTNAのbotとチャットして気になるところをテストケース化する

--- spec/features/ptna_conversations_spec.rb
+++ spec/features/ptna_conversations_spec.rb
@@ -24,4 +24,11 @@ feature 'PTNAのデータで意図した通りにBotとの対話が出来る' do
       expect(subject[0].body).to eq '回答出来ませんでした。この回答失敗時のメッセージはBot編集画面から変更できます。'
     end
   end
+
+  pending context '「当日の予定を知りたい」とポストされた場合' do
+    let(:message) { chat.messages.build(speaker: 'guest', body: '当日の予定を知りたい') }
+    scenario do
+      expect(subject[0].body).to be_include '当日の時間割の発表は、全ての地区で公平を保つため一律で参加票の発送、'
+    end
+  end
 end

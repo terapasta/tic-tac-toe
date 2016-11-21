40fc1dcb2d6ca4b98bd23aa3c0076297166e994c
a.harada <redhornet96@gmail.com>
2016-11-21 19:45:00 +0900

    multilabel分類の骨子を実装

--- learning/learning/core/learn/tag.py
+++ learning/learning/core/learn/tag.py
@@ -1,10 +1,11 @@
 import numpy as np
 # import pandas as pd
-# import MySQLdb
+import MySQLdb
 from learning.log import logger
 from learning.core.training_set.text_array import TextArray
+from learning.core.training_set.training_text import TrainingText
 from learning.core.nlang import Nlang
-# from sklearn.svm import SVC
+from learning.config.config import Config
 # from sklearn.grid_search import GridSearchCV
 # from sklearn.cross_validation import KFold
 # from sklearn.externals import joblib
@@ -14,7 +15,6 @@ from learning.core.nlang import Nlang
 # from sklearn.metrics import precision_recall_fscore_support
 # from sklearn import cross_validation
 # from learning.core.evaluator import Evaluator
-# from learning.config.config import Config
 # from learning.core.training_set.training_message import TrainingMessage
 # # from ..plotter import Plotter
 from sklearn.svm import SVC
@@ -24,68 +24,34 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 class Tag:
     def __init__(self):
-        print('Tag()')
-        # self.bot_id = bot_id
-        # self.learning_parameter = learning_parameter
-        # logger.debug("self.learning_parameter: % s" % self.learning_parameter)
+        logger.debug('Tag.__init__()')
 
     def learn(self):
+        # TODO Dryにする
+        config = Config()
+        dbconfig = config.get('database')
+        db = MySQLdb.connect(host=dbconfig['host'], db=dbconfig['name'], user=dbconfig['user'], passwd=dbconfig['password'], charset='utf8')
+
         c = OneVsRestClassifier(SVC(probability=True))
-        # X = [[1,0,0],[0,1,0],[0,0,1],[1,0,1],[1,1,1]]
-        # Y = [[1],[2],[3],[1,2],[1,3]]
+        training_set = TrainingText(db)
+        training_set.build()
 
+        logger.debug("training_set.x: %s" % training_set.x)
+        logger.debug("training_set.y: %s" % training_set.y)
 
-        # logger.debug('Tag.learn start')
-        #
-        # # {
-        # #   1: greeting,
-        # #   2: pc
-        # #   3: buy
-        # #   4: fail
-        # #   5: question
-        # #   6: password
-        # #   7: key
-        # #   8: forget
-        # # }
-        train_x = np.array([
-            'こんにちは',
-            'パソコンが壊れました。どうすればいいですか？',
-            'パソコンを買いたいのですがどこで買おうかな？',
-            'パスワードを忘れてしまいました',
-            'カードキーを自宅においてきてしまいました',
-        ])
-        train_y = np.array([
-            [1],
-            [2,4,5],
-            [2,3,5],
-            [6,8],
-            [7,8]
-        ])
-        #
-        text_array = TextArray(train_x)
-        # c = OneVsRestClassifier(SVC())
-        # binarizer = MultiLabelBinarizer().fit(train_y)
-        # train_y = binarizer.transform(train_y)
-        # logger.debug("train_y: %s" % train_y)
-        feature = text_array.to_vec()
-        # logger.debug(feature.toarray())
-        #
-        # # feature = np.array([
-        # #     [1,1,1,1],
-        # #     [1,2,1,4],
-        # #     [2,2,0,4],
-        # #     [2,1,0,4],
-        # #     [1,2,0,4],
-        # # ])
-        # estimator = c.fit(feature, train_y)
-        #
+        binarizer = MultiLabelBinarizer().fit(training_set.y)
+        binarized_y = binarizer.transform(training_set.y)
+        logger.debug("binarized_y: %s" % binarized_y)
+        estimator = c.fit(training_set.x, binarized_y)
+
         # predict
-        count_vectorizer = CountVectorizer(vocabulary=text_array.vocabulary)
+        count_vectorizer = CountVectorizer(vocabulary=training_set.body_array.vocabulary)
         splited_data = [
             Nlang.split('こんにちは'),
             Nlang.split('パソコンが壊れました。どうすればいいですか？'),
         ]
         feature_vectors = count_vectorizer.fit_transform(splited_data)
+
         # logger.debug("feature_vectors.torray(): %s" % feature_vectors.toarray())
         # result = estimator.predict(feature_vectors)
         # logger.debug("result: %s" % result)
@@ -93,95 +59,9 @@ class Tag:
         # # hoge = MultiLabelBinarizer().inverse_transform(result)
         # # print(hoge)
 
-        binarizer = MultiLabelBinarizer().fit(train_y)
-        yyy = binarizer.transform(train_y)
-        estimator = c.fit(feature, yyy)
 
         result = estimator.predict_proba(feature_vectors)
         logger.debug(result)
 
         result = estimator.predict(feature_vectors)
         logger.debug(binarizer.inverse_transform(result))
-
-#         config = Config()
-#         dbconfig = config.get('database')
-#         db = MySQLdb.connect(host=dbconfig['host'], db=dbconfig['name'], user=dbconfig['user'], passwd=dbconfig['password'], charset='utf8')
-#         training_set = TrainingMessage(db, self.bot_id, self.learning_parameter)
-#         training_set.build()
-#
-#         # SVMのグリッドサーチに時間がかかるので、一旦ロジスティック回帰のみにする
-#         # estimator = self.__get_best_estimator(training_set)
-#         # estimator = self.__logistic_regression(training_set).best_estimator_
-#         # シンプルなロジスティック回帰
-#         estimator = LogisticRegression(C=1e5)
-#         estimator.fit(training_set.x, training_set.y)
-#         # SVMを使用する
-#         # estimator = self.__svm(training_set).best_estimator_
-#         joblib.dump(training_set.body_array.vocabulary, "learning/models/%s/%s_vocabulary.pkl" % (config.env, self.bot_id))  # TODO dumpする処理はこのクラスの責務外なのでリファクタリングしたい
-#         joblib.dump(estimator, "learning/models/%s/%s_logistic_reg_model" % (config.env, self.bot_id))
-#         # test_scores_mean = Plotter().plot(estimator, training_set.x, training_set.y)
-#
-#         evaluator = Evaluator()
-#         evaluator.evaluate(estimator, training_set.x, training_set.y)
-#         # クロスバリデーションではなく既存データに対して評価する
-#         # evaluator.evaluate_using_exist_data(estimator, training_set.x, training_set.y)
-#
-#         # logger.debug('分類に失敗したデータのインデックス(bot.learning_training_messages[index]で参照出来る): %s' % evaluator.indexes_of_failed(estimator, training_set.x, training_set.y))
-#         logger.debug('Bot.learn end')
-#
-#         return evaluator
-#
-#     def __get_best_estimator(self, training_set):
-#         grid_logi = self.__logistic_regression(training_set)
-#         grid_svm = self.__svm(training_set)
-#         if grid_logi.best_score_ > grid_svm.best_score_:
-#             logger.debug('採用するアルゴリズム: ロジスティック回帰')
-#             return grid_logi.best_estimator_
-#         else:
-#             logger.debug('採用するアルゴリズム: SVM')
-#             return grid_svm.best_estimator_
-#
-#
-#     def __logistic_regression(self, training_set):
-#         param_grid = {'C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000] }
-#         grid = GridSearchCV(
-#             LogisticRegression(penalty='l2'),
-#             param_grid,
-#             # verbose=3
-#         )
-#         # grid = GridSearchCV(
-#         #     cv=None,
-#         #     estimator=LogisticRegression(
-#         #         C=1.0, intercept_scaling=1, dual=False, fit_intercept=True,
-#         #         penalty='l2', tol=0.0001),
-#         #     param_grid={'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]},
-#         #     verbose=3
-#         # )
-#         grid.fit(training_set.x, training_set.y)
-#         # estimator = grid.best_estimator_
-#
-#         # 単純なロジスティック回帰モデル
-#         # estimator = LogisticRegression(C=1e5)
-#         # estimator.fit(training_set.x, training_set.y)
-#         return grid
-#
-#     def __svm(self, training_set):
-#         svm_tuned_parameters = [
-#             {
-#                 'kernel': ['rbf', 'linear'],
-#                 # 'gamma': [2**n for n in range(-15, 3)],
-#                 # 'C': [2**n for n in range(-5, 15)]
-#                 'gamma': [2**n for n in range(-5, 3)],  # FIXME 開発中はgridsearchの試行数を減らす
-#                 'C': [2**n for n in range(-5, 8)]
-#             }
-#         ]
-#         gscv = GridSearchCV(
-#             SVC(),
-#             svm_tuned_parameters,
-#             cv=2,
-#             #cv=KFold(n=3),
-#             n_jobs = 1,
-#             # verbose = 3
-#         )
-#         gscv.fit(training_set.x, training_set.y)
-#         return gscv

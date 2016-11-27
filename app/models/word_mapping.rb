37ba0a02b98191d86b8b0f2459ebafa328ebc3c5
blueplanet <erguolinge@gmail.com>
2016-11-27 18:53:19 +0900

    重複バリデーション追加

--- app/models/word_mapping.rb
+++ app/models/word_mapping.rb
@@ -2,6 +2,8 @@ class WordMapping < ActiveRecord::Base
   validates :word, presence: true, length: { maximum: 20 }
   validates :synonym, presence: true, length: { maximum: 20 }
 
+  validate :unique_pair
+
   def self.variations_of(sentence)
     arr1 = all.map do |word_mapping|
       if sentence.include?(word_mapping.word)
@@ -17,4 +19,12 @@ class WordMapping < ActiveRecord::Base
 
     arr1 + arr2
   end
+
+  private
+
+    def unique_pair
+      if WordMapping.exists?(user_id: user_id, word: word, synonym: synonym)
+        errors.add :base, '単語と同意語の組み合わせは既に存在しています。'
+      end
+    end
 end

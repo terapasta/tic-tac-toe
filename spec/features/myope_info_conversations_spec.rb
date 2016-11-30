66e1e231002d80f737369226901b9e328a02ae77
a.harada <redhornet96@gmail.com>
2016-11-30 14:42:24 +0900

    my-ope¿¿bot¿¿¿¿¿¿¿¿¿spec¿¿¿

---
+++ spec/features/myope_info_conversations_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'My-ope紹介Botのデータで意図した通りにBotとの対話が出来る' do
+  let(:chat) { @bot.chats.create!(guest_key: 'random-hogehoge-myope-info') }
+  let(:conversation_bot) { Conversation::Bot.new(@bot, message) }
+
+  before(:all) do
+    @bot = create(:bot)
+    file_import(@bot, 'myope_info.csv')
+  end
+
+  subject { conversation_bot.reply }
+
+  context '「セキュリティーはどうなっている？」とポストされた場合' do
+    let(:message) { chat.messages.build(speaker: 'guest', body: 'セキュリティーはどうなっている？') }
+    scenario do
+      expect(subject[0].body).to be_include 'セキュリティ対策については、ファイアウォールやSSL接続などの一般的な対策は行っております。'
+    end
+  end
+
+  context '「何歳ですか？」とポストされた場合' do
+    let(:message) { chat.messages.build(speaker: 'guest', body: '何歳ですか') }
+    scenario '回答失敗を返すこと' do
+      expect(subject[0].body).to eq '回答出来ませんでした。この回答失敗時のメッセージはBot編集画面から変更できます。'
+    end
+  end
+end

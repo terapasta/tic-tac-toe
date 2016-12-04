ca9c6b3a757ec54ee14f659c2f56913f72678c39
a.harada <redhornet96@gmail.com>
2016-12-04 15:33:18 +0900

    分類時にlearning_parameter.classify_thresoldを参照する

--- app/models/learning_parameter.rb
+++ app/models/learning_parameter.rb
@@ -4,6 +4,15 @@ class LearningParameter < ActiveRecord::Base
   enum algorithm: [ :logistic_regression, :naive_bayes ]
 
   def self.default_attributes
-      { algorithm: algorithms[:logistic_regression], params_for_algorithm: {}, include_failed_data: false }
+      {
+        algorithm: algorithms[:logistic_regression],
+        params_for_algorithm: {},
+        include_failed_data: false,
+        classify_threshold: 0.5,
+      }
+  end
+
+  def self.build_with_default
+    new(default_attributes)
   end
 end

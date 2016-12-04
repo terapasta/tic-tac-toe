9d98b492ad8d36627f82b31ddf66f12ca3b83978
a.harada <redhornet96@gmail.com>
2016-12-04 13:42:09 +0900

    新しいフィールドのデフォルト値を定義しておく(Seed_fuなど)

--- app/models/learning_parameter.rb
+++ app/models/learning_parameter.rb
@@ -1,5 +1,5 @@
 class LearningParameter < ActiveRecord::Base
   belongs_to :bot
 
-  enum algorithm: { :logistic_regression, :naive_bayes }
+  enum algorithm: [ :logistic_regression, :naive_bayes ]
 end

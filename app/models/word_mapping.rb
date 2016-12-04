c796a2347baf3e7d7be036dbbd6788b03a9c8d72
blueplanet <erguolinge@gmail.com>
2016-12-04 19:41:04 +0900

    リファクタリング

--- app/models/word_mapping.rb
+++ app/models/word_mapping.rb
@@ -9,21 +9,13 @@ class WordMapping < ActiveRecord::Base
   scope :for_user, -> (user) { where "user_id IS NULL OR user_id = :user_id", user_id: user.id }
 
   def self.variations_of(sentence, user)
-    word_mappings = for_user(user)
-
-    arr1 = word_mappings.map do |word_mapping|
+    for_user(user).map do |word_mapping|
       if sentence.include?(word_mapping.word)
         sentence.gsub(/#{word_mapping.word}/, word_mapping.synonym)
-      end
-    end.compact
-
-    arr2 = word_mappings.map do |word_mapping|
-      if sentence.include?(word_mapping.synonym)
+      elsif sentence.include?(word_mapping.synonym)
         sentence.gsub(/#{word_mapping.synonym}/, word_mapping.word)
       end
     end.compact
-
-    arr1 + arr2
   end
 
   private

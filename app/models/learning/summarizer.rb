89353ca78a26c7d776fb6d8f8b028a51391b8a60
a.harada <redhornet96@gmail.com>
2016-12-06 08:43:14 +0900

    imported_training_messageにもタグ付け出来る機能を実装する(途中)

--- app/models/learning/summarizer.rb
+++ app/models/learning/summarizer.rb
@@ -34,6 +34,7 @@ class Learning::Summarizer
         learning_training_messages << learning_training_message
       end
     end
+    # merge_tag_ids!(learning_training_messages)
     LearningTrainingMessage.import!(learning_training_messages)
   end
 

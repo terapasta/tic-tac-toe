1856bb8dd657bb98ea4986e10f180dbb7eeab972
a.harada <redhornet96@gmail.com>
2016-12-10 14:44:12 +0900

    学習済みモデルの取得パスを共通化する

--- learning/learning/core/persistance.py
+++ learning/learning/core/persistance.py
@@ -6,12 +6,14 @@ class Persistance:
 
     @classmethod
     def dump_model(self, estimator, bot_id):
-        config = Config()
-        # TODO ロジスティック回帰とは限らない
-        joblib.dump(estimator, "learning/models/%s/%s_logistic_reg_model" % (config.env, bot_id))
+        joblib.dump(estimator, self.get_model_path(bot_id))
 
     @classmethod
     def load_model(self, bot_id):
+        return joblib.load(self.get_model_path(bot_id))
+
+    @classmethod
+    def get_model_path(self, bot_id):
         config = Config()
         # TODO ロジスティック回帰とは限らない
-        return joblib.load("learning/models/%s/%s_logistic_reg_model" % (config.env, bot_id))
+        return "learning/models/%s/%s_logistic_reg_model" % (config.env, bot_id)

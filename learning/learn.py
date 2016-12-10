54d359c756ad6051e7421d29593b9e42a215af1f
a.harada <redhornet96@gmail.com>
2016-12-10 14:57:11 +0900

    learn.pyのパラメータを編集

--- learning/learn.py
+++ learning/learn.py
@@ -1,11 +1,11 @@
 from learning.core.learn.bot import Bot
 from learning.core.learn.learning_parameter import LearningParameter
 
-bot_id = 1
+bot_id = 5
 attr = {
     'include_failed_data': False,
-    'include_tag_vector': True,
-    'algorithm': LearningParameter.ALGORITHM_LOGISTIC_REGRESSION
+    'include_tag_vector': False,
+    'algorithm': LearningParameter.ALGORITHM_NAIVE_BAYES
 }
 learning_parameter = LearningParameter(attr)
 evaluator = Bot(bot_id, learning_parameter).learn()

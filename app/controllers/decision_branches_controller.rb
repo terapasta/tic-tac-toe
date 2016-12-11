85511f2ea59beef1d6043de2fd2baf70c00b94ca
kozo yamagata <tune002@gmail.com>
2016-12-11 13:47:48 +0900

    Add decision_branches#index action

--- app/controllers/decision_branches_controller.rb
+++ app/controllers/decision_branches_controller.rb
@@ -1,7 +1,15 @@
 class DecisionBranchesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_bot
-  before_action :set_decision_branch
+  before_action :set_decision_branch, only: [:show, :update]
+
+  def index
+    respond_to do |format|
+      @answer = @bot.answers.find_by(id: params[:answer_id])
+      @decision_branches = @answer.try(:decision_branches) || @bot.decision_branches
+      format.json { render json: @decision_branches.decorate.map(&:as_json) }
+    end
+  end
 
   def show
     respond_to do |format|

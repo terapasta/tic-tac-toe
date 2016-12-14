23d065bd194271607cc14977034d31fd57468961
kozo yamagata <tune002@gmail.com>
2016-12-14 16:25:48 +0900

    Revert "Revert "Merge pull request #35 from mofmof/fix_conversation_tree""
    This reverts commit 07f48b7a12aa14a501e129540979b2515476dcae.

--- app/controllers/conversation_trees_controller.rb
+++ app/controllers/conversation_trees_controller.rb
@@ -3,6 +3,10 @@ class ConversationTreesController < ApplicationController
 
   def show
     @bot = Bot.find params[:bot_id]
-    @answers = @bot.answers.order('id desc').decorate
+    @answers = @bot.answers
+      .includes(:decision_branches)
+      .top_level(@bot.id)
+      .order('id desc')
+      .decorate
   end
 end

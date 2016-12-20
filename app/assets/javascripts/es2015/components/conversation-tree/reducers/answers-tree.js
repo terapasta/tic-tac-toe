2f8e4a32193dc4b3af040f9675e45693ff8b45aa
kozo yamagata <tune002@gmail.com>
2016-12-20 20:57:06 +0900

    Modify answersTree reducer

--- app/assets/javascripts/es2015/components/conversation-tree/reducers/answers-tree.js
+++ app/assets/javascripts/es2015/components/conversation-tree/reducers/answers-tree.js
@@ -1,16 +1,78 @@
+import compact from "lodash/compact";
+import flatten from "lodash/flatten";
+
 import * as t from "../action-types";
 
 export default function answersTree(state = [], action) {
-  const { type/*, answerModel*/ } = action;
+  const { type, answerModel, decisionBranchModel, answerId, decisionBranchId } = action;
+  let newAnswerNode = {};
+  let newDecisionBranchNode = {};
 
   switch(type) {
-    case t.UPDATE_ANSWERS_TREE:
+    case t.ADD_ANSWER_TO_ANSWERS_TREE:
+      newAnswerNode = { id: answerModel.id, decisionBranches: [] };
+
+      if (decisionBranchId == null) {
+        return state.concat([newAnswerNode]);
+      } else {
+        findDecisionBranchFromTree(state, decisionBranchId, (decisionBranchNode) => {
+          decisionBranchNode.answer = newAnswerNode;
+        });
+        return state.concat();
+      }
+
+    case t.ADD_DECISION_BRANCH_TO_ANSWERS_TREE:
+      newDecisionBranchNode = { id: decisionBranchModel.id, answer: null };
+
+      findAnswerFromTree(state, answerId, (answerNode) => {
+        answerNode.decisionBranches.push(newDecisionBranchNode);
+      });
       return state.concat();
-    case t.DELETE_ANSWERS_TREE:
+
+    case t.DELETE_ANSWER_FROM_ANSWERS_TREE:
+      if (decisionBranchId == null) {
+        return state.filter((answerNode) => answerNode.id != answerModel.id);
+      } else {
+        findDecisionBranchFromTree(state, decisionBranchId, (decisionBranchNode) => {
+          delete decisionBranchNode.answer;
+        });
+        return state.concat();
+      }
+
+    case t.DELETE_DECISION_BRANCH_FROM_ANSWERS_TREE:
+      findAnswerFromTree(state, answerId, (answerNode) => {
+        answerNode.decisionBranches = answerNode.decisionBranches.filter((decisionBranchNode) => {
+          return decisionBranchNode.id != decisionBranchModel.id;
+        });
+      });
       return state.concat();
-    case t.ADD_ANSWERS_TREE:
-      return state.concat();
+
     default:
       return state;
   }
+}
+
+function findAnswerFromTree(answersTree, answerId, foundCallback) {
+  answersTree.forEach((answerNode) => {
+    if (answerNode.id === answerId) {
+      foundCallback(answerNode);
+    } else {
+      const answers = compact(answerNode.decisionBranches.map((db) => db.answer));
+      findAnswerFromTree(answers, answerId, foundCallback);
+    }
+  });
+}
+
+function findDecisionBranchFromTree(answersTree, decisionBranchId, foundCallback) {
+  const handler = (decisionBranchNodes) => {
+    decisionBranchNodes.forEach((decisionBranchNode) => {
+      if (decisionBranchNode.id === decisionBranchId) {
+        foundCallback(decisionBranchNode);
+      } else if (decisionBranchNode.answer != null) {
+        handler(decisionBranchNode.answer.decisionBranches);
+      }
+    });
+  };
+  const decisionBranchNodes = flatten(answersTree.map((a) => a.decisionBranches));
+  handler(decisionBranchNodes);
 }

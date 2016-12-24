b3683d1c6715f58f759f1f33a8291da82ea9ca79
kozo yamagata <tune002@gmail.com>
2016-12-24 12:42:08 +0900

    Update answersRepo reducer

--- app/assets/javascripts/es2015/components/conversation-tree/reducers/answers-repo.js
+++ app/assets/javascripts/es2015/components/conversation-tree/reducers/answers-repo.js
@@ -3,15 +3,16 @@ import assign from "lodash/assign";
 import * as t from "../action-types";
 
 export default function answersRepo(state = {}, action) {
-  const { type/*, answersRepo*/ } = action;
+  const { type, answerModel } = action;
 
   switch(type) {
     case t.UPDATE_ANSWERS_REPO:
-      return assign({}, state);
+      return assign({}, state, { [answerModel.id]: answerModel.attrs });
     case t.DELETE_ANSWERS_REPO:
+      delete state[answerModel.id];
       return assign({}, state);
     case t.ADD_ANSWERS_REPO:
-      return assign({}, state);
+      return assign({}, state, { [answerModel.id]: answerModel.attrs });
     default:
       return state;
   }

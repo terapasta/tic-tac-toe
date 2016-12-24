db21cebd7657923599d62b2df6cdbbebc438a6d0
kozo yamagata <tune002@gmail.com>
2016-12-24 12:43:40 +0900

    Update decisionBranchesRepo reducer

--- app/assets/javascripts/es2015/components/conversation-tree/reducers/decision-branches-repo.js
+++ app/assets/javascripts/es2015/components/conversation-tree/reducers/decision-branches-repo.js
@@ -3,15 +3,16 @@ import assign from "lodash/assign";
 import * as t from "../action-types";
 
 export default function decisionBranchesRepo(state = {}, action) {
-  const { type } = action;
+  const { type, decisionBranchModel } = action;
 
   switch(type) {
     case t.UPDATE_DECISION_BRANCHES_REPO:
-      return assign({}, state);
+      return assign({}, state, { [decisionBranchModel.id]: decisionBranchModel.attrs });
     case t.DELETE_DECISION_BRANCHES_REPO:
+      delete state[decisionBranchModel.id];
       return assign({}, state);
     case t.ADD_DECISION_BRANCHES_REPO:
-      return assign({}, state);
+      return assign({}, state, { [decisionBranchModel.id]: decisionBranchModel.attrs });
     default:
       return state;
   }

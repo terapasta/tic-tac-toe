9359262387746d69683054ce41d0a0b0670d1c40
kozo yamagata <tune002@gmail.com>
2017-01-18 19:01:52 +0900

    Modify BotPolicy

--- app/policies/bot_policy.rb
+++ app/policies/bot_policy.rb
@@ -1,10 +1,10 @@
 class BotPolicy < ApplicationPolicy
   def index?
-    user.normal?
+    user.normal? || user.staff?
   end
 
   def show?
-    user.normal?
+    user.staff? || (user.normal? && record.user == user)
   end
 
   def new?
@@ -12,7 +12,7 @@ class BotPolicy < ApplicationPolicy
   end
 
   def create?
-    false
+    user.staff?
   end
 
   def edit?
@@ -20,10 +20,10 @@ class BotPolicy < ApplicationPolicy
   end
 
   def update?
-    user.normal?
+    user.staff? || (user.normal? && record.user == user)
   end
 
   def destroy?
-    false
+    user.staff?
   end
 end

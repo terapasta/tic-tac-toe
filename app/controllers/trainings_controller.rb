c4e1ae3a754f20358a9df26486a75a3234704a8a
kozo yamagata <tune002@gmail.com>
2017-01-19 15:28:31 +0900

    Modify TrainingsController to use BotUsable

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,4 +1,5 @@
 class TrainingsController < ApplicationController
+  include BotUsable
   before_action :authenticate_user!
   before_action :set_bot
   before_action :set_training, only: [:show, :create, :destroy]
@@ -28,7 +29,8 @@ class TrainingsController < ApplicationController
 
   private
     def set_bot
-      @bot = current_user.bots.find(params[:bot_id])
+      @bot = bots.find(params[:bot_id])
+      authorize @bot
     end
 
     def set_training

4848ef33837d4114476f06a819ce9b4f5bb730a9
kozo yamagata <tune002@gmail.com>
2017-01-19 19:27:44 +0900

    Add SentenceSynonym.target_date scope

--- app/models/sentence_synonym.rb
+++ app/models/sentence_synonym.rb
@@ -3,4 +3,10 @@ class SentenceSynonym < ActiveRecord::Base
   belongs_to :created_user, class_name: 'User'
 
   validates :body, presence: true
+
+  scope :target_date, -> (date) {
+    if date.present?
+      where('sentence_synonyms.created_at BETWEEN ? AND ?', date.beginning_of_day, date.end_of_day)
+    end
+  }
 end

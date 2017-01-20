88d6dc16040dcaf10eca5a1b180ce5e57a356659
kozo yamagata <tune002@gmail.com>
2017-01-20 15:10:19 +0900

    Modify ImportedSentenceSynonymsController to works without bot

--- app/controllers/imported_sentence_synonyms_controller.rb
+++ app/controllers/imported_sentence_synonyms_controller.rb
@@ -1,51 +1,43 @@
 class ImportedSentenceSynonymsController < ApplicationController
+  include SentenceSynonymsOperatable
   before_action :authenticate_user!
   before_action :set_bot
 
   def index
-    @imported_training_messages = @bot.imported_training_messages
+    authorize SentenceSynonym
+    @imported_training_messages = imported_training_messages
     @target_date = parse_target_date
   end
 
   def new
     @imported_training_message = ImportedTrainingMessage.pick_sentence_synonyms_not_enough(@bot, current_user)
-    @registered_sentence_synonyms_count = @imported_training_message.sentence_synonyms.where(created_user_id: current_user.id).count
     if @imported_training_message.blank?
-      flash[:error] = '未作業の文章がありませんでした'
-      redirect_to root_path and return
+      redirect_to root_path, alert: '未作業の文章がありませんでした' and return
     end
-    (3 - @registered_sentence_synonyms_count).times.map {
-      @imported_training_message.sentence_synonyms.build
-    }
+    @imported_training_message.build_sentence_synonyms_for(current_user)
   end
 
   def create
-    @imported_training_message = @bot.imported_training_messages.find(imported_training_message_params[:id])
+    @imported_training_message = imported_training_messages.find(imported_training_message_params[:id])
     @imported_training_message.assign_attributes(imported_training_message_params)
-    # HACK 他ユーザーのデータも上書きしてしまう問題の対処。ひとまず画面側にcreated_user_idをもたせた
-    # @imported_training_message.sentence_synonyms.each { |ss| ss.created_user = current_user }
     if @imported_training_message.save
-      redirect_to new_bot_imported_sentence_synonym_path(@bot), notice: '登録しました。'
+      redirect_to new_path, notice: '登録しました。'
     else
       logger.debug @imported_training_message.errors.full_messages
-      flash[:alert] = '登録に失敗しました。'
+      flash.now.alert = '登録に失敗しました。'
       render :new
     end
   end
 
   def destroy
-    @sentence_synonym = @bot.sentence_synonyms.find(params[:id])
+    @sentence_synonym = sentence_synonyms.find(params[:id])
     @sentence_synonym.destroy
-    redirect_to bot_imported_sentence_synonyms_path(@bot), notice: '削除しました'
+    redirect_to index_path, notice: '削除しました'
   end
 
   private
     def set_bot
-      @bot = Bot.find(params[:bot_id])
-    end
-
-    def imported_training_message_params
-      params.require(:imported_training_message).permit(:id, sentence_synonyms_attributes: [:body, :created_user_id])
+      @bot = Bot.find(params[:bot_id]) if params[:bot_id].present?
     end
 
     def parse_target_date

7d7ec0ded1abb6e74d2afc0713ad1575aa760dc3
kozo yamagata <tune002@gmail.com>
2017-01-25 12:25:19 +0900

    Add threads_controller_spec

---
+++ spec/controllers/threads_controller_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+RSpec.describe ThreadsController, type: :controller do
+  let!(:normal_user) do
+    create(:user, role: :normal)
+  end
+
+  let!(:staff_user) do
+    create(:user, role: :staff)
+  end
+
+  let!(:bot) do
+    create(:bot, user: normal_user)
+  end
+
+  let!(:succeeded_chat) do
+    create(:chat, bot: bot).tap do |chat|
+      create_list(:message, 2, chat: chat)
+    end
+  end
+
+  let!(:failed_chat) do
+    create(:chat, bot: bot).tap do |chat|
+      create_list(:message, 1, :failed, chat: chat)
+      create_list(:message, 2, chat: chat)
+    end
+  end
+
+  let!(:succeeded_caht_by_staff) do
+    create(:chat, bot: bot, is_staff: true).tap do |chat|
+      create_list(:message, 2, chat: chat)
+    end
+  end
+
+  let!(:failed_chat_by_staff) do
+    create(:chat, bot: bot, is_staff: true).tap do |chat|
+      create_list(:message, 1, :failed, chat: chat)
+      create_list(:message, 2, chat: chat)
+    end
+  end
+
+  describe 'GET #index' do
+    subject do
+      get :index, bot_id: bot.id, filter: filter
+      assigns[:chats]
+    end
+
+    let(:filter) do
+      nil
+    end
+
+    context 'when logged in as normal user' do
+      before do
+        sign_in normal_user
+      end
+
+      context 'without filter param' do
+        it 'assigns all chats (not included is_staff)' do
+          expect(subject).to match_array([succeeded_chat, failed_chat])
+        end
+      end
+
+      context 'with filter param' do
+        let(:filter) do
+          '1'
+        end
+
+        it 'assigns only failed chats (not included is_staff)' do
+          expect(subject).to match_array([failed_chat])
+        end
+      end
+    end
+
+    context 'when logged in as staff user' do
+      before do
+        sign_in staff_user
+      end
+
+      context 'without filter param' do
+        it 'assigns all chats (included is_staff)' do
+          expect(subject).to match_array([succeeded_chat, succeeded_caht_by_staff, failed_chat, failed_chat_by_staff])
+        end
+      end
+
+      context 'with filter param' do
+        let(:filter) do
+          '1'
+        end
+
+        it 'assigns only failed chats (included is_staf)' do
+          expect(subject).to match_array([failed_chat, failed_chat_by_staff])
+        end
+      end
+    end
+  end
+end

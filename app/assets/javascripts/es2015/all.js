5ed9df2a90e286be4380b9b326f1348290a13089
kozo yamagata <tune002@gmail.com>
2017-01-27 19:16:58 +0900

    Implement MessageRatingButtons component

--- app/assets/javascripts/es2015/all.js
+++ app/assets/javascripts/es2015/all.js
@@ -2,8 +2,13 @@ import ConversationTree from "./components/conversation-tree";
 import ConversationTreeReducers from "./components/conversation-tree/reducers";
 
 import BotResetButton from "./components/bot-reset-button";
+import MessageRatingButtons from "./components/message-rating-buttons";
 
 import mountComponent, { mountComponentWithRedux } from "./modules/mount-component";
+
+window.initMessageRatingButtons = () => {
+  MessageRatingButtons.mountComponentAll();
+};
 
 function init() {
   mountComponentWithRedux(ConversationTree, ConversationTreeReducers);

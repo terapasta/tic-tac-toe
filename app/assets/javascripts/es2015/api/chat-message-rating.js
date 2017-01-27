0752ebd75c4f4b6e81c1f6f5741514c2e2370f6e
kozo yamagata <tune002@gmail.com>
2017-01-27 19:15:59 +0900

    Add api functions for message rating

---
+++ app/assets/javascripts/es2015/api/chat-message-rating.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import assign from "lodash/assign";
+
+import authenticityToken from "../modules/authenticity-token";
+
+export function good(token, messageId) {
+  return request(token, messageId, "good");
+}
+
+export function bad(token, messageId) {
+  return request(token, messageId, "bad");
+}
+
+export function nothing(token, messageId) {
+  return request(token, messageId, "nothing");
+}
+
+function request(token, messageId, resource) {
+  const url = `/embed/${token}/chats/messages/${messageId}/rating/${resource}.json`;
+  return axios.put(url, {}, config());
+}
+
+function config(options = {}) {
+  return assign({
+    headers: {
+      "X-CSRF-Token": authenticityToken(),
+    }
+  }, options);
+}

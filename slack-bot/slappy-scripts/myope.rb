7b442f32b37f9bae2bdb05b347608428340bb7d5
kozo yamagata <tune002@gmail.com>
2017-02-01 17:05:08 +0900

    Fix slappy config

--- slack-bot/slappy-scripts/myope.rb
+++ slack-bot/slappy-scripts/myope.rb
@@ -1,5 +1,4 @@
 require 'json'
-require File.expand_path('../config/environment')
 
 helper = SlackBotHelper.new
 
--- slack-bot/slappy_config.rb
+++ slack-bot/slappy_config.rb
@@ -1,3 +1,5 @@
+require File.expand_path('../config/environment')
+
 Slappy.configure do |config|
   ## Slappy Settings
   #

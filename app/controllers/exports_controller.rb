0ee9dbc8a1e18be1aafbffe33f3169e23f4086c6
kozo yamagata <tune002@gmail.com>
2017-02-02 14:58:09 +0900

    Add bots/exports#index action

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -3,6 +3,9 @@ class ExportsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_bot
 
+  def index
+  end
+
   def show
     Learning::Summarizer.new(@bot).summary
     send_data LearningTrainingMessage.to_csv(@bot), filename: 'learning_training_messages.csv', type: :csv

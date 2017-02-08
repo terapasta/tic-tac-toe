deef54afdbeaf789f9ba0f0bc43c4abd8fc7d1ad
kozo yamagata <tune002@gmail.com>
2017-02-08 15:07:50 +0900

    Add conversation tree feature spec

---
+++ spec/features/conversation_tree_spec.rb
@@ -0,0 +1,114 @@
+require 'rails_helper'
+
+RSpec.describe 'ConversationTree', type: :feature, js: true do
+  include RequestSpecHelper
+
+  let!(:staff) do
+    create(:user, role: :staff)
+  end
+
+  let!(:user) do
+    create(:user)
+  end
+
+  let!(:bot) do
+    create(:bot, user: user)
+  end
+
+  let!(:question_answer) do
+    create(:question_answer, answer: answer)
+  end
+
+  let!(:answer) do
+    create(:answer, bot: bot)
+  end
+
+  let!(:decision_branches) do
+    create_list(:decision_branch, 2, bot: bot, answer: answer)
+  end
+
+  let!(:nested_answer) do
+    create(:answer, bot: bot).tap do |a|
+      decision_branches.first.tap do |d|
+        d.next_answer = a
+        d.save
+      end
+    end
+  end
+
+  before do
+    sign_in staff
+    visit "/bots/#{bot.id}/conversation_tree"
+  end
+
+  scenario 'display tree nodes' do
+    find("#answer-#{answer.id}").click
+    expect(page).to have_content(question_answer.question)
+    find("#decision-branch-#{decision_branches.first.id}").click
+    expect(page).to have_content(nested_answer.body)
+  end
+
+  scenario 'creates answer'  do
+    find('.tree__item-add').click
+    fill_in 'answer-body', with: 'new answer'
+    click_link '保存'
+    within '.master-detail-panel__master' do
+      expect(page).to have_content('new answer')
+    end
+  end
+
+  scenario 'creates decision_branch' do
+    find("#answer-#{answer.id}").click
+    find('#add-decision-branch-button').click
+    fill_in 'decision-branch-body', with: 'new decision branch'
+    find('span.btn.btn-primary').click
+    within '.master-detail-panel__master' do
+      expect(page).to have_content('new decision branch')
+    end
+  end
+
+  scenario 'updates answer' do
+    find("#answer-#{answer.id}").click
+    sleep 1
+    fill_in 'answer-body', with: 'updated answer'
+    click_link '保存'
+    within '.master-detail-panel__master' do
+      expect(page).to have_content('updated answer')
+    end
+  end
+
+  scenario 'updates decision branch' do
+    find("#answer-#{answer.id}").click
+    within "#decision-branch-item-#{decision_branches.first.id}" do
+      find('.btn').click
+    end
+    within "#decision-branch-item-#{decision_branches.first.id}" do
+      fill_in 'decision-branch-body', with: 'updated decision branch'
+      find('.btn-default').click
+    end
+    within '.master-detail-panel__master' do
+      expect(page).to have_content('updated decision branch')
+    end
+  end
+
+  scenario 'deletes answer' do
+    find("#answer-#{answer.id}").click
+    find("#delete-answer-button").click
+    within '.master-detail-panel__master' do
+      expect(page).to_not have_content(answer.body)
+    end
+  end
+
+  scenario 'deletes decision branch' do
+    find("#answer-#{answer.id}").click
+    within "#decision-branch-item-#{decision_branches.first.id}" do
+      find('.btn').click
+    end
+    within "#decision-branch-item-#{decision_branches.first.id}" do
+      find('.btn-danger').click
+    end
+    within '.master-detail-panel__master' do
+      expect(page).to_not have_content(decision_branches.first.body)
+    end
+  end
+end

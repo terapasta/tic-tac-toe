b6e6e7facbcc29e3ce61cec9a3aaa230d39db49e
kozo yamagata <tune002@gmail.com>
2017-02-09 18:03:18 +0900

    Insert error log to slappy

--- slack-bot/slappy-scripts/myope.rb
+++ slack-bot/slappy-scripts/myope.rb
@@ -12,30 +12,38 @@ hello do
 end
 
 respond '正答率' do |e|
-  Slappy.logger.info e.data.to_json
-  e.reply_to(e.user, 'はい！各ボットの正答率です！', {
-    attachments: helper.bot_scores_as_attachment,
-  })
+  begin
+    Slappy.logger.info e.data.to_json
+    e.reply_to(e.user, 'はい！各ボットの正答率です！', {
+      attachments: helper.bot_scores_as_attachment,
+    })
+  rescue => e
+    Slappy.logger.error e.message
+  end
 end
 
 respond '.*' do |e|
   Slappy.logger.info e.data.to_json
-  bot = Bot.first
-  chat = bot.chats.find_or_create_by(guest_key: Slappy.configuration.token + Time.zone.now.strftime('%Y-%m-%d'))
+  begin
+    bot = Bot.first
+    chat = bot.chats.find_or_create_by(guest_key: Slappy.configuration.token + Time.zone.now.strftime('%Y-%m-%d'))
 
-  if chat.messages.count.zero?
-    chat.messages << chat.build_start_message
-    chat.save!
+    if chat.messages.count.zero?
+      chat.messages << chat.build_start_message
+      chat.save!
+    end
+
+    message = chat.messages.build(
+      body: e.data['text'].gsub(/<@[A-Z0-9]+>/, ''),
+      speaker: 'guest',
+      user_agent: 'slack',
+    )
+    messages = receive_and_reply!(chat, message)
+    text = messages.map(&:body).join(",")
+    e.reply_to(e.user, text) if text.present?
+  rescue => e
+    Slappy.logger.error e.message
   end
-
-  message = chat.messages.build(
-    body: e.data['text'].gsub(/<@[A-Z0-9]+>/, ''),
-    speaker: 'guest',
-    user_agent: 'slack',
-  )
-  messages = receive_and_reply!(chat, message)
-  text = messages.map(&:body).join(",")
-  e.reply_to(e.user, text) if text.present?
 end
 
 # 毎週月曜10am

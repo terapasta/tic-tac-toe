b3c275143fa4bd388811f24a214fa538dfb8d7ab
kozo yamagata <tune002@gmail.com>
2017-02-15 14:22:10 +0900

    Fix AllowedHost#scheme

--- app/models/allowed_host.rb
+++ app/models/allowed_host.rb
@@ -1,5 +1,5 @@
 class AllowedHost < ActiveRecord::Base
   validates :domain, presence: true
   belongs_to :bot, required: true
-  enum scheme: [:http, :https]
+  enum scheme: ['http://', 'https://']
 end

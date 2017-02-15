c7d1a393646f3e6f5087039416b84d3e74763638
kozo yamagata <tune002@gmail.com>
2017-02-15 14:22:22 +0900

    Add AllowedHost#to_origin

--- app/models/allowed_host.rb
+++ app/models/allowed_host.rb
@@ -2,4 +2,8 @@ class AllowedHost < ActiveRecord::Base
   validates :domain, presence: true
   belongs_to :bot, required: true
   enum scheme: ['http://', 'https://']
+
+  def to_origin
+    "#{scheme}#{domain}"
+  end
 end

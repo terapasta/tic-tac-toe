f6320e2d093876be6a16bb799ad8654ac2cefb3e
kozo yamagata <tune002@gmail.com>
2017-02-20 12:28:41 +0900

    Fix chat policy

--- app/policies/chat_policy.rb
+++ app/policies/chat_policy.rb
@@ -4,31 +4,29 @@ class ChatPolicy < ApplicationPolicy
   end
 
   def new?
-    if request.referer.blank?
-      user.staff? || user.id == record.bot.user.id || record.bot.allowed_hosts.blank?
-    else
-      referer_is_allowed_origin?
-    end
+    return true if user.staff? || bot_owner? || record.bot.allowed_hosts.blank?
+    return false if request.referer.blank?
+    referer_is_allowed_origin?
   end
 
   private
     def referer_is_allowed_origin?
       ref = URI.parse(request.referer)
-      if record.bot.allowed_hosts.any?
-        record.bot.allowed_hosts
-          .map(&:to_origin)
-          .map(&Addressable::URI.method(:parse))
-          .map { |o|
-            is_match_host = if o.host.starts_with?('*.')
-              ref.host.ends_with?(o.host.sub(/^\*\./, ''))
-            else
-              ref.host == o.host
-            end
-            is_match_host && ref.scheme == o.scheme
-          }
-          .include?(true)
-        else
-          true
-        end
+      record.bot.allowed_hosts
+        .map(&:to_origin)
+        .map(&Addressable::URI.method(:parse))
+        .map { |o|
+          is_match_host = if o.host.starts_with?('*.')
+                            ref.host.ends_with?(o.host.sub(/^\*\./, ''))
+                          else
+                            ref.host == o.host
+                          end
+          is_match_host && ref.scheme == o.scheme
+        }
+        .include?(true)
+    end
+
+    def bot_owner?
+      user.id == record.bot.user.id
     end
 end

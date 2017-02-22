7fb141a981a03990983d72d81332fb7b3d577f57
kozo yamagata <tune002@gmail.com>
2017-02-22 11:46:38 +0900

    Modify TrainingMessageForm

--- app/assets/javascripts/es2015/components/training-message-form.js
+++ app/assets/javascripts/es2015/components/training-message-form.js
@@ -6,15 +6,22 @@ export default class TrainingMessageForm {
     this.el = document.querySelector("form#new_training_message");
     if (this.el == null) { return; }
     this.bindSubmit();
+    this.isSubmitFromScript = false;
   }
 
   bindSubmit() {
     this.el.addEventListener("submit", (e) => {
+      if (!this.isSubmitFromScript) { e.preventDefault(); }
+
       const { value } = this.el.querySelector("input[type='text']");
       const trimed = trim(value);
+
       if (trimed.length == 0) {
         e.preventDefault();
         toastr.warning("メッセージを入力してください");
+      } else {
+        this.isSubmitFromScript = true;
+        this.el.submit();
       }
     });
   }

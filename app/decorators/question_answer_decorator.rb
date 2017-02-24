7b6cbf2f9975aa9db4d07c588632c80a2d2c4c4e
kozo yamagata <tune002@gmail.com>
2017-02-24 11:31:30 +0900

    Add nil guard to QuestionAnswerDecorator

--- app/decorators/question_answer_decorator.rb
+++ app/decorators/question_answer_decorator.rb
@@ -1,10 +1,10 @@
-class QuestionAnswerDecorator < Draper::Decorator
+class QuestionAnswerDecorator < ApplicationDecorator
   delegate_all
 
   def as_tree_node_json
     {
       id: object.id,
-      answer: object.answer.decorate.as_tree_node_json
+      answer: object.answer&.decorate&.as_tree_node_json
     }
   end
 end

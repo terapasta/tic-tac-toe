1ac5b0d9a0234ac5b158e80952a649f2cc966472
kozo yamagata <tune002@gmail.com>
2017-02-24 11:33:02 +0900

    Update questionsTree reducer

--- app/assets/javascripts/es2015/components/conversation-tree/reducers/questions-tree.js
+++ app/assets/javascripts/es2015/components/conversation-tree/reducers/questions-tree.js
@@ -1,9 +1,17 @@
 import * as t from "../action-types";
 
 export default function questionsTree(state = [], action) {
-  const { type } = action;
+  const { type, questionModel } = action;
+  let newQuestionNode;
 
   switch(type) {
+    case t.ADD_QUESTION_TO_QUESTIONS_TREE:
+      newQuestionNode = { id: questionModel.id };
+      return [newQuestionNode].concat(state);
+
+    case t.DELETE_QUESTION_FROM_QUESTIONS_TREE:
+      return state.filter((questionNode) => questionNode.id != questionModel.id);
+
     default:
       return state;
   }

ed1506b45a52145ab2c42d1030de3d86930440d7
kozo yamagata <tune002@gmail.com>
2017-03-13 14:33:18 +0900

    Add Panel component

---
+++ app/assets/javascripts/es2015/components/panel.jsx
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from "react";
+import isEmpty from "is-empty";
+import classNames from "classnames";
+
+function PanelHead(props) {
+  const { title } = props;
+  if (isEmpty(title)) { return null; }
+
+  return (
+    <div className="panel-heading">
+      <div className="panel-title">{title}</div>
+    </div>
+  );
+}
+
+function Panel(props) {
+  const { title, modifier, children } = props;
+  const className = classNames("panel", {
+    "panel-default": modifier == null || modifier === "default",
+    "panel-danger": modifier === "danger",
+  });
+
+  return (
+    <div className={className}>
+      <PanelHead {...{ title }} />
+      <div className="panel-body">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+Panel.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Panel;

f202267dfb4095ff730155c0f7dfde99eeafc218
kozo yamagata <tune002@gmail.com>
2017-03-14 18:11:44 +0900

    Update QuestionAnswerForm component

--- app/assets/javascripts/es2015/components/question-answer-form.jsx
+++ app/assets/javascripts/es2015/components/question-answer-form.jsx
@@ -1,6 +1,18 @@
 import React, { Component, PropTypes } from "react";
+import TextArea from "react-textarea-autosize";
+import { RadioGroup, Radio } from "react-radio-group";
+import isEmpty from "is-empty";
+import axios from "axios";
+import debounce from "lodash/debounce";
+import get from "lodash/get";
 
 import Panel from "./panel";
+import Question from "../models/question";
+
+export const AnswerMode = {
+  Input: "input",
+  Select: "select",
+};
 
 export default class QuestionAnswerForm extends Component {
   static get componentName() {
@@ -9,23 +21,328 @@ export default class QuestionAnswerForm extends Component {
 
   static get propTypes() {
     return {
+      botId: PropTypes.number.isRequired,
+      id: PropTypes.number,
     };
   }
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      questionBody: "",
+      answerHeadline: "",
+      answerBody: "",
+      answerId: null,
+      answerMode: AnswerMode.Input,
+      searchingAnswerQuery: "",
+      searchingAnswerPage: 1,
+      candidateAnswers: [],
+      selectedAnswer: null,
+      isProcessing: false,
+    };
+    this.debouncedSearchAnswers = debounce(this.searchAnswers, 250);
+  }
+
+  componentDidMount() {
+    this.fetchQuestionAnswer();
   }
 
   render() {
+    const {
+      questionBody,
+      answerHeadline,
+      answerBody,
+      answerId,
+      answerMode,
+      searchingAnswerQuery,
+      candidateAnswers,
+      selectedAnswer,
+      isProcessing,
+    } = this.state;
+
     return (
       <Panel title="Q&A">
-        <form>
-          <div className="form-group">
-            <input className="btn btn-primary" value="登録" />
+        <div className="form-group">
+          <label htmlFor="question-body">質問</label>
+          <TextArea
+            {...{
+              id: "question-body",
+              value: questionBody,
+              className: "form-control",
+              rows: 3,
+              onChange: this.onChangeQuestionBody.bind(this),
+              placeholder: "質問を入力してください（例：カードキー無くしてしまったのですが、どうすればいいですか）",
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <RadioGroup {...{
+            id: "answer-mode",
+            name: "answer-mode",
+            selectedValue: answerMode,
+            onChange: this.onChangeAnswerMode.bind(this),
+          }}>
+            <label>回答</label>
+            <label className="checkbox-inline">
+              <Radio value={AnswerMode.Input} />{" "}新しく回答を入力
+            </label>
+            <label className="checkbox-inline">
+              <Radio value={AnswerMode.Select} />{" "}既存の回答を選択
+            </label>
+          </RadioGroup>
+        </div>
+        {answerMode === AnswerMode.Input && (
+          <div>
+            <div className="form-group">
+              <input
+                {...{
+                  id: "answer-headline",
+                  value: answerHeadline,
+                  name: "question_answer[answer_attributes][headline]",
+                  className: "form-control",
+                  onChange: this.onChangeAnswerHeadline.bind(this),
+                  placeholder: "回答の見出しを入力してください（例：紛失物について）",
+                }}
+              />
+            </div>
+            <div className="form-group">
+              <TextArea
+                {...{
+                  id: "answer-body",
+                  value: answerBody,
+                  name: "question_answer[answer_attributes][body]",
+                  className: "form-control",
+                  rows: 3,
+                  onChange: this.onChangeAnswerBody.bind(this),
+                  placeholder: "回答を入力してください（例：カードキーの再発行手続きを行ってください。申込書はこちら http://example.com/...）"
+                }}
+              />
+            </div>
           </div>
-        </form>
+        )}
+        {answerMode === AnswerMode.Select && (
+          <div className="form-group">
+            <input
+              {...{
+                type: "search",
+                id: "answer-search",
+                className: "form-control",
+                placeholder: "既存の回答に含まれる文字列を入力してください",
+                value: searchingAnswerQuery,
+                onChange: this.onChangeSearchAnswer.bind(this),
+                disabled: isProcessing,
+              }}
+            />
+          {isProcessing && <div className="well">検索中...</div>}
+          {!isProcessing && !isEmpty(candidateAnswers) && (
+            <div className="well" id="candidate-answers">
+              {candidateAnswers.map((a, i) => {
+                return (
+                  <Panel {...{
+                    key: i,
+                    isClickable: true,
+                    onClickBody: this.onClickCandidateAnswer.bind(this, a),
+                    id: `candidate-answer-${a.id}`,
+                  }}>
+                    {this.renderAnswer(a)}
+                  </Panel>
+                );
+              })}
+            </div>
+          )}
+          {!isEmpty(selectedAnswer) && (
+            <div className="well">
+              <label>
+                選択した回答{" "}
+                <a {...{
+                  href: "#",
+                  classNames: "btn btn-warning btn-sm",
+                  onClick: this.onClickRejectAnswer.bind(this),
+                  id: "reject-answer",
+                }}>&times; 選択を解除</a>
+              </label>
+              <Panel>{this.renderAnswer(selectedAnswer)}</Panel>
+            </div>
+          )}
+          </div>
+        )}
+        <div className="form-group">
+          <input
+            {...{
+              type: "submit",
+              className: "btn btn-primary",
+              value: "登録",
+              onClick: this.onClickSubmitButton.bind(this),
+            }}
+          />
+        </div>
       </Panel>
     );
+  }
+
+  renderAnswer(a) {
+    return (
+      <span>
+        <p>
+          {!isEmpty(a.headline) && (
+            <span>
+              <small>{a.headline}</small>
+              <br />
+            </span>
+          )}
+          {a.body}
+        </p>
+        <div className="text-muted">
+          <small>ID: {a.id} / 登録日時: {a.created_at}</small>
+        </div>
+      </span>
+    );
+  }
+
+  fetchQuestionAnswer() {
+    const { botId, id } = this.props;
+    if (isEmpty(botId) || isEmpty(id)) { return; }
+
+    Question.fetch(botId, id).then((questionModel) => {
+      console.log(questionModel);
+    });
+  }
+
+  searchAnswers() {
+    if (this.state.isProcessing) { return; }
+    this.setState({ isProcessing: true });
+
+    const { botId } = this.props;
+    const { searchingAnswerQuery, searchingAnswerPage } = this.state;
+    const params = {
+      q: { body_or_headline_cont: searchingAnswerQuery },
+      page: searchingAnswerPage,
+    };
+
+    return axios.get(`/bots/${botId}/answers.json`, { params }).then((res) => {
+      const newSearchingAnswerPage = res.data.length > 0 ?
+        searchingAnswerPage + 1 : searchingAnswerPage;
+
+      this.setState({
+        isProcessing: false,
+        candidateAnswers: res.data,
+        searchingAnswerPage: newSearchingAnswerPage,
+      });
+    }).catch((err) => {
+      console.error(err);
+      this.setState({ isProcessing: false });
+    });
+  }
+
+  postQuestionAnswer(payload) {
+    if (this.state.isProcessing) { return; }
+    this.setState({ isProcessing: true });
+
+    const { botId, id } = this.props;
+    const method = isEmpty(id) ? "post" : "put";
+    const path = isEmpty(id) ?
+      `/bots/${botId}/question_answers.json` :
+      `/bots/${botId}/question_answers/${id}.json`;
+
+    return axios[method](path, payload)
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          isProcessing: false,
+        });
+      }).catch((err) => {
+        this.setState({
+          isProcessing: false,
+        });
+      });
+  }
+
+  onChangeQuestionBody(e) {
+    this.setState({ questionBody: e.target.value });
+  }
+
+  onChangeAnswerMode(answerMode) {
+    this.setState({ answerMode });
+  }
+
+  onChangeAnswerHeadline(e) {
+    this.setState({ answerHeadline: e.target.value });
+  }
+
+  onChangeAnswerBody(e) {
+    this.setState({ answerBody: e.target.value });
+  }
+
+  onChangeSearchAnswer(e) {
+    const { value } = e.target;
+    this.setState({ searchingAnswerQuery: value });
+    if (!isEmpty(value) && value.length > 0) {
+      this.debouncedSearchAnswers(value);
+    } else {
+      this.setState({ candidateAnswers: [] });
+    }
+  }
+
+  onClickCandidateAnswer(selectedAnswer, e) {
+    e.preventDefault();
+    this.setState({
+      selectedAnswer,
+      candidateAnswers: [],
+      searchingAnswerQuery: "",
+    });
+  }
+
+  onClickRejectAnswer(e) {
+    e.preventDefault();
+    this.setState({
+      selectedAnswer: null,
+    })
+  }
+
+  onClickSubmitButton() {
+    const {
+      answerMode,
+      answerBody,
+      answerHeadline,
+      selectedAnswer,
+      questionBody
+    } = this.state;
+
+    let errors = [];
+    let payload = {
+      question: questionBody,
+    };
+
+    if (isEmpty(questionBody)) {
+      errors.push("質問を入力してください");
+    }
+
+    switch(answerMode) {
+      case AnswerMode.Input:
+        if (isEmpty(answerBody)) {
+          errors.push("回答を入力してください");
+        } else {
+          payload.answer_attributes = {
+            body: answerBody,
+            headline: answerHeadline,
+          };
+        }
+        break;
+      case AnswerMode.Select:
+        if (isEmpty(selectedAnswer)) {
+          errors.push("回答を選択してください");
+        } else {
+          payload.answer_id = selectedAnswer.id;
+        }
+        break;
+    }
+
+    if (errors.length === 0) {
+      this.postQuestionAnswer({
+        question_answer: payload
+      });
+    } else {
+      window.alert(errors.join(", "));
+    }
   }
 }
---
+++ spec/javascripts/components/question-answer-form.test.jsx
@@ -0,0 +1,353 @@
+import React from "react";
+import { mount, shallow } from "enzyme";
+import axios from "axios";
+import Promise from "promise";
+import testHelper from "../test-helper";
+
+const componentPath = testHelper.appPath("es2015/components/question-answer-form");
+const panelComponentPath = testHelper.appPath("es2015/components/panel");
+const modelPath = testHelper.appPath("es2015/models/question");
+
+jest.unmock(componentPath);
+jest.unmock(panelComponentPath);
+jest.unmock(modelPath);
+
+const QuestionAnswerForm = require(componentPath).default;
+const { AnswerMode } = require(componentPath);
+const Question = require(modelPath).default;
+
+let fakeFetch;
+let originalFetch = Question.fetch;
+let originalAxiosGet = axios.get;
+
+describe("QuestionAnswerForm", () => {
+  beforeEach(() => {
+    Question.fetch = fakeFetch = jest.fn(() => {
+      return { then: ()=>{} };
+    });
+  });
+
+  afterEach(() => {
+    Question.fetch = originalFetch;
+  })
+
+  describe("when exists id prop", () => {
+    it("requests to question_answers#show.json", () => {
+      const wrapper = mount(<QuestionAnswerForm {...{
+        botId: 1,
+        id: 1,
+      }} />);
+      expect(Question.fetch).toBeCalledWith(1, 1);
+    });
+  });
+
+  describe("when not exists id prop", () => {
+    it("never requests to question_answers#show.json", () => {
+      const wrapper = mount(<QuestionAnswerForm {...{
+        botId: 1,
+        id: null,
+      }} />);
+      expect(Question.fetch).not.toBeCalled();
+    });
+  });
+
+  describe("when input text to question body's textarea", () => {
+    it("calls onChangeQuestionBody and set value to questionBody state", () => {
+      const wrapper = shallow(<QuestionAnswerForm {...{
+        botId: 1,
+        id: 1,
+      }} />);
+
+      expect(wrapper.state("questionBody")).toBe("");
+
+      const fakeEvent = { target: { value: "sample value" } };
+      wrapper.find("#question-body").simulate("change", fakeEvent);
+
+      expect(wrapper.state("questionBody")).toBe("sample value");
+    });
+  });
+
+  describe("when change answer mode", () => {
+    it("assigns new answerMode state", () => {
+      const wrapper = shallow(<QuestionAnswerForm {...{
+        botId: 1,
+        id: 1
+      }} />);
+
+      expect(wrapper.state("answerMode")).toBe("input");
+
+      wrapper.find("#answer-mode").simulate("change", "select");
+
+      expect(wrapper.state("answerMode")).toBe("select");
+    });
+  });
+
+  describe("when answerMode is input", () => {
+    describe("when input text to answer body's textarea", () => {
+      it("assigns new answerBody state", () => {
+        const wrapper = shallow(<QuestionAnswerForm {...{
+          botId: 1,
+          id: 1,
+        }} />)
+
+        const fakeEvent = { target: { value: "sample answer body" } };
+        wrapper.find("#answer-body").simulate("change", fakeEvent);
+
+        expect(wrapper.state("answerBody")).toBe("sample answer body");
+      });
+    });
+  });
+
+  describe("when answerMode is select", () => {
+    describe("when input text to searching answer field", () => {
+      it("searches answers", () => {
+        const wrapper = shallow(<QuestionAnswerForm {...{
+          botId: 1,
+          id: 1,
+        }} />)
+
+        const instance = wrapper.instance()
+        instance.debouncedSearchAnswers = jest.fn();
+
+        wrapper.setState({
+          answerMode: AnswerMode.Select,
+          candidateAnswers: ["sample answer"]
+        });
+
+        const fakeEvent = { target: { value: "sample query" } };
+        wrapper.find("#answer-search").simulate("change", fakeEvent);
+
+        expect(instance.debouncedSearchAnswers).toBeCalledWith("sample query");
+
+        instance.debouncedSearchAnswers = jest.fn();
+
+        const fakeEvent2 = { target: { value: "" } };
+        wrapper.find("#answer-search").simulate("change", fakeEvent2);
+
+        expect(instance.debouncedSearchAnswers).not.toBeCalled();
+        expect(wrapper.state("candidateAnswers")).toEqual([]);
+      });
+    });
+  });
+
+  describe("#searchAnswers", () => {
+    beforeEach(() => {
+      axios.get = jest.fn(() => {
+        return new Promise((resolve) => {
+          resolve({ data: ["sample answer"] });
+        });
+      });
+    });
+
+    afterEach(() => {
+      axios.get = originalAxiosGet;
+    });
+
+    describe("when isProcessing is false", () => {
+      it("does request to search API and set true to isProcessing", () => {
+        const wrapper = shallow(<QuestionAnswerForm {...{
+          botId: 1,
+          id: 1,
+        }} />)
+
+        wrapper.setState({ isProcessing: false, searchingAnswerQuery: "sample" });
+        expect(wrapper.state("candidateAnswers")).toEqual([]);
+
+        return wrapper.instance().searchAnswers().then(() => {
+          expect(axios.get).toBeCalledWith("/bots/1/answers.json", {
+            params: {
+              q: { body_or_headline_cont: "sample"},
+              page: 1,
+            }
+          });
+          expect(wrapper.state("candidateAnswers")).toEqual(["sample answer"]);
+        });
+      });
+    });
+
+    describe("when isProcessing is true", () => {
+      it("does not request to search API", () => {
+        const wrapper = shallow(<QuestionAnswerForm {...{
+          botId: 1,
+          id: 1,
+        }} />);
+
+        wrapper.setState({ isProcessing: true, searchingAnswerQuery: "sample" });
+        expect(wrapper.instance().searchAnswers()).toBeUndefined();
+        expect(axios.get).not.toBeCalled();
+      });
+    });
+  });
+
+  describe("each candidate answers", () => {
+    describe("when click one of candidate answers", () => {
+      it("does assign to selectedAnswer that is clicked one", () => {
+        const wrapper = shallow(<QuestionAnswerForm {...{
+          botId: 1,
+          id: 1,
+        }} />);
+        const candidateAnswers = [
+          { id: 1, body: "target answer" },
+        ];
+        const instance = wrapper.instance();
+        const fakeEvent = { preventDefault: () => {} };
+
+        const originalHandler = instance.onClickCandidateAnswer;
+        instance.onClickCandidateAnswer = jest.fn((answer, e) => {
+          originalHandler.call(instance, answer, e);
+        });
+
+        wrapper.setState({ answerMode: AnswerMode.Select, candidateAnswers });
+        wrapper.find("#candidate-answers").find("Panel")
+          .node.props.onClickBody(fakeEvent);
+
+        expect(instance.onClickCandidateAnswer).toBeCalledWith(
+          candidateAnswers[0], fakeEvent
+        );
+        expect(wrapper.state("selectedAnswer")).toEqual(candidateAnswers[0]);
+        expect(wrapper.state("searchingAnswerQuery")).toBe("");
+      });
+    });
+  });
+
+  describe("selected answer", () => {
+    describe("when click reject button", () => {
+      it("clear selectedAnswer", () => {
+        const wrapper = shallow(<QuestionAnswerForm {...{
+          botId: 1,
+          id: 1,
+        }} />);
+        const selectedAnswer = { id: 123 };
+        const fakeEvent = { preventDefault(){} };
+        wrapper.setState({ answerMode: AnswerMode.Select, selectedAnswer });
+        wrapper.find("#reject-answer").simulate("click", fakeEvent);
+        expect(wrapper.state("selectedAnswer")).toBeNull();
+      });
+    });
+  });
+
+  describe("when input text to answer headline's input", () => {
+    it("calls onChangeAnswerHeadline and assign value to answerHeadline", () => {
+      const wrapper = shallow(<QuestionAnswerForm {...{
+        botId: 1,
+        id: 1,
+      }} />);
+      const fakeEvent = { target: { value: "sample headline" } };
+      const instance = wrapper.instance();
+
+      wrapper.find("#answer-headline").simulate("change", fakeEvent);
+      expect(wrapper.state("answerHeadline")).toBe("sample headline");
+    });
+  });
+
+  describe("when click submit button", () => {
+    describe("when answerMode is 'input'", () => {
+      describe("when empty answerBody", () => {
+        it("does not send POST request", () => {
+          const wrapper = shallow(<QuestionAnswerForm {...{
+            botId: 1,
+            id: 1,
+          }} />);
+          const instance = wrapper.instance();
+          instance.postQuestionAnswer = jest.fn();
+          window.alert = jest.fn();
+          wrapper.setState({
+            questionBody: "sample",
+            answerMode: AnswerMode.Input,
+            answerBody: "",
+          });
+          wrapper.find("input[type='submit']").simulate("click");
+          expect(instance.postQuestionAnswer).not.toBeCalled();
+          expect(window.alert).toBeCalled();
+        });
+      });
+
+      describe("when filled answerBody", () => {
+        it("does send POST request", () => {
+          const wrapper = shallow(<QuestionAnswerForm {...{
+            botId: 1,
+            id: 1,
+          }} />);
+          const instance = wrapper.instance();
+          instance.postQuestionAnswer = jest.fn();
+          window.alert = jest.fn();
+          wrapper.setState({
+            questionBody: "sample",
+            answerMode: AnswerMode.Input,
+            answerBody: "sample",
+          });
+          wrapper.find("input[type='submit']").simulate("click");
+          expect(instance.postQuestionAnswer).toBeCalledWith({
+            question_answer: {
+              question: "sample",
+              answer_attributes: {
+                body: "sample",
+                headline: "",
+              },
+            },
+          });
+          expect(window.alert).not.toBeCalled();
+        });
+      })
+    });
+
+    describe("when answerMode is 'select'", () => {
+      describe("when selected answer", () => {
+        it("does send POST request", () => {
+          const wrapper = shallow(<QuestionAnswerForm {...{
+            botId: 1,
+            id: 1,
+          }} />);
+          const instance = wrapper.instance();
+          instance.postQuestionAnswer = jest.fn();
+          window.alert = jest.fn();
+          wrapper.setState({
+            questionBody: "sample",
+            answerMode: AnswerMode.Select,
+            selectedAnswer: { id: 1 },
+          });
+          wrapper.find("input[type='submit']").simulate("click");
+          expect(instance.postQuestionAnswer).toBeCalledWith({
+            question_answer: {
+              question: "sample",
+              answer_id: 1,
+            },
+          });
+          expect(window.alert).not.toBeCalled();
+        });
+      });
+
+      describe("when not yet selected answer", () => {
+        it("does not send POST request", () => {
+          const wrapper = shallow(<QuestionAnswerForm {...{
+            botId: 1,
+            id: 1,
+          }} />);
+          const instance = wrapper.instance();
+          instance.postQuestionAnswer = jest.fn();
+          window.alert = jest.fn();
+          wrapper.setState({
+            questionBody: "sample",
+            answerMode: AnswerMode.Select,
+            selectedAnswer: null,
+          });
+          wrapper.find("input[type='submit']").simulate("click");
+          expect(instance.postQuestionAnswer).not.toBeCalledWith();
+          expect(window.alert).toBeCalled();
+        });
+      });
+    });
+  });
+
+  describe("#postQuestionAnswer", () => {
+    describe("when exists id", () => {
+      xit("does send request to question_answers#update", () => {
+      });
+    });
+
+    describe("when not exists id", () => {
+      xit("does send request to question_answers#create", () => {
+      });
+    });
+  });
+});

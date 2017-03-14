30063fe2ceb06bcfbe95ded532623557734f8ab6
a.harada <redhornet96@gmail.com>
2017-03-15 01:08:16 +0900

    Reply.predictのテストを実行出来るようにする

--- learning/learning/tests/core/predict/test_reply.py
+++ learning/learning/tests/core/predict/test_reply.py
@@ -1,10 +1,26 @@
-# from unittest import TestCase
-# from nose.tools import ok_, eq_
-#
-# class ReplyTestCase(TestCase):
-#
-#     # TODO 機械学習がからむケースでも単体テストを実行できるようにしたい
-#     def test_predict(self):
-#         # bot_id = 1
-#         # path = Persistance.get_model_path(bot_id)
-#         # eq_(path, 'learning/models/development/1_estimator')
+from unittest import TestCase
+from nose.tools import ok_, eq_
+
+from learning.core.learn.bot import Bot
+from learning.core.predict.reply import Reply
+from learning.tests import helper
+
+class ReplyTestCase(TestCase):
+    BOT_ID = 10001  # engineのテストと重複しないIDを指定
+
+    @classmethod
+    def setUpClass(cls):
+        _ = Bot(cls.BOT_ID, helper.learning_parameter())\
+            .learn(csv_file_path='learning/tests/fixtures/default.csv')
+
+    def test_predict(self):
+        X = ['Windowsにログイン出来ない']
+        reply = Reply(self.BOT_ID, helper.learning_parameter())
+        answers = reply.predict(X)
+
+        # 戻り値にlistが返ること
+        eq_(answers.__class__, list)
+        # answer_idsに1件以上の値が格納されていること
+        ok_(len(reply.answer_ids) > 0)
+        # probabilitiesに1件以上の値が格納されていること
+        ok_(len(reply.probabilities) > 0)
--- learning/learning/tests/helper.py
+++ learning/learning/tests/helper.py
@@ -1,6 +1,16 @@
 import pandas as pd
 from nose.tools import eq_
 
+from learning.core.learn.learning_parameter import LearningParameter
+
+
+def learning_parameter():
+    return LearningParameter({
+        'include_failed_data': False,
+        'include_tag_vector': False,
+        'algorithm': LearningParameter.ALGORITHM_LOGISTIC_REGRESSION
+    })
+
 def build_answers(csv_file_path, encoding='UTF-8'):
     learning_training_messages = pd.read_csv(csv_file_path, encoding=encoding)
     return learning_training_messages.drop_duplicates(subset=['answer_id'])

da3d2fa17bba86af60032b3c1435ea66be98017b
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-03-21 16:41:08 +0900

    DefinedAnswerがブランク時の制御を追加

--- db/migrate/20170313140847_answer_failed_to_zero.rb
+++ db/migrate/20170313140847_answer_failed_to_zero.rb
@@ -1,6 +1,7 @@
 class AnswerFailedToZero < ActiveRecord::Migration
   def change
     defined_answer = DefinedAnswer.classify_failed
+    return if defined_answer.blank?
     ActiveRecord::Base.transaction do
       DecisionBranch.where(answer_id: defined_answer.id).update_all(answer_id: Answer::NO_CLASSIFIED_ID)
       DecisionBranch.where(next_answer_id: defined_answer.id).update_all(next_answer_id: Answer::NO_CLASSIFIED_ID)

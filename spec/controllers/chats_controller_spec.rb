128fb6c39e3bb2a8fe77c1c4be2e77557f521d90
kozo yamagata <tune002@gmail.com>
2017-03-22 15:33:33 +0900

    Add spec of ChatsController

---
+++ spec/controllers/chats_controller_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe ChatsController do
+  let!(:bot) do
+    create(:bot, user: owner)
+  end
+
+  let!(:owner) do
+    create(:user)
+  end
+
+  let!(:chat) do
+    create(:chat, bot: bot, guest_key: guest_key)
+  end
+
+  let(:guest_key) do
+    'sample guest key'
+  end
+
+  describe 'GET #show' do
+    before do
+      sign_in owner
+    end
+
+    subject do
+      get :show, { token: bot.token }, { guest_key: session_guest_key }
+    end
+
+    context 'has guest_key' do
+      let(:session_guest_key) do
+        guest_key
+      end
+
+      it 'renders chat' do
+        expect(subject).to render_template(:show)
+      end
+    end
+
+    context 'has not guest_key' do
+      let(:session_guest_key) do
+        nil
+      end
+
+      it 'redirects to new' do
+        expect(subject).to redirect_to(new_chats_path)
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    subject do
+      get :new, { token: bot.token }
+    end
+
+    it 'creates new chat' do
+      expect{ subject }.to change(Chat, :count).by(1)
+    end
+
+    it 'renders show' do
+      expect(subject).to render_template(:show)
+    end
+  end
+end

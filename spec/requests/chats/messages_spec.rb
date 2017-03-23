d975c7234eb302fc3b63386ad692c798a8f1450b
kozo yamagata <tune002@gmail.com>
2017-03-23 16:22:08 +0900

    Update spec/requests/chats/messages

--- spec/requests/chats/messages_spec.rb
+++ spec/requests/chats/messages_spec.rb
@@ -10,7 +10,15 @@ RSpec.describe 'chats/messages', type: :request do
   end
 
   let!(:answer) do
-    create(:answer)
+    create(:answer, bot: bot)
+  end
+
+  let!(:decision_branches) do
+    create_list(:decision_branch, 2, answer: answer, bot: bot)
+  end
+
+  let!(:next_answer) do
+    create(:answer, bot: bot)
   end
 
   let(:chat_path) do
@@ -28,12 +36,16 @@ RSpec.describe 'chats/messages', type: :request do
         m.answer = answer
         m.save
       end
+      decision_branches.tapp.first.tap do |db|
+        db.next_answer = next_answer
+        db.save
+      end
     end
 
     context 'when json format' do
       it 'returns json data' do
         get "#{chat_path}/messages.json"
-        expect(res_json['messages'].is_a?(Array)).to be
+        expect(res_json.tapp['messages'].is_a?(Array)).to be
         expect(res_json['messages'].first['answer']['id']).to be
         expect(res_json['meta']['totalPages']).to be_present
       end

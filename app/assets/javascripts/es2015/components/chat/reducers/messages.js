8afcfde5740cae83b30838b6259a8e6ed5a7ae92
kozo yamagata <tune002@gmail.com>
2017-03-23 16:22:25 +0900

    Update chat/reducers/messages

--- app/assets/javascripts/es2015/components/chat/reducers/messages.js
+++ app/assets/javascripts/es2015/components/chat/reducers/messages.js
@@ -1,11 +1,51 @@
 import assign from "lodash/assign";
+import chunk from "lodash/chunk";
+import get from "lodash/get";
+import cloneDeep from "lodash/cloneDeep";
+import last from "lodash/last";
+import pick from "lodash/pick";
+import isEmpty from "is-empty";
+import Promise from "promise";
 import { handleActions } from "redux-actions";
 import { fetchMessages } from "../action-creators";
 
-const initialState = {
-  data: [],
-  totalPages: 0,
+const Speaker = {
+  Bot: "bot",
+  Guest: "guest",
 };
+
+export function classify(data, messages) {
+  let sections = cloneDeep(data);
+  if (data.length === 0) {
+    sections.push({ answer: messages.shift() });
+  }
+
+  messages.forEach((message) => {
+    switch(message.speaker) {
+      case Speaker.Guest:
+        sections.push({ question: message });
+        break;
+      case Speaker.Bot:
+        sections = classifyBotMessage(sections, message);
+        break;
+    }
+  });
+
+  return sections;
+}
+
+export function classifyBotMessage(sections, message) {
+  let secs = cloneDeep(sections);
+  let lastSec = last(secs);
+  lastSec.answer = pick(message, ["id", "body", "createdAt", "rating"]);
+  secs[secs.length - 1] = lastSec;
+
+  const decisionBranches = get(message, "answer.decisionBranches");
+  if (!isEmpty(decisionBranches)) {
+    secs.push({ decisionBranches });
+  }
+  return secs;
+}
 
 export default handleActions({
   [fetchMessages]: (state, action) => {
@@ -16,9 +56,13 @@ export default handleActions({
       return state;
     }
 
-    return assign({}, state, {
-      data: state.data.concat(payload.data.messages),
-      totalPages: payload.data.total_pages,
-    });
+    const { messages, meta } = payload.data;
+    const data = state.data.concat(messages);
+    const classifiedData = classify(state.classifiedData, messages);
+    return assign({}, state, { data, classifiedData, meta });
   },
-}, initialState);
+}, {
+  data: [],
+  classifiedData: [],
+  meta: {},
+});
--- spec/javascripts/components/chat/reducers/messages.test.js
+++ spec/javascripts/components/chat/reducers/messages.test.js
@@ -9,22 +9,64 @@ jest.unmock(reducerPath);
 jest.unmock(actionCreatorPath);
 
 const messages = require(reducerPath).default;
+const { classify, classifyBotMessage } = require(reducerPath);
 const { fetchMessages } = require(actionCreatorPath);
+
+describe("classifyBotMessage", () => {
+  it("assign answer to last section", () => {
+    const fakeSection = [{ question: "fake" }];
+    const fakeMessage = { body: "fake" };
+    const result = classifyBotMessage(fakeSection, fakeMessage);
+    expect(result).toEqual([
+      { question: "fake", answer: { body: "fake"} }
+    ]);
+  });
+
+  describe("when has decisionBranches", () => {
+    it("assign decisionBranches to next section", () => {
+      const fakeSection = [{ question: "fake" }];
+      const fakeMessage = {
+        body: "fake",
+        answer: {
+          decisionBranches: "fake"
+        }
+      };
+      const result = classifyBotMessage(fakeSection, fakeMessage);
+      expect(result).toEqual([
+        { question: "fake", answer: { body: "fake" } },
+        { decisionBranches: "fake" }
+      ]);
+    });
+  });
+});
 
 describe("chat/reducers/messages", () => {
   describe("fetchMessages", () => {
     describe("when succeeded", () => {
       it("returns merged state", () => {
-        const fakeResponse = { data: { messages: [2, 3], total_pages: 10 } };
+        const fakeResponse = {
+          data: {
+            messages: [2, 3],
+            meta: {
+              totalPages: 10
+            }
+          }
+        };
         const fakeAction = {
           error: false,
           type: "FETCH_MESSAGES",
           payload: fakeResponse,
         };
-        const state = messages({ data: [1] }, fakeAction);
+        const state = messages({ data: [1], classifiedData: [] }, fakeAction);
+
         expect(state).toEqual({
           data: [1, 2, 3],
-          totalPages: 10
+          classifiedData: [
+            { answer: 2 },
+          ],
+          meta: {
+            totalPages: 10
+          }
         });
       });
     });

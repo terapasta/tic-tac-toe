58eb0824c901ff7af1cae0e9b2fce264f25e82af
kozo yamagata <tune002@gmail.com>
2017-03-23 18:51:53 +0900

    Add icon_image_url attribute to MessageSerializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,4 +1,13 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :speaker, :rating, :created_at, :body
+  attributes :id, :speaker, :rating, :created_at, :body, :icon_image_url
   has_one :answer
+
+  def icon_image_url
+    case object.speaker
+    when 'guest'
+      ActionController::Base.helpers.asset_path('silhouette.png')
+    when 'bot'
+      object.chat.bot.image_url(:thumb)
+    end
+  end
 end

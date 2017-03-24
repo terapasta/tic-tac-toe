c2c343c2cc785e9b3c9b5cd2adf544965817d19b
kozo yamagata <tune002@gmail.com>
2017-03-24 18:51:42 +0900

    Add postMessage action creator

--- app/assets/javascripts/es2015/components/chat/action-creators.js
+++ app/assets/javascripts/es2015/components/chat/action-creators.js
@@ -1,6 +1,20 @@
 import { createAction } from "redux-actions";
+import trim from "lodash/trim";
+import isEmpty from "is-empty";
+import toastr from "toastr";
 import * as API from "../../api/chat-messages";
 
 export const fetchMessages = createAction("FETCH_MESSAGES", API.fetchMessages);
+
+export const postMessage = createAction("POST_MESSAGE", (token, messageBody) => {
+  return new Promise((resolve, reject) => {
+    const m = trim(messageBody);
+    if (isEmpty(m)) {
+      reject(toastr.warning("メッセージを入力してください"));
+    } else {
+      resolve(API.postMessage(token, messageBody));
+    }
+  });
+});
 
 export const changeMessageBody = createAction("CHANGE_MESSAGE_BODY");

906d2a4f85386daa62940c9d64fe520941ea7be5
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-03-24 14:51:33 +0900

    worker作業画面のfeatureテスト修正

--- spec/features/worker_spec.rb
+++ spec/features/worker_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
-feature 'worker作業画面の確認', js: true do
+feature 'worker作業画面の確認' do
+  include RequestSpecHelper
+
   let!(:user) do
     create(:user, role: :worker)
   end
@@ -15,28 +17,32 @@ feature 'worker作業画面の確認', js: true do
 
   background do
     visit new_user_session_path
-    fill_in 'Eメール', with: user.email
-    fill_in 'パスワード', with: 'hogehoge'
-    click_on 'ログイン'
+    sign_in user
   end
 
-  scenario 'スキップした場合' do
-    visit new_imported_sentence_synonym_path
-    find('#cancel').click
-    expect(current_path).to eq '/imported_sentence_synonyms/new'
+  context 'スキップした場合' do
+    scenario 'display same page' do
+      visit new_imported_sentence_synonym_path
+      find('#cancel').click
+      expect(current_path).to eq '/imported_sentence_synonyms/new'
+    end
   end
 
-  scenario '同義文がブランクの場合の登録' do
-    visit new_imported_sentence_synonym_path
-    find('#submit').click
-    expect(current_path).to eq '/imported_sentence_synonyms/new'
+  context '同義文がブランクの場合の登録' do
+    scenario 'Message "登録しました。" is displayed' do
+      visit new_imported_sentence_synonym_path
+      find('#submit').click
+      expect(current_path).to eq '/imported_sentence_synonyms/new'
+      expect(page). to have_content '登録しました。'
+    end
   end
 
-  scenario '同義文が入力してある場合の登録' do
-    visit new_imported_sentence_synonym_path
-    fill_in 'question_answer_sentence_synonyms_attributes_0_body', with: 'test'
-    find('#submit').click
-    expect(page). to have_content '登録しました。'
+  context '同義文が入力してある場合の登録' do
+    scenario 'Message "登録しました。" is displayed' do
+      visit new_imported_sentence_synonym_path
+      fill_in 'question_answer_sentence_synonyms_attributes_0_body', with: 'test'
+      find('#submit').click
+      expect(page). to have_content '登録しました。'
+    end
   end
-
 end

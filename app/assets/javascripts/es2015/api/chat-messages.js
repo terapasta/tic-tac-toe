efd3ac6f564e21d32eaf692c4dc653b06d0bcb52
kozo yamagata <tune002@gmail.com>
2017-03-24 18:48:46 +0900

    Add postMessage function

--- app/assets/javascripts/es2015/api/chat-messages.js
+++ app/assets/javascripts/es2015/api/chat-messages.js
@@ -1,5 +1,14 @@
 import axios from "axios";
+import config from "./config"
 
 export function fetchMessages(token, page = 1) {
   return axios.get(`/embed/${token}/chats/messages.json`);
+}
+
+export function postMessage(token, messageBody) {
+  return axios.post(`/embed/${token}/chats/messages.json`, {
+    message: {
+      body: messageBody,
+    }
+  }, config());
 }

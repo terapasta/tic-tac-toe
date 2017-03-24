0c4e8e8f61a8824e34639d83917b7f4640c4761c
kozo yamagata <tune002@gmail.com>
2017-03-24 14:38:54 +0900

    Install react-loading and use in message components

--- app/assets/javascripts/es2015/components/chat/bot-message.jsx
+++ app/assets/javascripts/es2015/components/chat/bot-message.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from "react";
+import Loading from "react-loading";
 
 import MessageRatingButtons from "./message-rating-buttons";
 
@@ -10,6 +11,7 @@ export default class ChatBotMessage extends Component {
   render() {
     const {
       isFirst,
+      isLoading,
       iconImageUrl,
       rating,
       body,
@@ -23,10 +25,15 @@ export default class ChatBotMessage extends Component {
       <div className="chat-message">
         <div className="chat-message__icon" style={iconStyle} />
         <div className="chat-message__balloon">
-          {body}
+          {!isLoading && body}
+          {isLoading && (
+            <div className="chat-message__balloon-loader">
+              <Loading type="spin" color="#e3e3e3" height={32} width={32} />
+            </div>
+          )}
         </div>
         <div className="chat-message__rating">
-          {!isFirst && (
+          {!isLoading && !isFirst && (
             <MessageRatingButtons {...{
               rating,
             }} />
--- app/assets/javascripts/es2015/components/chat/guest-message.jsx
+++ app/assets/javascripts/es2015/components/chat/guest-message.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from "react";
+import Loading from "react-loading";
 
 export default class ChatGuestMessage extends Component {
   static get propTypes() {
@@ -7,6 +8,7 @@ export default class ChatGuestMessage extends Component {
 
   render() {
     const {
+      isLoading,
       iconImageUrl,
       body,
     } = this.props;
@@ -18,7 +20,12 @@ export default class ChatGuestMessage extends Component {
     return (
       <div className="chat-message--my">
         <div className="chat-message__balloon">
-          {body}
+          {!isLoading && body}
+          {isLoading && (
+            <div className="chat-message__balloon-loader">
+              <Loading type="spin" color="#e3e3e3" height={32} width={32} />
+            </div>
+          )}
         </div>
         <div className="chat-message__icon" style={iconStyle} />
       </div>

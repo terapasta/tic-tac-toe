5485adbe016a555755f2dadacd0f830b6f913700
kozo yamagata <tune002@gmail.com>
2017-03-28 15:57:53 +0900

    Fix syntax error

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,7 +2,7 @@ class Chat < ActiveRecord::Base
   include ContextHoldable
   paginates_per 50
 
-  has_many :messages,
+  has_many :messages
   has_many :contact_states
   belongs_to :bot
   enum context: ContextHoldable::CONTEXTS

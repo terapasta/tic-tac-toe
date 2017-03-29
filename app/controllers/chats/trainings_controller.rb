4fab2093133d7bf361868576ad37d3ef02413854
kozo yamagata <tune002@gmail.com>
2017-03-29 19:00:11 +0900

    Implement chats/trainings#create action

---
+++ app/controllers/chats/trainings_controller.rb
@@ -0,0 +1,29 @@
+class Chats::TrainingsController < ApplicationController
+  before_action :set_bot_chat_messages
+
+  def create
+    ActiveRecord::Base.transaction do
+      @question_answer = @bot.question_answers.create!(question_answer_params)
+      @question_message.update!(body: @question_answer.question)
+      @answer_message.update!(body: @question_answer.answer.body, answer_id: @question_answer.answer.id)
+    end
+    render json: @question_answer, adapter: :json
+  rescue => e
+    logger.error e.message + e.backtrace.join("\n")
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  private
+    def set_bot_chat_messages
+      @bot = Bot.find_by!(token: params[:token])
+      @chat = @bot.chats.where(guest_key: session[:guest_key]).last
+      @question_message = @chat.messages.find(params[:question_message_id])
+      @answer_message = @chat.messages.find(params[:answer_message_id])
+    end
+
+    def question_answer_params
+      permitted_attributes(QuestionAnswer).tap do |prm|
+        prm[:answer_attributes][:bot_id] = @bot.id
+      end
+    end
+end

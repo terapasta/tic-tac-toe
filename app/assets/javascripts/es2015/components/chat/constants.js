9dfeb3a65ae231b803f7cda199d0603643e8a35d
kozo yamagata <tune002@gmail.com>
2017-03-29 19:02:44 +0900

    Add SucceededTraining to chat/constants

--- app/assets/javascripts/es2015/components/chat/constants.js
+++ app/assets/javascripts/es2015/components/chat/constants.js
@@ -1,6 +1,7 @@
 export const ErrorTitle = "エラー";
 export const ErrorPostMessage = "メッセージを入力してください";
 export const ErrorCreateMessage = "質問を送信できませんでした。ご迷惑おかけして申し訳ありません。";
+export const SucceededTraining = "学習データに登録しました";
 
 export const Ratings = {
   Nothing: "nothing",

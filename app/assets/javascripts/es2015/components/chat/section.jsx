a0ecdc5ec1effcf8dbde6da007b65747a5596ec0
kozo yamagata <tune002@gmail.com>
2017-03-30 12:15:54 +0900

    Rewrite ChatSection component to class

--- app/assets/javascripts/es2015/components/chat/section.jsx
+++ app/assets/javascripts/es2015/components/chat/section.jsx
@@ -4,92 +4,109 @@ import get from "lodash/get";
 import classNames from "classnames";
 import isEmpty from "is-empty";
 
-function ChatSection(props) {
-  const {
-    children,
-    isManager,
-    isFirst,
-    isActive,
-    isDisabled,
-    onClick,
-    section,
-    learnings,
-    index,
-    onSaveLearning,
-  } = props;
+export default class ChatSection extends Component {
+  static get propTypes() {
+    return {
+      isManager: PropTypes.bool.isRequired,
+      isFirst: PropTypes.bool.isRequired,
+      isActive: PropTypes.bool,
+      isDisabled: PropTypes.bool,
+      section: PropTypes.object.isRequired,
+      index: PropTypes.number.isRequired,
+      onClick: PropTypes.func.isRequired,
+      onSaveLearning: PropTypes.func.isRequired,
+    };
+  }
 
-  const { decisionBranches, isDone, question, answer } = section;
-  const questionId = get(question, "id");
-  const answerId = get(answer, "id");
-  const learning = find(learnings, { questionId, answerId });
-  const isDecisionBranch = !isEmpty(decisionBranches);
-  if (isDecisionBranch && isDone) { return null; }
+  get learning() {
+    const { learnings } = this.props;
+    const { questionId, answerId } = this;
+    return find(learnings, { questionId, answerId }) || {};
+  }
 
-  const className = classNames({
-    "chat-section": !isManager,
-    "chat-section--bordered": isManager,
-    "active": isActive,
-  });
+  get questionId() {
+    return get(this.props, "section.question.id");
+  }
 
-  const onClickWrapper = (e) => {
+  get answerId() {
+    return get(this.props, "section.answer.id");
+  }
+
+  render() {
+    const {
+      children,
+      isManager,
+      isFirst,
+      isActive,
+      isDisabled,
+      onClick,
+      section,
+      index,
+      onSaveLearning,
+    } = this.props;
+
+    const { decisionBranches, isDone, question, answer } = section;
+    const isDecisionBranch = !isEmpty(decisionBranches);
+    if (isDecisionBranch && isDone) { return null; }
+
+    const className = classNames({
+      "chat-section": !isManager,
+      "chat-section--bordered": isManager,
+      "active": isActive,
+    });
+
+    return (
+      <div className={className}>
+        <div className="chat-section__switch-container">
+          {!isFirst && !isDecisionBranch && (
+            <a href="#"
+              className="chat-section__switch"
+              onClick={this.onClick.bind(this)}
+              disabled={get(this.learning, "isDisabled")}>
+              <i className="material-icons">school</i>
+              {!isActive && (
+                <div className="chat-section__tooltip">
+                  回答が正しくない場合、クリックして別の回答を教えられます
+                </div>
+              )}
+            </a>
+          )}
+        </div>
+        {children}
+        {isActive && (
+          <div className="chat-section__actions">
+            <a className="btn btn-link btn-xs-sm"
+              href="#"
+              onClick={this.onClick.bind(this)}
+              disabled={this.learning.isDisabled}>キャンセル</a>
+            <a className="btn btn-primary btn-xs-sm"
+              href="#"
+              onClick={this.onSaveLearning.bind(this)}
+              disabled={this.learning.isDisabled}>この内容で教える</a>
+          </div>
+        )}
+        {isDisabled && (
+          <div className="chat-section__disable-cover" />
+        )}
+      </div>
+    );
+  }
+
+  onClick(e) {
     e.preventDefault();
-    if (get(learning, "isDisabled")) { return; }
+    const { onClick, index } = this.props;
+    if (get(this.learning, "isDisabled")) { return; }
     onClick(index);
-  };
+  }
 
-  const onSaveLearningWrapper = (e) => {
+  onSaveLearning(e) {
     e.preventDefault();
-    if (learning.isDisabled) { return; }
-    onSaveLearning({
+    const { onSaveLearning } = this.props;
+    const { questionId, answerId } = this;
+    if (get(this.learning, "isDisabled")) { return; }
+    this.props.onSaveLearning({
       questionId,
       answerId,
     });
-  };
-
-  return (
-    <div className={className}>
-      <div className="chat-section__switch-container">
-        {!isFirst && !isDecisionBranch && (
-          <a href="#"
-            className="chat-section__switch"
-            onClick={onClickWrapper}
-            disabled={get(learning, "isDisabled")}>
-            <i className="material-icons">school</i>
-            {!isActive && (
-              <div className="chat-section__tooltip">
-                回答が正しくない場合、クリックして別の回答を教えられます
-              </div>
-            )}
-          </a>
-        )}
-      </div>
-      {children}
-      {isActive && (
-        <div className="chat-section__actions">
-          <a className="btn btn-link btn-xs-sm"
-            href="#"
-            onClick={onClickWrapper}
-            disabled={learning.isDisabled}>キャンセル</a>
-          <a className="btn btn-primary btn-xs-sm"
-            href="#"
-            onClick={onSaveLearningWrapper}
-            disabled={learning.isDisabled}>この内容で教える</a>
-        </div>
-      )}
-      {isDisabled && (
-        <div className="chat-section__disable-cover" />
-      )}
-    </div>
-  );
+  }
 }
-
-ChatSection.propTypes = {
-  isManager: PropTypes.bool.isRequired,
-  isFirst: PropTypes.bool.isRequired,
-  isActive: PropTypes.bool,
-  isDisabled: PropTypes.bool,
-  section: PropTypes.object.isRequired,
-  onSaveLearning: PropTypes.func.isRequired,
-};
-
-export default ChatSection;

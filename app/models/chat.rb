20e73f1d7f0f43af3fdb9291996631f1ebb6c278
Daisuke Iwasaki <d.iwasaki.g@gmail.com>
2017-03-31 18:05:24 +0900

    対話一覧のフィルタに注意回答がある対話を追加

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -40,6 +40,12 @@ class Chat < ActiveRecord::Base
     end
   }
 
+  scope :has_answer_marked, -> (flag) {
+    if flag.present?
+      where(id: Message.select(:chat_id).answer_marked)
+    end
+  }
+
   def build_start_message
     body = bot.start_message.presence || DefinedAnswer.start_answer_unsetting.body
     Message.new(speaker: 'bot', answer_id: nil, body: body)

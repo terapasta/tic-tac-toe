bef595b67aa051e04b423b79d88c721cd50c9fe6
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-04-05 16:57:59 +0900

    TopicTagモデルテスト

--- spec/models/topic_tag_spec.rb
+++ spec/models/topic_tag_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe TopicTag, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe TopicTag do
+  include RequestSpecHelper
+
+  let!(:user) do
+    create(:user, role: :worker)
+  end
+
+  let!(:bot) do
+    create(:bot, user: user)
+  end
+
+  context 'topicTagモデルテスト' do
+    it 'should be valid' do
+      topic_tag = TopicTag.new(name: 'テスト', bot_id: 1)
+      expect(topic_tag).to be_valid
+    end
+  end
 end

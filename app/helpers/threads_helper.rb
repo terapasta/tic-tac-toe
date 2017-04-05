3fbaad19e7cfb50c9480c2f6067bec0a02414f23
Daisuke Iwasaki <d.iwasaki.g@gmail.com>
2017-04-06 07:10:15 +0900

    コードフォーマットの修正

--- app/helpers/threads_helper.rb
+++ app/helpers/threads_helper.rb
@@ -1,10 +1,10 @@
 module ThreadsHelper
-  def role_filter_params(param={})
+  def role_filter_params(param = {})
     params = message_params
     params.merge param
   end
 
-  def message_filter_params(param={})
+  def message_filter_params(param = {})
     params = role_params
     params.merge param
   end

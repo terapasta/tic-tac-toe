b64952c625b47e08b4b816db06e9a0681582cdab
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-04-10 17:25:22 +0900

    Q&Aトピックfeatureテスト レビュー修正

--- spec/factories/topic_tags.rb
+++ spec/factories/topic_tags.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :topic_tag do
-    
+    name 'hogehoge'
+    bot_id 1
   end
 end
--- spec/features/topic_tag_spec.rb
+++ spec/features/topic_tag_spec.rb
@@ -15,11 +15,15 @@ feature 'Q&Aトピックの検索テスト' do
     create(:bot, user: user)
   end
 
+  let(:topic_tag_attrs) do
+    attributes_for(:topic_tag)
+  end
+
   background do
     sign_in user
   end
 
-  context 'Q&Aトピック画面テスト' do
+  describe 'Crud test of TopicTag' do
     scenario 'display Q&A topic' do
       visit bot_topic_tags_path(bot)
     end
@@ -27,8 +31,8 @@ feature 'Q&Aトピックの検索テスト' do
     scenario 'Message "登録しました。" is displayed' do
       visit bot_topic_tags_path(bot)
       click_on '登録する'
-      fill_in 'topic_tag_name', with: 'トピック登録'
-      click_on '登録する'
+      fill_in 'topic_tag_name', with: topic_tag_attrs[:name]
+      expect{ click_on '登録する' }.to change(TopicTag, :count).by(1)
       expect(page).to have_content '登録しました'
     end
 
@@ -42,27 +46,29 @@ feature 'Q&Aトピックの検索テスト' do
     scenario 'Message "削除しました。" is displayed' do
       visit bot_topic_tags_path(bot)
       click_on '登録する'
-      fill_in 'topic_tag_name', with: 'トピック登録'
+      fill_in 'topic_tag_name', with: topic_tag_attrs[:name]
       click_on '登録する'
-      click_on '削除'
+      expect{ click_on '削除' }.to change(TopicTag, :count).by(-1)
       expect(page).to have_content '削除しました。'
     end
 
     scenario 'Message "更新しました。" is displayed'  do
       visit bot_topic_tags_path(bot)
       click_on '登録する'
-      fill_in 'topic_tag_name', with: 'トピック登録'
+      fill_in 'topic_tag_name', with: topic_tag_attrs[:name]
       click_on '登録する'
       click_on '編集'
-      fill_in 'topic_tag_name', with: 'トピック編集'
+      fill_in 'topic_tag_name', with: 'fugafuga'
       click_on '更新する'
-      expect(page).to have_content '更新しました。'
+      expect(page).to have_content '更新しました'
+      updated_topic_tag = TopicTag.last
+      expect(topic_tag_attrs[:name]).not_to eq updated_topic_tag.name
     end
 
     scenario 'Message "更新できませんでした。" is displayed'  do
       visit bot_topic_tags_path(bot)
       click_on '登録する'
-      fill_in 'topic_tag_name', with: 'トピック登録'
+      fill_in 'topic_tag_name', with: topic_tag_attrs[:name]
       click_on '登録する'
       click_on '編集'
       fill_in 'topic_tag_name', with: ''

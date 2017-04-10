c6dc4a152295de60a4156c49f4ac4430046806b6
kozo yamagata <tune002@gmail.com>
2017-04-10 16:21:30 +0900

    Modify root container div’s class name to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def need_side_nav?
     !chat_page? && policy(:static_pages).can_use_nav?(@bot)
   end
+
+  def root_container_class
+    "container container-main".tap do |result|
+      if need_side_nav?
+        result << " col-sm-10 col-sm-offset-2"
+      end
+    end
+  end
 end

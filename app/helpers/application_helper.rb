9b9f32dc7fd983facae2c761366d5eda65e056bd
kozo yamagata <tune002@gmail.com>
2017-04-10 16:19:59 +0900

    Separate side nav to partial view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def space(length = 1)
     ('&nbsp;' * length).html_safe
   end
+
+  def need_side_nav?
+    !chat_page? && policy(:static_pages).can_use_nav?(@bot)
+  end
 end

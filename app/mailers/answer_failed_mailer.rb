dee310cf04907d0f5f4a0ff2d188dd6eff20e0b7
Daisuke Iwasaki <d.iwasaki.g@gmail.com>
2017-04-15 14:04:35 +0900

    chatモデルからbot管理者ユーザーをする

--- app/mailers/answer_failed_mailer.rb
+++ app/mailers/answer_failed_mailer.rb
@@ -2,7 +2,7 @@ class AnswerFailedMailer < ApplicationMailer
   default from: 'noreply@mof-mof.co.jp'
 
   def create(message)
-    @bot_user = message.chat.bot.user
+    @bot_user = message.chat.bot_user
     @bot_message = message
     @guest_message = Chat.question_message(message.chat_id, message.id)
     mail(to: @bot_user.email, subject: '[My-ope office] チャットで回答に失敗しました')

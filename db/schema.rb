05424e649f6a7dff9ee84fff08a56e13245ea99c
a.harada <redhornet96@gmail.com>
2017-04-15 14:50:40 +0900

    learning_parametersにuse_similarity_classificationを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402073948) do
+ActiveRecord::Schema.define(version: 20170415054829) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -152,14 +152,15 @@ ActiveRecord::Schema.define(version: 20170402073948) do
   end
 
   create_table "learning_parameters", force: :cascade do |t|
-    t.integer  "bot_id",               limit: 4
-    t.integer  "algorithm",            limit: 4,     default: 0,     null: false
-    t.text     "params_for_algorithm", limit: 65535
-    t.boolean  "include_failed_data",                default: false, null: false
-    t.boolean  "include_tag_vector",                 default: false, null: false
-    t.float    "classify_threshold",   limit: 24,    default: 0.5,   null: false
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
+    t.integer  "bot_id",                        limit: 4
+    t.integer  "algorithm",                     limit: 4,     default: 0,     null: false
+    t.text     "params_for_algorithm",          limit: 65535
+    t.boolean  "include_failed_data",                         default: false, null: false
+    t.boolean  "include_tag_vector",                          default: false, null: false
+    t.float    "classify_threshold",            limit: 24,    default: 0.5,   null: false
+    t.boolean  "use_similarity_classification",               default: false, null: false
+    t.datetime "created_at",                                                  null: false
+    t.datetime "updated_at",                                                  null: false
   end
 
   add_index "learning_parameters", ["bot_id"], name: "index_learning_parameters_on_bot_id", using: :btree

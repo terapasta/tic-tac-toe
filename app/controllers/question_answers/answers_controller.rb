2eb898cdadd4cae88e0956e076ebbde1d3186ed6
kozo yamagata <tune002@gmail.com>
2017-04-17 15:26:51 +0900

    Modify to return 404 response with json format

--- app/controllers/question_answers/answers_controller.rb
+++ app/controllers/question_answers/answers_controller.rb
@@ -6,9 +6,12 @@ class QuestionAnswers::AnswersController < ApplicationController
     @bot = bots.find(params[:bot_id])
     @question_answer = @bot.question_answers.find(params[:question_answer_id])
     @answer = @question_answer.answer
-    fail ActionController::RoutingError.new("Has not answer") if @answer.nil?
     respond_to do |format|
-      format.json { render json: @answer.decorate.as_json, status: :ok }
+      if @answer.present?
+        format.json { render json: @answer.decorate.as_json, status: :ok }
+      else
+        format.json { render json: {}, status: :not_found }
+      end
     end
   end
 end

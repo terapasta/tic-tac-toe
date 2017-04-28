92ce4c5a0589333bd32fe4273a18e4b01bc6e667
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-04-28 17:44:51 +0900

    ストロングパラメータの削除とalertの記述方法の修正

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,21 +6,19 @@ class ContactsController < ApplicationController
 
   def create
     client = ZendeskClient.shared_client
-    ticket = ZendeskAPI::Ticket.new(client, requester: {email: (ticket_params[:email])},
-      subject:(ticket_params[:subject]),
-      description: (ticket_params[:description]))
+    ticket = ZendeskAPI::Ticket.new(client,
+      requester:   { email: params[:ticket][:email] },
+      subject:     params[:ticket][:subject],
+      description: params[:ticket][:description]
+    )
     if ticket.save
       redirect_to root_path, notice: 'お問い合わせが完了しました。'
     else
-      redirect_to new_contacts_path,
-        alert: 'お問い合わせに失敗しました。内容を確認してください。
-                * メールアドレスは入力されましたか?
-                * お問い合わせ内容は入力されましたか?'
+      redirect_to new_contacts_path, alert: <<~MSG
+        お問い合わせに失敗しました。内容を確認してください。
+        ※メールアドレスは入力されましたか?
+        ※お問い合わせ内容は入力されましたか?
+      MSG
     end
   end
-
-  private
-    def ticket_params
-      params.require(:ticket).permit(:email, :subject, :description)
-    end
 end

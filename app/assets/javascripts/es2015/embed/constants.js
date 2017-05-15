fb7296d0bfa65fc27e9a9bc6b522ee6d1fbde673
kozo yamagata <tune002@gmail.com>
2017-05-15 21:33:17 +0900

    Re-implement chat widget with react

---
+++ app/assets/javascripts/es2015/embed/constants.js
@@ -0,0 +1,11 @@
+export const Width = 300;
+export const Height = 325;
+export const WidthMobileHidden = 37;
+export const Margin = 16;
+export const HeaderHeight = 30;
+export const HeaderBGColor = "#F36B30";
+export const HeaderBGColorHover = "#F38657";
+export const MobileMaxWidth = 767;
+export const Namespace = "MyOpeChatWidget";
+export const EmbedScriptId = "MyOpeEmbedScript";
+export const HiddenBottomValue = Height - HeaderHeight;

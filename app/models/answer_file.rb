080f13f426045d906532ce3996569a50e6fc5e2c
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-05-19 16:51:21 +0900

    モデルとテーブル作成

---
+++ app/models/answer_file.rb
@@ -0,0 +1,8 @@
+class AnswerFile < ActiveRecord::Base
+  belongs_to :answer
+
+  validates :answer_id, presence: true
+  validates :file, presence: true
+  
+  mount_uploader :file, AnswerFileUploader
+end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20170524054315) do
 
   add_index "allowed_hosts", ["scheme", "domain", "bot_id"], name: "index_allowed_hosts_on_scheme_and_domain_and_bot_id", unique: true, using: :btree
 
+  create_table "answer_files", force: :cascade do |t|
+    t.integer  "answer_id",  limit: 4,   null: false
+    t.string   "file",       limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "answers", force: :cascade do |t|
     t.integer  "defined_answer_id", limit: 4
     t.string   "context",           limit: 255,   default: "0", null: false
@@ -90,6 +97,14 @@ ActiveRecord::Schema.define(version: 20170524054315) do
   end
 
   add_index "contact_states", ["chat_id"], name: "index_contact_states_on_chat_id", using: :btree
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "email",      limit: 255,   null: false
+    t.string   "subject",    limit: 255,   null: false
+    t.text     "content",    limit: 65535, null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "contexts", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false

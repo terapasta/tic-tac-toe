dbfd50764f05fb7b3385b00da12835864367ad81
kozo yamagata <tune002@gmail.com>
2017-05-23 16:20:36 +0900

    Add validation to WordMapping

--- spec/models/word_mapping_spec.rb
+++ spec/models/word_mapping_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe WordMapping, type: :model do
+  describe 'validations' do
+    context 'word does already exists as synonym' do
+      let!(:other_word_mapping) do
+        create(:word_mapping)
+      end
+
+      let!(:word_mapping) do
+        build(:word_mapping, word: other_word_mapping.synonym)
+      end
+
+      it 'invalid' do
+        expect(word_mapping.valid?).to_not be
+        expect(word_mapping.errors.has_key?(:word)).to be
+      end
+    end
+  end
 end

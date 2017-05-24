169cf617bfe0029bd6846543f8ed62b0338c88e2
kozo yamagata <tune002@gmail.com>
2017-05-24 14:57:14 +0900

    Add question_answer_id column to learning_training_messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425144246) do
+ActiveRecord::Schema.define(version: 20170524054315) do
 
   create_table "allowed_hosts", force: :cascade do |t|
     t.integer  "scheme",     limit: 4,   default: 0
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20170425144246) do
   end
 
   add_index "allowed_hosts", ["scheme", "domain", "bot_id"], name: "index_allowed_hosts_on_scheme_and_domain_and_bot_id", unique: true, using: :btree
+
+  create_table "answer_files", force: :cascade do |t|
+    t.integer  "answer_id",  limit: 4,               null: false
+    t.string   "file",       limit: 255,             null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "file_type",  limit: 255,             null: false
+    t.integer  "file_size",  limit: 4,   default: 0
+  end
 
   create_table "answers", force: :cascade do |t|
     t.integer  "defined_answer_id", limit: 4
@@ -147,17 +156,19 @@ ActiveRecord::Schema.define(version: 20170425144246) do
   add_index "learning_parameters", ["bot_id"], name: "index_learning_parameters_on_bot_id", using: :btree
 
   create_table "learning_training_messages", force: :cascade do |t|
-    t.integer  "bot_id",      limit: 4
-    t.string   "question",    limit: 255
-    t.text     "answer_body", limit: 65535
-    t.integer  "answer_id",   limit: 4
-    t.text     "tag_ids",     limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.integer  "bot_id",             limit: 4
+    t.string   "question",           limit: 255
+    t.text     "answer_body",        limit: 65535
+    t.integer  "answer_id",          limit: 4
+    t.text     "tag_ids",            limit: 65535
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "question_answer_id", limit: 4
   end
 
   add_index "learning_training_messages", ["answer_id"], name: "index_learning_training_messages_on_answer_id", using: :btree
   add_index "learning_training_messages", ["bot_id"], name: "index_learning_training_messages_on_bot_id", using: :btree
+  add_index "learning_training_messages", ["question_answer_id"], name: "index_learning_training_messages_on_question_answer_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.integer  "chat_id",       limit: 4

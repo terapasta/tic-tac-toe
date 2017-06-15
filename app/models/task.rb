2051f4713c6df4bdfab34e5053abd59587fecd6e
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-06-15 17:51:25 +0900

    不要なメソッドを削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,16 +8,4 @@ class Task < ActiveRecord::Base
   scope :unstarted, -> (bot) {
     where(bot_id: bot.id, is_done: false)
   }
-
-  def build_new_record(bot_id, chat, message_id, is_bad:)
-    self.bot_id = bot_id
-    # bad評価もしくは回答失敗した、guestとbotのメッセージを取得する
-    messages = chat.messages.select{ |message, i| message.id == message_id || message.id == message_id + 1 }
-      if is_bad == "bad"
-        self.guest_message = messages[0].body
-        self.bot_message   = messages[1].body
-      else
-        self.guest_message = messages[0].body
-      end
-  end
 end

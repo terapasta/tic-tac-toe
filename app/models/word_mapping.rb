d3f8ce1a817177200ba5521e2256dca0a62222af
kozo yamagata <tune002@gmail.com>
2017-06-16 11:20:06 +0900

    Fix WordMapping validation

--- app/models/word_mapping.rb
+++ app/models/word_mapping.rb
@@ -6,7 +6,6 @@ class WordMapping < ActiveRecord::Base
   validates :synonym, presence: true, length: { maximum: 20 }
 
   validate :unique_pair
-  validate :word_is_not_in_synonyms
   validate :word_is_not_eq_synonym
 
   before_validation :strip_word_and_synonym
@@ -16,14 +15,8 @@ class WordMapping < ActiveRecord::Base
   private
 
     def unique_pair
-      if WordMapping.exists?(user_id: user_id, word: word, synonym: synonym)
+      if WordMapping.exists?(bot_id: bot_id, word: word, synonym: synonym)
         errors.add :base, '単語と同意語の組み合わせは既に存在しています。'
-      end
-    end
-
-    def word_is_not_in_synonyms
-      if WordMapping.exists?(user_id: user_id, synonym: word)
-        errors.add :word, 'は既に同義語として登録されている単語は使用できません'
       end
     end
 

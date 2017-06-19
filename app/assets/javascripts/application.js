9429c8934c80d02da0e1d06bfc71426e4621db5f
kozo yamagata <tune002@gmail.com>
2017-06-19 15:45:43 +0900

    Implement selecting script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,11 +31,31 @@ $(function() {
   var selectionCheckBoxies = $('[data-role="select-question-answer"]');
   selectionCheckBoxies.on('change', function() {
     var $checkbox = $(this);
+    var $counter = $('[data-role="select-question-answer-counter"]');
+    var isChecked = $checkbox.is(':checked');
+    var currentCount = parseInt($counter.text());
+    if (currentCount === 5 && isChecked) {
+      $checkbox.attr({ checked: false });
+      return window.alert("選択できるQ&Aは" + currentCount + "個までです");
+    }
     var botId = $checkbox.data('bot-id');
     var questionAnswerId = $checkbox.data('question-answer-id');
     var url = '/bots/' + botId + '/question_answers/' + questionAnswerId + '/selections.json';
-    var type = $checkbox.is(':checked') ? 'POST' : 'DELETE';
-    $.ajax({ type: type, url: url, datatype: 'json' });
+    var type = isChecked ? 'POST' : 'DELETE';
+
+    $checkbox.attr({ disabled: true });
+    $.ajax({ type: type, url: url, datatype: 'json' })
+      .done(function() {
+        var $label = $('[data-role="select-question-answer-label"][data-question-answer-id="' + questionAnswerId + '"]');
+        $label.text(isChecked ? '選択済' : '未選択')
+              .attr({ class: "label label-" + (isChecked ? "success" : "default") });
+
+        var nextCount = currentCount + (isChecked ? 1 : -1);
+        $counter.text(nextCount);
+      })
+      .always(function() {
+        $checkbox.attr({ disabled: false });
+      });
   });
 
   $('select[data-role="url-switcher"]').on('change', function() {

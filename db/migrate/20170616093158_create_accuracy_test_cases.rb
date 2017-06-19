9a13932cfed1a2074c68563d57cc77a7e4a186b0
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-06-19 10:20:37 +0900

    カラム名を変更

--- db/migrate/20170616093158_create_accuracy_test_cases.rb
+++ db/migrate/20170616093158_create_accuracy_test_cases.rb
@@ -3,7 +3,7 @@ class CreateAccuracyTestCases < ActiveRecord::Migration
     create_table :accuracy_test_cases do |t|
       t.text :question_text
       t.text :expected_text
-      t.boolean :is_expected_suggest, default: false
+      t.boolean :is_expected_suggestion, default: false
       t.belongs_to :bot, index: true, foreign_key: true
 
       t.timestamps null: false

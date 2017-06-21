1d4df3f80112150e9c45a89c637ebfc541aea4c4
宮田裕貴 <hq3p15ofcv@gmail.com>
2017-06-21 12:36:01 +0900

    追加してカラムにindex貼る

--- db/migrate/20170621015626_add_column_question_answer_id_to_sentence_synonyms.rb
+++ db/migrate/20170621015626_add_column_question_answer_id_to_sentence_synonyms.rb
@@ -1,6 +1,8 @@
 class AddColumnQuestionAnswerIdToSentenceSynonyms < ActiveRecord::Migration
   def up
     add_column :sentence_synonyms, :question_answer_id, :integer
+    
+    add_index :sentence_synonyms, :question_answer_id
 
     sentence_synonyms = SentenceSynonym.all
     sentence_synonyms.each do |ss|
@@ -11,5 +13,6 @@ class AddColumnQuestionAnswerIdToSentenceSynonyms < ActiveRecord::Migration
 
   def down
     remove_column :sentence_synonyms, :question_answer_id
+    remove_index :sentence_synonyms, :question_answer_id
   end
 end

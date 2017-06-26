4c00ade14773b0f7497fd345604ec66e7750d244
kozo yamagata <tune002@gmail.com>
2017-06-26 17:42:15 +0900

    Add redirection in after task update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,11 @@ class TasksController < ApplicationController
   def update
     @task = @bot.tasks.find(params[:id])
     if @task.update(is_done: true)
-      redirect_to bot_tasks_path(@bot)
+      if params[:redirect_to_new_question_answer_form].to_bool
+        redirect_to new_bot_question_answer_path(@bot, question: @task.guest_message, answer: @task.bot_message)
+      else
+        redirect_to bot_tasks_path(@bot)
+      end
     else
       redirect_to bot_tasks_path(@bot), alert: '対応済みにできませんでした'
     end

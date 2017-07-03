5f3bcb3b1a5bd9db9791fa86cd98a6ed5ead1479
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-07-03 15:44:18 +0900

    リファクタリング

--- app/controllers/chat_tests_controller.rb
+++ app/controllers/chat_tests_controller.rb
@@ -7,24 +7,18 @@ class ChatTestsController < ApplicationController
   end
 
   def create
-    if params[:file].present? && File.extname(params[:file].path) == ".csv"
-      ActiveRecord::Base.transaction do
-        @chat = Chat.build_with_user_role(@bot)
-        @chat.save
-
-        if params[:commit].include?('UTF-8')
-          CSV.foreach(params[:file].path) do |csv_data|
-            build_message(csv_data)
-          end
-        else
-          CSV.foreach(params[:file].path, encoding: "Shift_JIS:UTF-8") do |csv_data|
-            build_message(csv_data)
-          end
-        end
-        raise ActiveRecord::Rollback
-      end
-    else
+    unless params[:file].present? && File.extname(params[:file].path) == ".csv"
       redirect_to new_bot_chat_test_path(@bot), alert: 'csvファイルを選択してください。'
+    end
+
+    ActiveRecord::Base.transaction do
+      @chat = Chat.build_with_user_role(@bot)
+      @chat.save
+      option = params[:commit].include?('UTF-8')? {} : { encoding: "Shift_JIS:UTF-8" }
+      CSV.foreach(params[:file].path, option) do |csv_data|
+        build_message(csv_data)
+      end
+      raise ActiveRecord::Rollback
     end
   end
 

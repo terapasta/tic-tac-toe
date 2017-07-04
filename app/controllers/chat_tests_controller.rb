705978700144834d74e3492031e7377a7cbf85dd
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-07-04 12:02:37 +0900

    ボットテスト結果をダウンロードできる

--- app/controllers/chat_tests_controller.rb
+++ app/controllers/chat_tests_controller.rb
@@ -7,18 +7,36 @@ class ChatTestsController < ApplicationController
   end
 
   def create
-    unless params[:file].present? && File.extname(params[:file].path) == ".csv"
-      redirect_to new_bot_chat_test_path(@bot), alert: 'csvファイルを選択してください。'
-    end
+    respond_to do |format|
+      format.html do
+        if params[:file].blank? || File.extname(params[:file].path) != ".csv"
+          redirect_to new_bot_chat_test_path(@bot), alert: 'csvファイルを選択してください。'
+          return
+        end
 
-    ActiveRecord::Base.transaction do
-      @chat = Chat.build_with_user_role(@bot)
-      @chat.save
-      option = params[:commit].include?('UTF-8')? {} : { encoding: "Shift_JIS:UTF-8" }
-      CSV.foreach(params[:file].path, option) do |csv_data|
-        build_message(csv_data)
+        ActiveRecord::Base.transaction do
+          @chat = Chat.build_with_user_role(@bot)
+          @chat.save
+          option = params[:commit].include?('UTF-8') ? {} : { encoding: "Shift_JIS:UTF-8" }
+          CSV.foreach(params[:file].path, option) do |csv_data|
+            create_message(csv_data)
+          end
+          raise ActiveRecord::Rollback
+        end
+        @bot_test_results = Message.build_for_bot_test(@chat)
       end
-      raise ActiveRecord::Rollback
+
+      format.csv do
+        file_data = JSON.parse(params[:file_data])
+        option = params[:encode].include?('UTF-8') ? Encoding::CP65001 : Encoding::SJIS
+        csv = CSV.generate(force_quotes: true, encoding: option) { |csv|
+          file_data.each do |data|
+            set_data = [data[0], data[1]]
+            csv << set_data
+          end
+        }
+        send_data csv, type: :csv, filename: "test_result.csv"
+      end
     end
   end
 
@@ -27,8 +45,8 @@ class ChatTestsController < ApplicationController
       @bot = bots.find params[:bot_id]
     end
 
-    def build_message(csv_data)
-      message = @chat.messages.create!(body: csv_data[0]) {|m|
+    def create_message(string)
+      message = @chat.messages.create!(body: string[0]) {|m|
         m.speaker = 'guest'
         m.user_agent = request.env['HTTP_USER_AGENT']
       }
--- app/models/message.rb
+++ app/models/message.rb
@@ -62,5 +62,16 @@ class Message < ActiveRecord::Base
     when 'guest'
       messages[index + 1]
     end
+
+  def self.build_for_bot_test(chat)
+    bot_test_results = []
+    chat.messages.each_with_index do |message, i|
+      next if i.odd?
+        messages = []
+        messages.push(message.body)
+        messages.push(chat.messages[i + 1].body)
+        bot_test_results.push(messages)
+    end
+    bot_test_results
   end
 end

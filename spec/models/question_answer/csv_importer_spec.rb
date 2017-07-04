b3f6d96ce4c9d6333a3cc25500f15724a258901c
shwld <shwld@outlook.com>
2017-07-04 14:29:39 +0900

    SJISデータの正常系テストを追加

---
+++ spec/models/question_answer/csv_importer_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe QuestionAnswer::CsvImporter do
+  let(:csv_file) { fixture_file_upload csv_path, 'text/csv' }
+  let(:importer) do
+    QuestionAnswer::CsvImporter.new(csv_file, bot, import_options)
+  end
+
+  describe '#import' do
+    let(:bot) { create(:bot) }
+
+    subject do
+      importer.import
+      importer
+    end
+
+    context 'SJISエンコードの正しいCSVをインポートする場合' do
+      let(:csv_path) { 'sjis_valid_question_answers.csv' }
+      let(:import_options) do
+        { is_utf8: false }
+      end
+
+      context '既存データが0件の場合' do
+        let!(:succeeded) { subject.succeeded }
+
+        it '正常終了すること' do
+          expect(succeeded).to be_truthy
+        end
+
+        it '2件のQuestionAnserが登録されること' do
+          expect(QuestionAnswer.count).to eq 2
+        end
+        it '4件のAnserが登録されること' do
+          expect(Answer.count).to eq 4
+        end
+        it '2件のDecisionBranchが登録されること' do
+          expect(DecisionBranch.count).to eq 2
+        end
+      end
+    end
+  end
+end

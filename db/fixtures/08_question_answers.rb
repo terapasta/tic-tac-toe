728566f17f0f0acfae05486a2d249de8266f41a7
kozo yamagata <tune002@gmail.com>
2017-07-10 14:03:19 +0900

    Add question_answers seed

---
+++ db/fixtures/08_question_answers.rb
@@ -0,0 +1,42 @@
+# DO NOT MODIFY THIS FILE, it was auto-generated.
+#
+# Date: 2017/07/10 12:52:18
+# Seeding QuestionAnswer
+# Written with the command:
+#
+#   bin/rails 
+#
+QuestionAnswer.seed(:id,
+  {"id"=>1, "bot_id"=>1, "question"=>"オフィスはどこにあるの？", "answer_id"=>2, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>2, "bot_id"=>1, "question"=>"渋谷駅からどれくらい歩く？", "answer_id"=>3, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>3, "bot_id"=>1, "question"=>"正式な社名は？", "answer_id"=>4, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>4, "bot_id"=>1, "question"=>"社長は誰？", "answer_id"=>5, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>5, "bot_id"=>1, "question"=>"会社はどこにある？", "answer_id"=>2, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>6, "bot_id"=>1, "question"=>"オフィスはどこにある？", "answer_id"=>2, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>7, "bot_id"=>1, "question"=>"代表は誰？", "answer_id"=>5, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>8, "bot_id"=>1, "question"=>"わかった。ありがとう！", "answer_id"=>6, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>9, "bot_id"=>1, "question"=>"会社の郵便番号教えて", "answer_id"=>7, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>10, "bot_id"=>1, "question"=>"会社の資本金はどれくらい？", "answer_id"=>8, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>11, "bot_id"=>1, "question"=>"社員は何人いるの？", "answer_id"=>9, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>12, "bot_id"=>1, "question"=>"どんな仕事してるの？", "answer_id"=>10, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>13, "bot_id"=>1, "question"=>"会社のビジョンは？", "answer_id"=>11, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>14, "bot_id"=>1, "question"=>"リモートワークあり？", "answer_id"=>12, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>15, "bot_id"=>1, "question"=>"採用してる？", "answer_id"=>13, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>16, "bot_id"=>1, "question"=>"My-opeってどんなサービス？", "answer_id"=>14, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>17, "bot_id"=>1, "question"=>"実績ある？", "answer_id"=>15, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>18, "bot_id"=>1, "question"=>"どこから問い合わせすればいい？", "answer_id"=>16, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>19, "bot_id"=>1, "question"=>"アジャイル開発得意なの？", "answer_id"=>17, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>20, "bot_id"=>1, "question"=>"会社のミッションは？", "answer_id"=>18, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>21, "bot_id"=>1, "question"=>"こんにちは", "answer_id"=>20, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>22, "bot_id"=>1, "question"=>"すごいね！", "answer_id"=>21, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>23, "bot_id"=>1, "question"=>"使えないね", "answer_id"=>22, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>24, "bot_id"=>1, "question"=>"まだ精度低いね", "answer_id"=>23, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>25, "bot_id"=>1, "question"=>"会話にならん", "answer_id"=>23, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>26, "bot_id"=>1, "question"=>"名前なんていうの？", "answer_id"=>24, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>27, "bot_id"=>1, "question"=>"名前は？", "answer_id"=>24, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>28, "bot_id"=>1, "question"=>"君はだれ？", "answer_id"=>24, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>29, "bot_id"=>1, "question"=>"好きなものは？", "answer_id"=>25, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>30, "bot_id"=>1, "question"=>"どんなお酒飲むの？", "answer_id"=>26, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false},
+  {"id"=>31, "bot_id"=>1, "question"=>"駅からどれくらい？", "answer_id"=>3, "underlayer"=>nil, "created_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "updated_at"=>Mon, 10 Jul 2017 12:51:44 JST +09:00, "selection"=>false}
+)
+# End auto-generated file.

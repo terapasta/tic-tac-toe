f6095ee3b9579247df8288c1e5cfec0442c48058
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-07-11 10:24:05 +0900

    contact_states関連ファイルとロジックを削除

--- app/models/contact_state.rb
+++
@@ -1,9 +0,0 @@
-class ContactState < ActiveRecord::Base
-  belongs_to :chat
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  # validates :name, presence: true
-  validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
-  # validates :body, presence: true
-end

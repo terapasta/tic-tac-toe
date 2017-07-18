2288318146cdc449176c7f759cd155598250400e
shwld <shwld@outlook.com>
2017-07-18 15:08:04 +0900

    ptnaのテストを通す

--- learning/learning/tests/engine/test_ptna_conversation.py
+++ learning/learning/tests/engine/test_ptna_conversation.py
@@ -15,59 +15,56 @@ class PtnaConversationTestCase(TestCase):
         self.csv_file_path = './fixtures/learning_training_messages/ptna.csv'
         self.bot_id = 8  # bot_id = 8 はPTNA
         self.threshold = 0.5
-        self.answers = helper.build_answers(self.csv_file_path)
+        self.question_answers = helper.build_question_answers(self.csv_file_path)
 
         # 学習処理は時間がかかるためmodelのdumpファイルを作ったらコメントアウトしてもテスト実行可能
-        # _evaluator = Bot(self.bot_id, helper.learning_parameter(use_similarity_classification=False)).learn(datasource_type='csv')
+        _evaluator = Bot(self.bot_id, self.learning_parameter).learn(datasource_type='csv')
 
     def test_hope_female_teacher(self):
         questions = ['女の先生']
-        result = Reply(self.bot_id, self.learning_parameter).perform(questions)
-        # TODO: answer_idの使用をやめテストを通す
-        answer_body = helper.get_answer_body(self.answers, result.answer_id)
+        result = Reply(self.bot_id, self.learning_parameter).perform(questions, datasource_type='csv')
+        answer_body = helper.get_answer(self.question_answers, result.question_answer_id)
 
         expected_answer = '教室の一覧に性別が表示されていますので、そちらをご参照ください。'
 
         eq_(helper.replace_newline_and_space(answer_body), helper.replace_newline_and_space(expected_answer))
+        # TODO: probabilityが低くなってしまう
         ok_(result.probability > self.threshold)
 
     def test_hello(self):
         questions = ['こんにちは']
-        result = Reply(self.bot_id, self.learning_parameter).perform(questions)
-        # TODO: answer_idの使用をやめテストを通す
-        answer_body = helper.get_answer_body(self.answers, result.answer_id)
+        result = Reply(self.bot_id, self.learning_parameter).perform(questions, datasource_type='csv')
+        answer_body = helper.get_answer(self.question_answers, result.question_answer_id)
 
         expected_answer = 'こんにちは'
 
         eq_(helper.replace_newline_and_space(answer_body), helper.replace_newline_and_space(expected_answer))
+        # TODO: probabilityが低くなってしまう
         ok_(result.probability > self.threshold)
 
     def test_want_to_join(self):
         questions = ['入会したいのですが']
-        result = Reply(self.bot_id, self.learning_parameter).perform(questions)
-        # TODO: answer_idの使用をやめテストを通す
-        answer_body = helper.get_answer_body(self.answers, result.answer_id)
+        result = Reply(self.bot_id, self.learning_parameter).perform(questions, datasource_type='csv')
+        answer_body = helper.get_answer(self.question_answers, result.question_answer_id)
 
         expected_answer = 'オンライン入会\r\nhttps://www.piano.or.jp/member_entry/member_entry_step0_1.php\r\n\r\n入会申込書のご請求\r\nhttp://www.piano.or.jp/info/member/memberentry.html'
 
         eq_(helper.replace_newline_and_space(answer_body), helper.replace_newline_and_space(expected_answer))
+        # TODO: probabilityが低くなってしまう
         ok_(result.probability > self.threshold)
 
     def test_fail_want_to_eat_ramen(self):
         questions = ['おいしいラーメンが食べたいです']
-        # questions = ['']
-        result = Reply(self.bot_id, self.learning_parameter).perform(questions)
+        result = Reply(self.bot_id, self.learning_parameter).perform(questions, datasource_type='csv')
 
         # ラベル0(分類失敗)に分類されること
-        # TODO: answer_idの使用をやめテストを通す
-        eq_(result.answer_id, Reply.CLASSIFY_FAILED_ANSWER_ID)
+        eq_(result.question_answer_id, Reply.CLASSIFY_FAILED_ANSWER_ID)
         ok_(result.probability > self.threshold)
 
     def test_fail_blank(self):
         questions = ['']
-        result = Reply(self.bot_id, self.learning_parameter).perform(questions)
+        result = Reply(self.bot_id, self.learning_parameter).perform(questions, datasource_type='csv')
 
         # ラベル0(分類失敗)に分類されること
-        # TODO: answer_idの使用をやめテストを通す
-        eq_(result.answer_id, Reply.CLASSIFY_FAILED_ANSWER_ID)
+        eq_(result.question_answer_id, Reply.CLASSIFY_FAILED_ANSWER_ID)
         ok_(result.probability > self.threshold)

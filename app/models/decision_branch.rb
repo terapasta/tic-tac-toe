a1669532cc56cf8e46b8a7ee13420c41726e1861
kozo yamagata <tune002@gmail.com>
2017-07-19 16:29:40 +0900

    Add dependent option

--- app/models/decision_branch.rb
+++ app/models/decision_branch.rb
@@ -2,7 +2,11 @@ class DecisionBranch < ActiveRecord::Base
   belongs_to :bot
   belongs_to :answer_data, class_name: 'Answer', foreign_key: :answer_id
   belongs_to :next_answer, class_name: 'Answer', foreign_key: :next_answer_id
-  has_many :child_decision_branches, class_name: 'DecisionBranch', foreign_key: :parent_decision_branch_id
+
+  has_many :child_decision_branches,
+    class_name: 'DecisionBranch',
+    foreign_key: :parent_decision_branch_id,
+    dependent: :destroy
 
   validates :question_answer_id,
     presence: true,

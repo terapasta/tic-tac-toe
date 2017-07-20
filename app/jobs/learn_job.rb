7ce78f521e137525675510413acd071932c19bd3
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-07-20 15:22:50 +0900

    ActiveJobの例外処理をrescue_fromに変更

--- app/jobs/learn_job.rb
+++ app/jobs/learn_job.rb
@@ -1,6 +1,14 @@
 class LearnJob < ActiveJob::Base
   queue_as :default
 
+  rescue_from(ActiveRecord::RecordNotFound) do |exception|
+    Rails.logger.debug('LearnJob: 学習の実行に失敗しました。')
+    Rails.logger.error exception.message
+    Rails.logger.error exception.backtrace.join("\n")
+    bot.update(learning_status: :failed)
+    raise exception
+  end
+
   def perform(bot_id)
     bot = Bot.find(bot_id)
     summarizer = Learning::Summarizer.new(bot)
@@ -17,11 +25,5 @@ class LearnJob < ActiveJob::Base
     bot.build_score if bot.score.nil?
     bot.score.update!(scores)
     bot.update!(learning_status: :successed)
-  rescue => e
-    Rails.logger.debug('LearnJob: 学習の実行に失敗しました。')
-    Rails.logger.error e.message
-    Rails.logger.error e.backtrace.join("\n")
-    bot.update(learning_status: :failed)
-    raise e
   end
 end

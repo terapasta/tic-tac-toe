917a328f2d067e050abc8da9a64e26308576b3ab
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-07-25 14:28:11 +0900

    Revert "テストケースの文言修正"
    This reverts commit e4ff4c91c1fdb0646088ee1820e5877991315411.

--- spec/models/allowed_ip_address_spec.rb
+++ spec/models/allowed_ip_address_spec.rb
@@ -10,28 +10,28 @@ RSpec.describe AllowedIpAddress do
   end
 
   context 'IPv4の場合' do
-    it 'バリデーションが追加されること' do
+    it 'バリデーションが成功すること' do
       allowed_ip_address = AllowedIpAddress.new(value: '122.132.195.11', bot: bot)
       expect(allowed_ip_address).to be_valid
     end
   end
 
   context 'IPv6の場合' do
-    it 'バリデーションが追加されること' do
+    it 'レコードが追加されること' do
       allowed_ip_address = AllowedIpAddress.new(value: 'ABCD:EF01:2345:6789:ABCD:EF01:2345:6789', bot: bot)
       expect(allowed_ip_address).to be_valid
     end
   end
 
   context 'ブランクの場合' do
-    it 'バリデーションが追加されること' do
+    it 'レコードが追加されないこと' do
       allowed_ip_address = AllowedIpAddress.new(value: '', bot: bot)
       expect(allowed_ip_address).to_not be_valid
     end
   end
 
   context 'IPアドレスフォーマット以外の場合' do
-    it 'バリデーションが追加されること' do
+    it 'レコードが追加されないこと' do
       allowed_ip_address = AllowedIpAddress.new(value: 'hogehoge', bot: bot)
       expect(allowed_ip_address).to_not be_valid
     end

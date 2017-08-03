56301fb1d70af8555ea2d98e9a936626b12f99d4
kozo yamagata <tune002@gmail.com>
2017-08-03 18:10:16 +0900

    Fix LearnJob's error handling

--- app/jobs/learn_job.rb
+++ app/jobs/learn_job.rb
@@ -1,29 +1,32 @@
 class LearnJob < ActiveJob::Base
   queue_as :default
 
-  rescue_from(::StandardError) do |exception|
-    Rails.logger.debug('LearnJob: 学習の実行に失敗しました。')
-    Rails.logger.error exception.message
-    Rails.logger.error exception.backtrace.join("\n")
-    bot.update(learning_status: :failed)
-    raise exception
-  end
+  rescue_from StandardError, with: :handle_error
 
   def perform(bot_id)
-    bot = Bot.find(bot_id)
-    summarizer = Learning::Summarizer.new(bot)
+    @bot = Bot.find(bot_id)
+    summarizer = Learning::Summarizer.new(@bot)
     summarizer.summary
 
-    if bot.learning_parameter&.use_similarity_classification? != false
+    if @bot.learning_parameter&.use_similarity_classification? != false
       summarizer.unify_learning_training_message_words!
     else
-      LearningTrainingMessage.amp!(bot)
-      LearningTrainingMessage.amp_by_sentence_synonyms!(bot)
+      LearningTrainingMessage.amp!(@bot)
+      LearningTrainingMessage.amp_by_sentence_synonyms!(@bot)
     end
 
-    scores = Ml::Engine.new(bot).learn
-    bot.build_score if bot.score.nil?
-    bot.score.update!(scores)
-    bot.update!(learning_status: :successed)
+    scores = Ml::Engine.new(@bot).learn
+    @bot.build_score if @bot.score.nil?
+    @bot.score.update!(scores)
+    @bot.update!(learning_status: :successed)
   end
+
+  private
+    def handle_error
+      Rails.logger.error('LearnJob: 学習の実行に失敗しました。')
+      Rails.logger.error exception.message
+      Rails.logger.error exception.backtrace.join("\n")
+      @bot.update(learning_status: :failed)
+      raise exception
+    end
 end

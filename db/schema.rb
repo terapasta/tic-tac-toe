483eeaf938ac36edde0162dfbc3323e1d854992e
kozo yamagata <tune002@gmail.com>
2017-08-07 14:36:33 +0900

    Delete is_selected_for_chat column from bots table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807053140) do
+ActiveRecord::Schema.define(version: 20170807053445) do
 
   create_table "accuracy_test_cases", force: :cascade do |t|
     t.text     "question_text",          limit: 65535
@@ -68,15 +68,14 @@ ActiveRecord::Schema.define(version: 20170807053140) do
   create_table "bots", force: :cascade do |t|
     t.integer  "user_id",                      limit: 4
     t.string   "name",                         limit: 255
-    t.string   "token",                        limit: 64,                    null: false
+    t.string   "token",                        limit: 64,    null: false
     t.string   "classify_failed_message",      limit: 255
     t.string   "start_message",                limit: 255
-    t.datetime "created_at",                                                 null: false
-    t.datetime "updated_at",                                                 null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.string   "image",                        limit: 255
     t.string   "learning_status",              limit: 255
     t.datetime "learning_status_changed_at"
-    t.boolean  "is_selected_for_chat",                       default: false
     t.text     "selected_question_answer_ids", limit: 65535
   end
 

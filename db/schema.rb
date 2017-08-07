346184215779f6c3867f858078f7726c91419f97
kozo yamagata <tune002@gmail.com>
2017-08-07 16:10:03 +0900

    Delete tag_ids from learning_training_messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807065825) do
+ActiveRecord::Schema.define(version: 20170807070110) do
 
   create_table "accuracy_test_cases", force: :cascade do |t|
     t.text     "question_text",          limit: 65535
@@ -154,7 +154,6 @@ ActiveRecord::Schema.define(version: 20170807065825) do
     t.string   "question",           limit: 255
     t.text     "answer_body",        limit: 65535
     t.integer  "answer_id",          limit: 4
-    t.text     "tag_ids",            limit: 65535
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.integer  "question_answer_id", limit: 4

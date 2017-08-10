8f633d6678386350fa7a95e2e602c895be51b314
kozo yamagata <tune002@gmail.com>
2017-08-10 17:30:56 +0900

    Update login feature spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -13,13 +13,32 @@ RSpec.describe 'Login', typre: :feature, js: true do
     create(:bot, user: user)
   end
 
-  scenario do
-    visit '/users/sign_in'
-    expect(page).to have_content('ログイン')
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: 'hogehoge'
-    click_on 'ログイン'
-    page.save_screenshot
-    expect(page).to have_content(bot.name)
+  feature 'login' do
+    scenario do
+      visit '/users/sign_in'
+      expect(page).to have_content('ログイン')
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: 'hogehoge'
+      click_on 'ログイン'
+      page.save_screenshot
+      expect(page).to have_content(bot.name)
+    end
+  end
+
+  feature 'login and select bot' do
+    scenario do
+      visit '/users/sign_in'
+      expect(page).to have_content('ログイン')
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: 'hogehoge'
+      click_on 'ログイン'
+      page.save_screenshot
+      within "#bot-#{bot.id}" do
+        click_on '詳細'
+      end
+
+      expect(page).to have_content('チャットする')
+      expect(page).to have_content('Q&A管理')
+    end
   end
 end

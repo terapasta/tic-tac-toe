33b3a6dfba141018b9045a8c8b1fb30cfd12911d
shwld <shwld@outlook.com>
2017-08-17 14:40:43 +0900

    ログを出力する

--- learning/app/controllers/learn_controller.py
+++ learning/app/controllers/learn_controller.py
@@ -1,5 +1,6 @@
-from app.factories.cosine_similarity_factory import CosineSimilarityFactory
+from app.shared.logger import logger
 from app.shared.current_bot import CurrentBot
+from app.factories.cosine_similarity_factory import CosineSimilarityFactory
 
 
 class LearnController:
@@ -8,9 +9,19 @@ class LearnController:
         self._factory = factory if factory is not None else CosineSimilarityFactory()
 
     def perform(self):
+        self.write_process_log('start')
+        logger.debug('bot_id: %s' % self.bot.id)
+
+        self.write_process_log('load learning_training_messages')
         all_learning_training_messages_data = self._factory.get_learning_training_messages().all()
+
+        self.write_process_log('tokenize learning_training_messages')
         tokenized_sentences = self._factory.get_tokenizer().tokenize(all_learning_training_messages_data['question'])
+
+        self.write_process_log('fit')
         self._factory.get_vectorizer().fit(tokenized_sentences)
+
+        self.write_process_log('end')
 
         return {
             'accuracy': 0,
@@ -18,3 +29,6 @@ class LearnController:
             'recall': 0,
             'f1': 0,
         }
+
+    def write_process_log(self, process_name):
+        logger.info('>> LearnController#perform : %s' % process_name)
--- learning/app/controllers/reply_controller.py
+++ learning/app/controllers/reply_controller.py
@@ -1,6 +1,7 @@
 import numpy as np
-from app.factories.cosine_similarity_factory import CosineSimilarityFactory
+from app.shared.logger import logger
 from app.shared.current_bot import CurrentBot
+from app.factories.cosine_similarity_factory import CosineSimilarityFactory
 
 
 class ReplyController:
@@ -9,15 +10,30 @@ class ReplyController:
         self._factory = factory if factory is not None else CosineSimilarityFactory()
 
     def perform(self, text):
+        self.write_process_log('start')
+        logger.debug('question: %s' % text)
+
+        self.write_process_log('load learning_training_messages')
         bot_learning_training_messages_data = self._factory.get_learning_training_messages().by_bot(self.bot.id)
+
+        self.write_process_log('tokenize learning_training_messages')
         bot_tokenized_sentences = self._factory.get_tokenizer().tokenize(bot_learning_training_messages_data['question'])
+
+        self.write_process_log('vectorize learning_training_messages')
         bot_features = self._factory.get_vectorizer().transform(bot_tokenized_sentences)
 
+        self.write_process_log('tokenize question')
         tokenized_sentences = self._factory.get_tokenizer().tokenize([text])
-        question_features = self._factory.get_vectorizer().transform(tokenized_sentences)
+        logger.debug(tokenized_sentences)
 
+        self.write_process_log('vectorize question')
+        question_features = self._factory.get_vectorizer().transform(tokenized_sentences)
+        logger.debug(question_features)
+
+        self.write_process_log('predict')
         probabilities = self._factory.get_estimator().predict(question_features, bot_features)
 
+        self.write_process_log('sort')
         sorted_data = sorted(
                 zip(bot_learning_training_messages_data['question_answer_id'], probabilities),
                 key=lambda x: x[1],
@@ -30,8 +46,15 @@ class ReplyController:
                 }),
                 sorted_data
             ))
+        for row in results[:10]:
+            logger.debug(row)
+
+        self.write_process_log('end')
 
         return {
             'question_feature_count': np.count_nonzero(question_features.toarray()),
             'results': results[:10],
         }
+
+    def write_process_log(self, process_name):
+        logger.info('>> ReplyController#perform : %s' % process_name)

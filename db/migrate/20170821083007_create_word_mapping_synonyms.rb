9fac36185e63ba00f5f43e5354506add2e89da38
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-08-22 14:36:25 +0900

    マイグレーションファイルを修正

--- db/migrate/20170821083007_create_word_mapping_synonyms.rb
+++ db/migrate/20170821083007_create_word_mapping_synonyms.rb
@@ -1,15 +1,14 @@
 class CreateWordMappingSynonyms < ActiveRecord::Migration
   def change
     create_table :word_mapping_synonyms do |t|
-      t.string :value
-      t.integer :word_mapping_id
+      t.string :value, null: false
+      t.integer :word_mapping_id, null: false
 
       t.timestamps null: false
     end
 
     ActiveRecord::Base.transaction do
-      word_mappings = WordMapping.all
-      word_mappings.each do |word|
+      WordMapping.find_each do |word|
         WordMappingSynonym.create(value: word.synonym, word_mapping_id: word.id)
       end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -268,8 +268,8 @@ ActiveRecord::Schema.define(version: 20170821083007) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "word_mapping_synonyms", force: :cascade do |t|
-    t.string   "value",           limit: 255
-    t.integer  "word_mapping_id", limit: 4
+    t.string   "value",           limit: 255, null: false
+    t.integer  "word_mapping_id", limit: 4,   null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
   end

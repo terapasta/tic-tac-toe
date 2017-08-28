efa1ab99606df675976f5fed717591f792a82a8e
shwld <shwld@outlook.com>
2017-08-28 16:46:00 +0900

    テストが0件の場合にエラーになる問題を解消

--- app/controllers/admin/bots/accuracy_test_cases/executions_controller.rb
+++ app/controllers/admin/bots/accuracy_test_cases/executions_controller.rb
@@ -6,7 +6,7 @@ class Admin::Bots::AccuracyTestCases::ExecutionsController < Admin::Bots::BaseCo
     @accuracy_test_cases = test_result.accuracy_test_cases
     @result = test_result.details
     @success_count_text = "#{test_result.success_count} 成功 / #{test_result.accuracy_test_cases.count} total,"
-    @success_percentage_text = "成功率 #{(test_result.accuracy * 100).to_i}%"
+    @success_percentage_text = "成功率 #{(test_result.accuracy * 100)}%"
     render 'admin/bots/accuracy_test_cases/index'
   end
 end

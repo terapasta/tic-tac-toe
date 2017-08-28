85ac7a9287a893c07949858f5068218ca6d21fb1
shwld <shwld@outlook.com>
2017-08-28 12:57:21 +0900

    画面表示できない問題に対応

--- app/controllers/admin/bots/accuracy_test_cases/executions_controller.rb
+++ app/controllers/admin/bots/accuracy_test_cases/executions_controller.rb
@@ -2,9 +2,11 @@ class Admin::Bots::AccuracyTestCases::ExecutionsController < Admin::Bots::BaseCo
   include ReplyTestable
 
   def create
-    @result = accuracy_test!(@bot)
-    @success_count_text = "#{@result.success_count} 成功 / #{@result.accuracy_test_cases.count} total,"
-    @success_percentage_text = "成功率 #{(@result.accuracy * 100).to_i}%"
+    test_result = accuracy_test!(@bot)
+    @accuracy_test_cases = test_result.accuracy_test_cases
+    @result = test_result.details
+    @success_count_text = "#{test_result.success_count} 成功 / #{test_result.accuracy_test_cases.count} total,"
+    @success_percentage_text = "成功率 #{(test_result.accuracy * 100).to_i}%"
     render 'admin/bots/accuracy_test_cases/index'
   end
 end

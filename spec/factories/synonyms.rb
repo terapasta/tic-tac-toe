0bf0dd41d0b39bb102383138a7f626bc31dcb87c
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-08-29 18:49:21 +0900

    煩雑なバリデーションのテスト追加

--- spec/factories/synonyms.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :synonym do
-    value "MyString"
-    word_mappings_id 1
-  end
-end
--- spec/models/word_mapping_spec.rb
+++ spec/models/word_mapping_spec.rb
@@ -2,18 +2,54 @@ require 'rails_helper'
 
 RSpec.describe WordMapping, type: :model do
   describe 'validations' do
-    context 'word does already exists as synonym' do
-      let!(:other_word_mapping) do
-        create(:word_mapping)
+    context 'word does already exists as synonym' do      
+      let(:word_mapping) do
+        build(:word_mapping, word: 'ほげ')
       end
-
-      let!(:word_mapping) do
-        build(:word_mapping, word: other_word_mapping.synonym)
-      end
-
-      it 'invalid' do
-        expect(word_mapping.valid?).to_not be
-        expect(word_mapping.errors.has_key?(:word)).to be
+      
+      context 'invalid' do
+        it 'invalidになること' do
+          params = {
+            word: 'ほげ',
+            word_mapping_synonyms_attributes: [
+              {
+                value: 'ほげ',
+              }
+            ]
+          }
+          @word_mapping = WordMapping.new(params)
+          expect(@word_mapping.valid?).to_not be
+          expect(@word_mapping.errors[:base]).to be_present
+        end
+        
+        it 'invalidになること' do
+          params = {
+            word: 'ほげ',
+            word_mapping_synonyms_attributes: [
+              {
+                value: 'hoge',
+              },
+              {
+                value: 'hoge',
+              }
+            ]
+          }
+          @word_mapping = WordMapping.new(params)
+          expect(@word_mapping.valid?).to_not be
+          expect(@word_mapping.errors[:base]).to be_present
+        end
+        
+        it 'invalidになること' do
+          word_mapping = create(:word_mapping)
+          synonym = create(:word_mapping_synonym, word_mapping_id: word_mapping.id)
+          
+          params = {
+            word: synonym.value,
+          }
+          @word_mapping = WordMapping.new(params)
+          expect(@word_mapping.valid?).to_not be
+          expect(@word_mapping.errors[:base]).to be_present
+        end
       end
     end
   end

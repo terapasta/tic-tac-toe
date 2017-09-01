8e2ef17829cab30323f8710ede54d56c5346fc69
kozo yamagata <tune002@gmail.com>
2017-09-01 16:03:55 +0900

    Modify label and icon to 'pin'

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -11,39 +11,39 @@ RSpec.describe Message, :type => :model do
 
   describe 'AnswerMarkable' do
     describe '#save_to_answer_marked' do
-      context '注意回答に変更できる場合' do
-        it 'Botの回答メッセージであれば注意回答に変更できること' do
+      context 'ピンできる場合' do
+        it 'Botの回答メッセージであればピンできること' do
           expect(answer_success_by_bot.save_to_answer_marked).to be
           expect(answer_failed_by_bot.save_to_answer_marked).to be
         end
       end
 
-      context '注意回答に変更できない場合' do
+      context 'ピンできない場合' do
         let(:guest_message) do
           create(:message)
         end
-        
-        it 'Bot以外のメッセージは注意回答に変更できないこと' do
+
+        it 'Bot以外のメッセージはピンできないこと' do
           expect(guest_message.save_to_answer_marked).not_to be
         end
       end
     end
 
     describe '#save_to_remove_answer_marked' do
-      context '注意回答の取り消しができる場合' do
-        it 'Botの回答メッセージであれば注意回答の取り消し状態にできること' do
+      context 'ピンの取り消しができる場合' do
+        it 'Botの回答メッセージであればピンの取り消し状態にできること' do
           expect(answer_success_by_bot.save_to_remove_answer_marked).to be
           expect(answer_failed_by_bot.save_to_remove_answer_marked).to be
         end
       end
 
-      context '注意回答の取り消しができない場合' do
+      context 'ピンの取り消しができない場合' do
         let(:answer_marked_by_guest) do
           create(:message, answer_marked: true)
         end
 
-        it 'Bot以外のメッセージは注意回答の取り消し状態にできないこと' do
-          # botのメッセージ以外は注意回答にできないので、本来は発生しないケース
+        it 'Bot以外のメッセージはピンの取り消し状態にできないこと' do
+          # botのメッセージ以外はピンできないので、本来は発生しないケース
           expect(answer_marked_by_guest.save_to_remove_answer_marked).not_to be
         end
       end

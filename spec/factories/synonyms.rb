2097aa7aa0c3525345b6ff540f3ca1c0358a30bd
kozo yamagata <tune002@gmail.com>
2017-09-05 18:42:24 +0900

    Fix factories

--- spec/factories/synonyms.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :word_mapping_synonym do
-    sequence(:value) { |n| "word_mapping_synonyms.value #{n}" }
-  end
-end
--- spec/factories/word_mapping_synonyms.rb
+++ spec/factories/word_mapping_synonyms.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :word_mapping_synonym do
-    sequence(:value) { |n| "value#{n}" }
+    sequence(:value) { |n| "word_mapping_synonyms.value #{n}" }
   end
 end

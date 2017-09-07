d452fe194d1354f3ba50174fcca5d11b33e27a58
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-09-07 18:25:34 +0900

    schemaファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -244,13 +244,6 @@ ActiveRecord::Schema.define(version: 20170831061200) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "word_mapping_synonyms", force: :cascade do |t|
-    t.string   "value",           limit: 255, null: false
-    t.integer  "word_mapping_id", limit: 4,   null: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-  end
-
   create_table "word_mappings", force: :cascade do |t|
     t.string   "word",       limit: 20, null: false
     t.datetime "created_at",            null: false

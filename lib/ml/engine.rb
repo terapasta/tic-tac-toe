1d26951f914e8c08834aaf1b85fb1cd6b8254507
shwld <shwld@outlook.com>
2017-09-11 15:20:01 +0900

    他で利用しないの変数をローカル変数にする

--- lib/ml/engine.rb
+++ lib/ml/engine.rb
@@ -4,10 +4,10 @@ require 'learning/gateway_services_pb'
 class Ml::Engine
 
   def initialize(bot)
-    @host = ENV.fetch('RPC_HOST'){ '127.0.0.1' }
-    @port = 6000
+    host = ENV.fetch('RPC_HOST'){ '127.0.0.1' }
+    port = 6000
     @bot = bot
-    @stub = Gateway::Bot::Stub.new("#{@host}:#{@port}", :this_channel_is_insecure)
+    @stub = Gateway::Bot::Stub.new("#{host}:#{port}", :this_channel_is_insecure)
   end
 
   def reply(body)

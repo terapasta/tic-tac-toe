914b4bdf573b15eeefbee11ac3812e3a49bc27df
shwld <shwld@outlook.com>
2017-09-11 14:22:23 +0900

    persistanceの対象テーブルをdumpsに変える

--- learning/app/shared/datasource/database/persistence.py
+++ learning/app/shared/datasource/database/persistence.py
@@ -12,31 +12,36 @@ class Persistence:
         self.database = database
         self.bot = bot
 
-    def load(self, key):
+    def load(self, name):
         records = self.database.select(
-            'SELECT * FROM bots WHERE id = %(bot_id)s',
+            'SELECT * FROM dumps WHERE bot_id = %(bot_id)s AND name = %(name)s;',
             {
-                'key': key,
                 'bot_id': self.bot.id,
+                'name': name,
             },
         )
-        if len(records) > 0 and records[key][0] is not None:
-            file = io.BytesIO(records[key][0])
+        if len(records) > 0 and records['content'][0] is not None:
+            file = io.BytesIO(records['content'][0])
             return joblib.load(file)
 
         return None
 
-    def dump(self, obj, key):
+    def dump(self, obj, name):
         file = io.BytesIO()
         joblib.dump(obj, file)
         file.seek(0)
-        # Note: SQLインジェクションを回避するためkeyのチェックを行う
-        if re.match(r'^[a-zA-Z_]+$', key):
-            sql = 'UPDATE bots SET ' + key + '=%(value)s WHERE id = %(bot_id)s'
-            self.database.execute(sql, {
-                    'key': key,
-                    'value': file.getvalue(),
+        self.database.execute(
+                'DELETE FROM dumps WHERE bot_id = %(bot_id)s AND name = %(name)s;',
+                {
                     'bot_id': self.bot.id,
-                },)
-        else:
-            raise ValueError('dump column is invalid')
+                    'name': name,
+                },
+            )
+        self.database.execute(
+                'INSERT INTO dumps (bot_id, name, content) VALUES (%(bot_id)s, %(name)s, %(content)s);',
+                {
+                    'bot_id': self.bot.id,
+                    'name': name,
+                    'content': file.getvalue(),
+                },
+            )

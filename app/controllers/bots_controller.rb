a73df1ac8e77665b61e5f8f1bcc682c93da8fe84
kozo yamagata <tune002@gmail.com>
2017-09-19 15:41:05 +0900

    Show chats count in today for only ec plan users

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -8,6 +8,12 @@ class BotsController < ApplicationController
     redirect_to bot_path(@bots.first) if @bots.count == 1
   end
 
+  def show
+    @today_chats_count = @bot.chats.in_today.count
+    @chats_limit = current_user.chats_limit_per_day
+    @progress = ((@today_chats_count.to_f / @chats_limit.to_f) * 100).round
+  end
+
   def reset
     @bot.reset_training_data!
     flash[:notice] = '学習データをリセットしました'

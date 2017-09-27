d8f14499f87aba3066e795b6d1786ed8fe3e3def
kozo yamagata <tune002@gmail.com>
2017-09-27 16:01:41 +0900

    Add email validation

--- app/models/guest_user.rb
+++ app/models/guest_user.rb
@@ -2,6 +2,10 @@ class GuestUser < ActiveRecord::Base
   validates :name,
     presence: true
 
+  validates :email,
+    email: true,
+    allow_nil: true
+
   validates :guest_key,
     presence: true
 end

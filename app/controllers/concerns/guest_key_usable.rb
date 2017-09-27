3039b09d072d67718e715a44d0212ac54063f367
kozo yamagata <tune002@gmail.com>
2017-09-27 21:22:13 +0900

    Modify GuestKeyUsable to use plain cookies

--- app/controllers/concerns/guest_key_usable.rb
+++ app/controllers/concerns/guest_key_usable.rb
@@ -3,17 +3,16 @@ module GuestKeyUsable
 
   private
     def set_guest_key
-      if cookies.encrypted[:guest_key].blank?
-        cookies.encrypted[:guest_key] = {
+      if cookies[:guest_key].blank?
+        cookies[:guest_key] = {
           value: SecureRandom.hex(64),
           path: '/',
-          expires: 45.days.from_now,
-          secure: Rails.env.production?
+          expires: 45.days.from_now
         }
       end
     end
 
     def guest_key
-      cookies.encrypted[:guest_key]
+      request.cookies['guest_key']
     end
 end

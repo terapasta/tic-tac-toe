4a603c60b2d4441fc11918de8f9f4db8c1f412af
kozo yamagata <tune002@gmail.com>
2017-10-02 16:29:48 +0900

    Skip creating task if logged in as staff

--- app/services/task_create_service.rb
+++ app/services/task_create_service.rb
@@ -1,5 +1,5 @@
 class TaskCreateService
-  def initialize(bot_messages_or_message, bot)
+  def initialize(bot_messages_or_message, bot, current_user)
     @bot_messages = bot_messages_or_message
     unless bot_messages_or_message.is_a?(Array)
       @bot_messages = [bot_messages_or_message]
@@ -7,9 +7,11 @@ class TaskCreateService
     @failed_bot_messages = @bot_messages.select(&:answer_failed).presence ||
                            @bot_messages.select(&:bad?)
     @bot = bot
+    @current_user = current_user
   end
 
   def process
+    return if @current_user.try(:staff?)
     @failed_bot_messages.each do |bot_message|
       guest_message = Message.find_pair_message_from(bot_message)
       next if guest_message.nil?

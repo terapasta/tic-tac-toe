4e03822be10ab125d999ef646a9026b8ab11575d
shwld <shwld@outlook.com>
2017-10-04 12:38:29 +0900

    学習時に辞書を更新するようにした

--- learning/app/core/vectorizer/gensim_tfidf_vectorizer.py
+++ learning/app/core/vectorizer/gensim_tfidf_vectorizer.py
@@ -1,7 +1,7 @@
 import inject
+import os.path
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
-from app.shared.logger import logger
 from app.shared.current_bot import CurrentBot
 from app.shared.datasource.datasource import Datasource
 
@@ -13,14 +13,19 @@ class GensimTfidfVectorizer:
     def __init__(self, bot=None, datasource=None):
         self.bot = bot
         # TODO: 検証用ファイルを参照しているので、S3などに配置してそこから取得するように変える
-        self.dictionary = Dictionary.load_from_text('./prototype/working/wiki_wordids.txt.bz2')
+        self.text_path = './prototype/working/myope_wordids.txt.bz2'
+        if not os.path.isfile(self.text_path):
+            self.dictionary = Dictionary.load_from_text('./prototype/working/wiki_wordids.txt.bz2')
+        else:
+            self.dictionary = Dictionary.load_from_text(self.text_path)
         self.vectorizer = TfidfModel.load('./prototype/working/wiki.tfidf_model')
 
     def fit(self, sentences):
-        logger.info('PASS')
+        self.dictionary.add_documents([sentence.split(' ') for sentence in sentences])
+        self.dictionary.save_as_text(self.text_path)
 
     def transform(self, sentences):
-        id_corpus = [self.dictionary.doc2bow(sentence.split(' '), allow_update=True) for sentence in sentences]
+        id_corpus = [self.dictionary.doc2bow(sentence.split(' ')) for sentence in sentences]
         return self.vectorizer[id_corpus]
 
     def fit_transform(self, sentences):

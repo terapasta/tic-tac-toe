60658920c4e610aa4b10b5a9cbfc1e6cb8829615
shwld <shwld@outlook.com>
2017-10-10 18:09:48 +0900

    tfidfにmyopeのqaデータを含める

--- learning/app/core/vectorizer/gensim_tfidf_vectorizer.py
+++ learning/app/core/vectorizer/gensim_tfidf_vectorizer.py
@@ -1,6 +1,7 @@
 import inject
 import os.path
-from gensim.corpora import Dictionary
+import itertools
+from gensim.corpora import Dictionary, MmCorpus
 from gensim.models import TfidfModel
 from app.shared.current_bot import CurrentBot
 from app.shared.datasource.datasource import Datasource
@@ -14,16 +15,27 @@ class GensimTfidfVectorizer:
         self.bot = bot
         # TODO: 検証用ファイルを参照しているので、S3などに配置してそこから取得するように変える
         self.text_path = './prototype/working/myope_wordids.txt.bz2'
+        self.tfidf_path = './prototype/working/myope.tfidf_model'
         if not os.path.isfile(self.text_path):
             self.dictionary = Dictionary.load_from_text('./prototype/working/wiki_wordids.txt.bz2')
         else:
             self.dictionary = Dictionary.load_from_text(self.text_path)
 
-        self.vectorizer = TfidfModel.load('./prototype/working/wiki.tfidf_model')
+        if not os.path.isfile(self.tfidf_path):
+            self.vectorizer = TfidfModel.load('./prototype/working/wiki.tfidf_model')
+        else:
+            self.vectorizer = TfidfModel.load(self.tfidf_path)
 
     def fit(self, sentences):
         self.dictionary.add_documents([sentence.split(' ') for sentence in sentences])
         self.dictionary.save_as_text(self.text_path)
+        corpus = [self.dictionary.doc2bow(text, allow_update=True) for text in [sentence.split(' ') for sentence in sentences]]
+        MmCorpus.serialize('./prototype/working/myope.mm', corpus, id2word=self.dictionary)
+        mm = MmCorpus('./prototype/working/myope.mm')
+        wiki_mm = MmCorpus('./prototype/working/wiki_bow.mm')
+        # Note: Macbook core i5 メモリ8GBで2時間半くらいかかる
+        self.vectorizer = TfidfModel(itertools.chain(wiki_mm, mm), id2word=self.dictionary, normalize=True)
+        self.vectorizer.save(self.tfidf_path)
 
     def transform(self, sentences):
         id_corpus = [self.dictionary.doc2bow(sentence.split(' ')) for sentence in sentences]

9303e018aa2775106aa9ab1fdd5da064ee4f01d8
a.harada <redhornet96@gmail.com>
2017-10-14 16:06:22 +0900

    Word2vecVectorizerを実装しようとしたが不要かもしれない

---
+++ learning/tests/app/core/vectorizer/test_word2vec_vectorizer.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+import inject
+import numpy as np
+from nose.tools import ok_, eq_
+
+from app.core.vectorizer.word2vec_vectorizer import Word2vecVectorizer
+from app.shared.config import Config
+from app.shared.constants import Constants
+from app.shared.current_bot import CurrentBot
+from app.shared.datasource.datasource import Datasource
+
+
+class LearningParameter:
+    datasource_type = Constants.DATASOURCE_TYPE_FILE
+    # use_similarity_classification = True
+    algorithm = Constants.ALGORITHM_WORD2VEC_WMD
+
+
+class Word2vecVectorizerTestCase(TestCase):
+
+    def setUp(self):
+        inject.configure_once()
+        Config().init('test')
+        self.bot_id = 1
+        self.learning_parameter = LearningParameter()
+
+    def test_fit(self):
+        bot = CurrentBot().init(self.bot_id, self.learning_parameter)
+        Datasource().init(bot)
+
+        vectorizer = Word2vecVectorizer()
+        vectorizer.fit(np.array(['hoge', 'moge', 'hage']))
+
+        ok_(True)

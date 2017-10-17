2b6df31925e9cc2052220d3eee424a8f4a2e7ce9
kozo yamagata <tune002@gmail.com>
2017-10-17 23:37:57 +0900

    Turn chromedriver on

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,20 +1,20 @@
 require 'capybara/rspec'
-# require 'selenium-webdriver'
+require 'selenium-webdriver'
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app,
     browser: :chrome,
-    # desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-    #   'chromeOptions' => {
-    #     args: %w(
-    #       headless
-    #       disable-gpu
-    #       no-sandbox
-    #       disable-popup-blocking
-    #       window-size=1680,1050
-    #     ),
-    #   },
-    # )
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      'chromeOptions' => {
+        args: %w(
+          headless
+          disable-gpu
+          no-sandbox
+          disable-popup-blocking
+          window-size=1680,1050
+        ),
+      },
+    )
   )
 end
 

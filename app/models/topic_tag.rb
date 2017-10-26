b44915404449137191ce87bc127d8f193d329550
kozo yamagata <tune002@gmail.com>
2017-10-26 10:21:17 +0900

    Modify updating topic_tags to showing up validation error messages

--- app/models/topic_tag.rb
+++ app/models/topic_tag.rb
@@ -4,4 +4,12 @@ class TopicTag < ApplicationRecord
   has_many :question_answers, through: :topic_taggings
 
   validates :name, presence: true, format: { with: /\A[^\/／]+\z/ }
+
+  validate :must_be_unique
+
+  def must_be_unique
+    if bot.present?
+      errors.add(:base, "タグ名「#{name}」は重複しています") if bot.topic_tags.pluck(:name).include?(name)
+    end
+  end
 end

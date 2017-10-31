d52f813551c76e6930e4f04f4eb2babe0fc0e0b2
shwld <shwld@outlook.com>
2017-10-31 17:49:53 +0900

    未学習の場合にエラーになることを確認

--- learning/tests/app/core/estimator/test_cosine_similarity.py
+++ learning/tests/app/core/estimator/test_cosine_similarity.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
 import inject
-import pandas as pd
-from nose.tools import ok_
+from nose.tools import ok_, assert_raises
 
+from sklearn.exceptions import NotFittedError
 from app.core.tokenizer.mecab_tokenizer import MecabTokenizer
 from app.core.vectorizer.tfidf_vectorizer import TfidfVectorizer
 from app.core.estimator.cosine_similarity import CosineSimilarity
@@ -19,15 +19,15 @@ class LearningParameter:
     algorithm = Constants.ALGORITHM_SIMILARITY_CLASSIFICATION
 
 
-class Word2vecWmdTestCase(TestCase):
+class CosineSimilarityTestCase(TestCase):
 
     def setUp(self):
         inject.configure_once()
         Config().init('test')
         Datasource().init(datasource_type=Constants.DATASOURCE_TYPE_FILE)
-        AppStatus().set_bot(bot_id=1, learning_parameter=LearningParameter())
 
     def test_predict_when_data_is_empty(self):
+        AppStatus().set_bot(bot_id=1, learning_parameter=LearningParameter())
         # データが存在しない場合
         estimator = CosineSimilarity(
             MecabTokenizer(),
@@ -41,3 +41,20 @@ class Word2vecWmdTestCase(TestCase):
 
         # エラーにならないこと
         ok_(True)
+
+    def test_predict_when_vocabulary_wasnot_fitted(self):
+        AppStatus().set_bot(bot_id=9, learning_parameter=LearningParameter())
+        # ボキャブラリーがない場合(未学習の場合)
+        estimator = CosineSimilarity(
+            MecabTokenizer(),
+            TfidfVectorizer(),
+            PassReducer(),
+            PassNormalizer(),
+            Datasource(),
+        )
+
+        def action():
+            estimator.predict(['ピアノ 始める 年齢'])
+
+        # エラーになること
+        assert_raises(NotFittedError, action)

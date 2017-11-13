5e9851aa6001ed7024b5fcb0558a30d562f4ca96
shwld <shwld@outlook.com>
2017-11-13 12:12:04 +0900

    migrateエラーを解決

--- db/fixtures/development/decision_branches.rb
+++
@@ -1,5 +0,0 @@
-DecisionBranch.seed(:id,
-  # 開発動作確認用のためnext_answer_idはいい加減に設定
-  { id: 201, bot_id: 1, answer_id: 20, body: 'こんにちは！', next_answer_id: 21 },
-  { id: 202, bot_id: 1, answer_id: 20, body: 'こんばんは！', next_answer_id: 23 },
-)

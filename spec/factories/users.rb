eaff2b1f01e6b7a13f4ec0b7d95e5c6f8001cfd6
kozo yamagata <tune002@gmail.com>
2017-11-15 14:37:00 +0900

    Fix users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     password 'hogehoge'
     role :normal
     sequence(:confirmation_token) { |n| "hogehoge#{n}" }
+    email_notification true
 
     trait :staff do
       role :staff

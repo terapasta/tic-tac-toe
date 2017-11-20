4302dc28b37d2721416aed8c812a1125179589d6
kozo yamagata <tune002@gmail.com>
2017-11-20 14:29:55 +0900

    Discard Ml::Engine's old methods

--- spec/support/ml_helper.rb
+++ spec/support/ml_helper.rb
@@ -5,11 +5,6 @@ module MlHelper
     LearnJob.new.perform(bot.id)
   end
 
-  def learn_tag_model
-    engine = Ml::Engine.new(nil)
-    engine.learn_tag_model
-  end
-
   def add_tag_to_question_answer(bot, body, tags)
     question_answer = bot.question_answers.find_by!(question: body)
     question_answer.update(tag_list: tags)

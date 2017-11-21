d90d469e01917b51ae5abca853e714c31e9e503e
kozo yamagata <tune002@gmail.com>
2017-11-21 16:11:19 +0900

    Refactor routes with authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  root 'bots#index'
+
   devise_for :users, only: [:sign_in, :sign_out, :confirmation, :session, :password]
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', as: 'user_registration'
-  end
 
   authenticate :user do
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+      put 'users' => 'devise/registrations#update', as: 'user_registration'
+    end
+
     scope :my do
       get '/' => redirect('/my/password/edit')
       get 'password/edit' => 'my/passwords#edit', as: 'edit_my_password'
@@ -15,53 +18,75 @@ Rails.application.routes.draw do
       get 'notification/edit' => 'my/notifications#edit', as: 'edit_my_notification'
       patch 'notification' => 'my/notifications#update', as: 'my_notification'
     end
-  end
 
-  resources :bots, only: [:show, :index] do
-    namespace :settings do
-      get 'embed', to: 'pages#embed'
-      resource :bot, only: [:show, :update]
-      resource :allowed_hosts, only: [:show, :update]
-      resource :allowed_ip_addresses, only: [:show, :update]
-      resource :reset, only: [:show, :create]
+    resource :contacts, only: [:new, :create]
+
+    resources :bots, only: [:show, :index] do
+      namespace :settings do
+        get 'embed', to: 'pages#embed'
+        resource :bot, only: [:show, :update]
+        resource :allowed_hosts, only: [:show, :update]
+        resource :allowed_ip_addresses, only: [:show, :update]
+        resource :reset, only: [:show, :create]
+      end
+      member do
+        post :reset
+      end
+      resources :tasks, only: [:index, :update]
+      resources :sentence_synonyms, only: [:index, :new, :create, :destroy]
+      resources :imported_sentence_synonyms, only: [:index, :new, :create, :destroy]
+      resources :question_answers do
+        resource :selections, only: [:create, :destroy], module: :question_answers
+        resource :answer, only: [:show], module: :question_answers
+        collection do
+          resources :selections, only: [:index], module: :question_answers, as: :question_answers_selections
+        end
+      end
+      resource :topic_tags, only: [:show, :update]
+      resource :imports, only: [:show, :create]
+      resources :exports, only: [:index, :create]
+      resources :chat_tests, only: [:new, :create, :show]
+      resources :threads, only: :index do
+        resources :messages, only: [:index] do
+          resource :answer_marked, only: [:create, :destroy], controller: :answer_marked
+        end
+      end
+      resource :learning, only: [:show, :update]
+      resources :answers, except: [:new] do
+        resources :decision_branches, only: [:index]
+        resources :question_answers, only: [:index], module: :answers
+      end
+      resources :decision_branches, only: [:show, :update, :create, :destroy]
+      resource :conversation_tree, only: [:show]
+      resources :word_mappings
+      resources :allowed_ip_addresses, only: [:index, :new, :create, :edit, :update, :destroy]
     end
-    member do
-      post :reset
-    end
-    resources :tasks, only: [:index, :update]
-    resource :chat_widget, only: [:show]
-    resources :sentence_synonyms, only: [:index, :new, :create, :destroy]
+
     resources :imported_sentence_synonyms, only: [:index, :new, :create, :destroy]
-    resources :question_answers do
-      resource :selections, only: [:create, :destroy], module: :question_answers
-      resource :answer, only: [:show], module: :question_answers
-      collection do
-        resources :selections, only: [:index], module: :question_answers, as: :question_answers_selections
-      end
-    end
-    resource :topic_tags, only: [:show, :update]
-    resource :imports, only: [:show, :create]
-    resources :exports, only: [:index, :create]
-    resources :chat_tests, only: [:new, :create, :show]
-    resources :threads, only: :index do
-      resources :messages, only: [:index] do
-        resource :answer_marked, only: [:create, :destroy], controller: :answer_marked
-      end
-    end
-    # resource :imports, only: [:new, :create]
 
-    resource :learning, only: [:show, :update]
-    resources :answers, except: [:new] do
-      resources :decision_branches, only: [:index]
-      resources :question_answers, only: [:index], module: :answers
+    namespace :api, { format: 'json' } do
+      resources :messages, only: :create
+      resources :question_answers
+      resources :bots do
+        resources :topic_tags, module: :bots
+        resources :question_answers, module: :bots do
+          resources :topic_taggings, module: :question_answers
+          resources :decision_branches, module: :question_answers
+          resource :child_decision_branches, only: [:destroy], module: :question_answers
+        end
+        resources :decision_branches, module: :bots do
+          resource :child_decision_branches, only: [:destroy], module: :decision_branches
+        end
+        resources :answers, module: :bots
+        resources :messages, module: :bots, only: [] do
+          resource :mark, module: :messages, only: [:create, :destroy]
+        end
+
+        resources :word_mappings, only: [:create, :update, :destroy], module: :bots
+      end
+      resources :word_mappings, only: [:create, :update, :destroy]
     end
-    resources :decision_branches, only: [:show, :update, :create, :destroy]
-    resource :conversation_tree, only: [:show]
-    resources :word_mappings
-    resources :allowed_ip_addresses, only: [:index, :new, :create, :edit, :update, :destroy]
   end
-
-  resources :imported_sentence_synonyms, only: [:index, :new, :create, :destroy]
 
   scope 'embed/:token' do
     resource :chats, only: [:show, :new, :destroy] do
@@ -98,37 +123,7 @@ Rails.application.routes.draw do
   end
 
   namespace :api, { format: 'json' } do
-    resources :messages, only: :create
-    resources :question_answers
     resources :public_bots, param: :token, only: [:show]
-    resources :bots do
-      resources :topic_tags, module: :bots
-      resources :question_answers, module: :bots do
-        resources :topic_taggings, module: :question_answers
-        resources :decision_branches, module: :question_answers
-        resource :child_decision_branches, only: [:destroy], module: :question_answers
-      end
-      resources :decision_branches, module: :bots do
-        resource :child_decision_branches, only: [:destroy], module: :decision_branches
-      end
-      resources :answers, module: :bots
-      resources :messages, module: :bots, only: [] do
-        resource :mark, module: :messages, only: [:create, :destroy]
-      end
-
-      resources :word_mappings, only: [:create, :update, :destroy], module: :bots
-    end
-    resources :word_mappings, only: [:create, :update, :destroy]
     resources :guest_users, only: [:show, :create, :update, :destroy], param: :guest_key
   end
-
-  resource :contacts, only: [:new, :create]
-
-  # authenticated :admin_user do
-  #   #rails_admin.dashboard_path
-  # end
-  # authenticated :user do
-  #   root 'dashboards#show', as: :user_root
-  # end
-  root 'bots#index'
 end

24c4d4a2755d39bb48bf67a9449ccec8b20ec52d
kozo yamagata <tune002@gmail.com>
2017-11-27 16:25:51 +0900

    Make tutorial's admin pages

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,3 +1,12 @@
 class Tutorial < ApplicationRecord
   belongs_to :bot
+
+  def self.tasks
+    [
+      :edit_bot_profile,
+      :fifty_question_answers,
+      :ask_question,
+      :embed_chat
+    ]
+  end
 end

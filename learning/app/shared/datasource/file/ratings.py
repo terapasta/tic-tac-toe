ccf20ef0c74b831aa81ed069fe6ada018ef6bde3
shwld <shu.account@outlook.com>
2017-11-27 12:58:14 +0900

    Rocchioアルゴリズムを試す

--- learning/app/shared/datasource/file/ratings.py
+++ learning/app/shared/datasource/file/ratings.py
@@ -19,6 +19,12 @@ class Ratings(BaseCls):
             (self._data['level'] == Constants.RATING_GOOD)
         ]
 
+    def with_bad_by_bot(self, bot_id):
+        return self._data[
+            (self._data['bot_id'] == bot_id) &
+            (self._data['level'] == Constants.RATING_BAD)
+        ]
+
     def higher_rate_by_bot_question(self, bot_id, question):
         # Fixme: 単に取得しているだけで、good/badの数がの多い順に並び替えられていない
         return self._data[

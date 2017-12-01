1335229d475d1c275134adfc4af89fde4850a131
kozo yamagata <tune002@gmail.com>
2017-12-01 09:35:29 +0900

    Fix typo

--- app/controllers/settings/bots_controller.rb
+++ app/controllers/settings/bots_controller.rb
@@ -3,7 +3,7 @@ class Settings::BotsController < Settings::BaseController
   def show; end
 
   def update
-    if @bot.update(permitted_attributes(@bot).merge(tutorial_attribtues: {
+    if @bot.update(permitted_attributes(@bot).merge(tutorial_attributes: {
       edit_bot_profile: true
     }))
       redirect_to bot_settings_bot_path(@bot), notice: 'ボット設定を更新しました'

d46c476e82a40a5867b1a6cc5a611c54a6b5ad7e
shwld <shu.account@outlook.com>
2017-12-04 15:56:03 +0900

    Add logging

--- learning/app/feedback/rocchio.py
+++ learning/app/feedback/rocchio.py
@@ -50,6 +50,7 @@ class Rocchio(BaseFeedback):
             positive_vectors = self.data['good'][posi_result['question_answer_id'][0]]
             new_positive = positive_vectors * self.parameters['positive_wait']
             new_vector = new_vector + new_positive
+            logger.info('complete: += positive vector')
         except:
             pass
 
@@ -59,6 +60,7 @@ class Rocchio(BaseFeedback):
             negative_vectors = self.data['bad'][nega_result['question_answer_id'][0]]
             new_negative = negative_vectors * self.parameters['negative_wait']
             new_vector = new_vector - new_negative
+            logger.info('complete: -= negative vector')
         except:
             pass
 

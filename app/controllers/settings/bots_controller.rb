d82c06940e1dc3bb83b8619a2c40a19763117f21
kozo yamagata <tune002@gmail.com>
2017-12-05 09:59:55 +0900

    Fix settings/bots#update

--- app/controllers/settings/bots_controller.rb
+++ app/controllers/settings/bots_controller.rb
@@ -3,13 +3,17 @@ class Settings::BotsController < Settings::BaseController
   def show; end
 
   def update
-    if @bot.update(permitted_attributes(@bot).merge(tutorial_attributes: {
-      edit_bot_profile: true
-    }))
-      redirect_to bot_settings_bot_path(@bot), notice: 'ボット設定を更新しました'
-    else
-      flash.now.alert = 'ボット設定を更新できませんでした'
-      render :show
+    ActiveRecord::Base.transaction do
+      @bot.assign_attributes(permitted_attributes(@bot))
+      @bot.save!
+      if @bot.tutorial
+        @bot.tutorial.edit_bot_profile = true
+        @bot.tutorial.save!
+      end
     end
+    redirect_to bot_settings_bot_path(@bot), notice: 'ボット設定を更新しました'
+  rescue
+    flash.now.alert = 'ボット設定を更新できませんでした'
+    render :show
   end
 end

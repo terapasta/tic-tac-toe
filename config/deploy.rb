a83d44203a5443546837a57f69a446038e7edf72
kozo yamagata <tune002@gmail.com>
2017-12-11 17:20:44 +0900

    Update provisoning and deployment for new production servers

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,7 +59,7 @@ namespace :deploy do
       execute :cp, shared_path.join('.python-version'), release_path.join('learning/.python-version')
       # execute :cp, shared_path.join('config.yml'), release_path.join('learning/learning/config/config.yml')
       within current_path.join('learning') do
-        sudo :pip, :install, '-r requirements.txt'
+        sudo :pip, :install, '-r requirements.txt --no-binary :all:'
       end
       sudo :supervisorctl, :restart, :engine, '-c /etc/supervisord.conf'
     end

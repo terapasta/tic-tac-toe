72157cf21bb6dce888f9ff8521d338909aa2ec44
kozo yamagata <tune002@gmail.com>
2017-12-13 15:18:10 +0900

    Fix spec

--- spec/models/bot/microsoft_credential_spec.rb
+++ spec/models/bot/microsoft_credential_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe MicrosoftCredential, type: :model do
+RSpec.describe Bot::MicrosoftCredential, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end

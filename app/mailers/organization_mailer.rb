83a5f745e3f0fe3a3e0a64ab8c2b8dfc2248ebde
kozo yamagata <tune002@gmail.com>
2017-12-18 15:28:34 +0900

    Fix typo

--- app/mailers/organization_mailer.rb
+++ app/mailers/organization_mailer.rb
@@ -2,7 +2,7 @@ class OrganizationMailer < ApplicationMailer
   default from: 'noreply@mof-mof.co.jp'
 
   def notify_before_1week_of_finishing_trial(organization)
-    @orgnization = organization
+    @organization = organization
     mail(
       to: @organization.users.map(&:email),
       subject: '[My-ope] フリープランの終了まであと1週間です'

2d95a47859417d37784b3337cd8bcc801cbca41d
shwld <shu.account@outlook.com>
2017-12-19 17:37:29 +0900

    Unsupport branch import and export

--- app/decorators/concerns/question_answers_csv_generatable.rb
+++ app/decorators/concerns/question_answers_csv_generatable.rb
@@ -4,32 +4,14 @@ module QuestionAnswersCsvGeneratable
   include CsvGeneratable
 
   def to_csv(encoding: :utf8)
-    headers = [:id, :topic_tags, :question, :answer, :decision_branches].map{|x| QuestionAnswer.human_attribute_name x}
+    headers = [:id, :topic_tags, :question, :answer].map{|x| QuestionAnswer.human_attribute_name x}
     super(encoding: encoding.to_sym, header_columns: headers) do |csv, item|
-      recursive_put_rows_to_csv(csv,
-        [
-          item.id,
-          item.topic_tags.map(&:name).join('/').presence,
-          item.question,
-          item.answer,
-        ],
-        item.decision_branches,
-      )
+      csv << [
+        item.id,
+        item.topic_tags.map(&:name).join('/').presence,
+        item.question,
+        item.answer,
+      ]
     end
   end
-
-  private
-    def recursive_put_rows_to_csv(csv, base, decision_branches)
-      row = base.dup
-      if decision_branches.any?
-        decision_branches.each do |decision_branch|
-          row2 = row.dup
-          row2 << decision_branch.body
-          row2 << decision_branch.answer
-          recursive_put_rows_to_csv(csv, row2, decision_branch.child_decision_branches)
-        end
-      else
-        csv << row
-      end
-    end
 end
--- app/models/question_answer/csv_importer.rb
+++ app/models/question_answer/csv_importer.rb
@@ -20,12 +20,12 @@ class QuestionAnswer::CsvImporter
   end
 
   def import
-    csv_data = embed_record_id(parse)
+    csv_data = parse
     ActiveRecord::Base.transaction do
       csv_data.each do |import_param|
         topic_tag_names = import_param.delete(:topic_tag_names)
 
-        # Q&Aのidを持っているか、idでレコードが見つかったら更新
+        # Q&Aのidでレコードが見つかったら更新
         # それ以外は作成する
         question_answer = begin
           if import_param[:id].present? &&
@@ -61,11 +61,6 @@ class QuestionAnswer::CsvImporter
       @current_row = index + 2
       data = detect_or_initialize_by_row(row)
 
-      decision_branches = Array(out[data[:key]].try(:dig, :decision_branches_attributes))
-      decision_branches.push(data[:decision_branch]) if data[:decision_branch].present?
-
-      decision_branches = deep_merge_db(decision_branches)
-
       topic_tag_names = Array(out[data[:key]].try(:dig, :topic_tag_names))
       topic_tag_names += Array(data[:topic_tag_names])
 
@@ -73,46 +68,10 @@ class QuestionAnswer::CsvImporter
         id: data[:id],
         question: data[:question],
         answer: data[:answer],
-        decision_branches_attributes: decision_branches,
         topic_tag_names: topic_tag_names,
       }
       out
     }.values
-  end
-
-  ##
-  # 全てのnodeをチェックして全文一致するレコードがあればidをセットする
-  # 選択肢は#deep_embed_db_idで再帰でidをセットする
-  def embed_record_id(tree)
-    tree.each do |qa_node|
-      next if qa_node[:id].blank?
-      qa = @bot.question_answers.find_by(id: qa_node[:id])
-      next if qa.blank?
-
-      qa_node[:decision_branches_attributes].each do |db_node|
-        db = qa.decision_branches.find_by(body: db_node[:body])
-        next if db.blank?
-        db_node[:id] = db.id
-        db_node = deep_embed_db_id(db, db_node)
-      end
-    end
-    tree
-  end
-
-  ##
-  # embed_record_idで呼び出される
-  # 選択肢のnodeを再帰でチェックしてbodyが一致するレコードがあればidをセットする
-  def deep_embed_db_id(db, db_node)
-    child_db_nodes = db_node[:child_decision_branches_attributes]
-    return db_node if child_db_nodes.blank?
-
-    child_db_nodes.each do |child_db_node|
-      child_db = db.child_decision_branches.find_by(body: child_db_node[:body])
-      next if child_db.blank?
-      child_db_node[:id] = child_db.id
-      child_db_node = deep_embed_db_id(child_db, child_db_node)
-    end
-    db_node
   end
 
   ##
@@ -122,15 +81,6 @@ class QuestionAnswer::CsvImporter
     topic_tag_names = sjis_safe(row[1])&.gsub('／', '/')&.split("/")&.map(&:strip) || []
     q = sjis_safe(row[2])
     a = sjis_safe(row[3])
-
-    # 選択肢と回答が入れ子になった状態で取得する
-    decision_branch = row[4..-1].each_slice(2).map { |pair|
-      # 選択肢のbodyがある場合のみデータを作る
-      pair.first.present? ? { bot_id: @bot.id, body: sjis_safe(pair.first), answer: sjis_safe(pair.second) } : nil
-    }.compact.reverse.reduce { |tail, parent|
-      parent[:child_decision_branches_attributes] = [tail]
-      parent
-    }
 
     bot_had = @bot.question_answers.detect{ |qa| qa.id == id }.present?
     fail EmptyQuestionError.new if q.blank?
@@ -142,27 +92,6 @@ class QuestionAnswer::CsvImporter
       topic_tag_names: topic_tag_names,
       question: q,
       answer: a,
-      decision_branch: decision_branch,
-    }
-  end
-
-  ##
-  # 選択肢を再帰でチェックして同じ内容ならマージする
-  def deep_merge_db(db_nodes)
-    db_nodes.inject([]) { |res, db|
-      same_db = res.detect{ |_db|
-        _db[:body] == db[:body] && _db[:answer] == db[:answer] }
-      if same_db.present?
-        same_db[:child_decision_branches_attributes].concat(
-          db[:child_decision_branches_attributes])
-        db = same_db
-      else
-        res.push(db)
-      end
-      if db[:child_decision_branches_attributes].present?
-        db[:child_decision_branches_attributes] = deep_merge_db(db[:child_decision_branches_attributes])
-      end
-      res
     }
   end
 

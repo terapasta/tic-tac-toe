ad7778975a7ffbca1fbd11256e0ec0f76766544f
shwld <shu.account@outlook.com>
2017-12-19 12:11:04 +0900

    jobにしておくか

--- app/controllers/admin/accuracy_test_cases/executions_controller.rb
+++ app/controllers/admin/accuracy_test_cases/executions_controller.rb
@@ -1,17 +1,7 @@
 class Admin::AccuracyTestCases::ExecutionsController < ApplicationController
   def create
-    Delayed::Job.enqueue ExecuteAllBotTestsJob.new
+    Delayed::Job.enqueue AllBotAccuracyTestsJob.new
 
     redirect_to root_path, notice: 'Succeeded all bot accuracy tests'
-  end
-
-  class ExecuteAllBotTestsJob
-    include ReplyTestable
-    def perform
-      helper = SlackBotHelper.new
-      attachments = helper.generate_attachments(all_bot_accuracy_test!)
-      notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
-      notifier.post text: '現在の各ボットの正答率ですよ', channel: '#dev', attachments: attachments
-    end
   end
 end

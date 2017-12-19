d575683ca73f457a372d5e1a07d225526c66f722
shwld <shu.account@outlook.com>
2017-12-19 16:05:44 +0900

    Add csv header to messages-log-file

--- app/decorators/bot_threads_messages_decorator.rb
+++ app/decorators/bot_threads_messages_decorator.rb
@@ -2,7 +2,9 @@ class BotThreadsMessagesDecorator < Draper::CollectionDecorator
   include CsvGeneratable
 
   def to_csv(encoding: :utf8)
-    super do |csv, chat|
+    headers = [:chat_id, :id, :speaker, :body, :answer_failed, :rating_level, :created_at, :user_agent]
+      .map{|x| Message.human_attribute_name x}
+    super(header_columns: headers) do |csv, chat|
       #chatオブジェクトのorderbyとlimitは無効。superのto_csv内で行われているfind_eachで無視される為。
       put_rows_to_csv(csv, chat)
     end

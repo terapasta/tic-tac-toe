020fab3115d38b63e4af54166b7072d6b8ccd001
kozo yamagata <tune002@gmail.com>
2017-12-19 11:02:46 +0900

    Disable compression js in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,10 @@
+class NoCompression
+  def compress(string)
+    # do nothing
+    string
+  end
+end
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -26,7 +33,8 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = NoCompression.new
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.

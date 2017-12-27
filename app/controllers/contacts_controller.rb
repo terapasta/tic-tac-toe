a1ab0616d9bfa71b9679107f3387a405b0ee2725
kozo yamagata <tune002@gmail.com>
2017-12-27 18:33:45 +0900

    Fix support form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,18 +5,18 @@ class ContactsController < ApplicationController
   def create
     client = ZendeskClient.shared_client
     ticket = ZendeskAPI::Ticket.new(client,
-      requester:   { email: params[:ticket][:email] },
-      subject:     params[:ticket][:subject],
+      subject:     "サポートフォームからのお問い合わせ<#{current_user.email}>",
       description: params[:ticket][:description]
     )
     if ticket.save
       redirect_to new_contacts_path, notice: 'お問い合わせが完了しました。'
     else
-      redirect_to new_contacts_path, alert: <<~MSG
+      flash.now.alert = <<~MSG
         お問い合わせに失敗しました。内容を確認してください。
         ※メールアドレスは入力されましたか?
         ※お問い合わせ内容は入力されましたか?
       MSG
+      render :new
     end
   end
 end

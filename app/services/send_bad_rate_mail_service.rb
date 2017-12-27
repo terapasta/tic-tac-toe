b1994e16eac1d667db6b92c135240314997c13cf
shwld <shu.account@outlook.com>
2017-12-27 10:11:37 +0900

    Add comment

--- app/services/send_bad_rate_mail_service.rb
+++ app/services/send_bad_rate_mail_service.rb
@@ -8,6 +8,7 @@ class SendBadRateMailService
   def send_mail
     return if @current_user
     if @message.bot? && @message.rating&.bad?
+      # Note: bad評価理由の入力があった場合に一緒に通知したいので、3分待ってからメールを送信する
       BadRateMailer.create(@message, task: @task).deliver_later(wait: 3.minutes)
     end
   end

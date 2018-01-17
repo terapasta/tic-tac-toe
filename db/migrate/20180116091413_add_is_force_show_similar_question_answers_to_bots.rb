ae66b5bcb38e9943327b463e56020c215d620cfd
kozo yamagata <tune002@gmail.com>
2018-01-17 16:39:11 +0900

    Revert "Add is_force_show_similar_question_answers column to bots"
    This reverts commit 592f8885d2c48291eb6aebe0e42fc943ccb97cc1.

--- db/migrate/20180116091413_add_is_force_show_similar_question_answers_to_bots.rb
+++
@@ -1,5 +0,0 @@
-class AddIsForceShowSimilarQuestionAnswersToBots < ActiveRecord::Migration[5.1]
-  def change
-    add_column :bots, :is_force_show_similar_question_answers, :boolean, default: false, null: false
-  end
-end

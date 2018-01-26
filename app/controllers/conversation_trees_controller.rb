07e6174f768f56985b330fc597c2473adc9002c1
kozo yamagata <tune002@gmail.com>
2018-01-26 17:19:46 +0900

    Improve performance of conversation_trees#show

--- app/controllers/conversation_trees_controller.rb
+++ app/controllers/conversation_trees_controller.rb
@@ -3,7 +3,7 @@ class ConversationTreesController < ApplicationController
 
   def show
     @bot = bots.find params[:bot_id]
-    @question_answers = @bot.question_answers.includes(decision_branches: [:child_decision_branches]).order(created_at: :desc).decorate
+    @question_answers = @bot.question_answers.includes(:sub_questions).order(created_at: :desc).decorate
     # @answers = @bot.answers
     #   .includes(:decision_branches)
     #   .top_level(@bot.id)

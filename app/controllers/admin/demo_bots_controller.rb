6c402e40a9a3003b38e741a3eae6c8d4453f2ad7
Matsuzawa Y <ymatsuzawa1485@gmail.com>
2018-02-09 19:44:00 +0900

    Add dialogue to DemoBotsController#update and view

--- app/controllers/admin/demo_bots_controller.rb
+++ app/controllers/admin/demo_bots_controller.rb
@@ -4,4 +4,8 @@ class Admin::DemoBotsController < ApplicationController
     @demobots = Bot.where(is_demo: true).order(created_at: :desc)
   end
 
+  def update
+    redirect_to admin_demo_bots_path
+  end
+
 end

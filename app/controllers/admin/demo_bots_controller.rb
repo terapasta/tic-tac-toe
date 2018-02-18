7f4f7aa1661608ed9067b806652af564a2f00d4d
Matsuzawa Y <ymatsuzawa1485@gmail.com>
2018-02-19 07:02:51 +0900

    Fix DemoBotsController#update

--- app/controllers/admin/demo_bots_controller.rb
+++ app/controllers/admin/demo_bots_controller.rb
@@ -1,11 +1,37 @@
 class Admin::DemoBotsController < ApplicationController
+  protect_from_forgery except: :update 
+  before_action :set_demobot, only: [:update]
 
   def index
     @demobots = Bot.where(is_demo: true).order(created_at: :desc)
   end
 
   def update
-    redirect_to admin_demo_bots_path
+    if @demobot.update_attributes(token: regenerate_token)
+      @orgs = @demobot.organizations.all
+      @orgs.each do |org|
+        @users = org.users.all
+        @users.each do |user|
+          repassword = reset_password
+          user.update_attributes(password: repassword, password_confirmation: repassword)
+        end
+      end
+      redirect_to admin_demo_bots_path
+    else
+      render :index
+    end
   end
 
+  private
+    def set_demobot
+      @demobot = Bot.find(params[:id])
+    end
+
+    def regenerate_token
+      SecureRandom.hex(32)
+    end
+
+    def reset_password
+      SecureRandom.hex(10)
+    end
 end

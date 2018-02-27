7a6f6d89e6be24059b5f514437b13be08fe44734
kozo yamagata <tune002@gmail.com>
2018-02-27 13:08:15 +0900

    Update webpacker config for production

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,2 +1,26 @@
+const webpack = require('webpack')
 const config = require('./environment')
+
+config.plugins = config.plugins.map(plug => {
+  if (plug instanceof webpack.optimize.UglifyJsPlugin) {
+    return new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      parallel: true,
+      mangle: false,
+      uglifyOptions: {
+        mangle: false
+      },
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    });
+  }
+  return plug;
+})
+
+config.devtool = 'eval'
+
 module.exports = config

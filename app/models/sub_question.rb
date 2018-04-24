fa05e1aba999abc4254d299c2ad34a1d50262858
kozo yamagata <tune002@gmail.com>
2018-04-24 16:14:43 +0900

    Add sub_questions in target of WordMapping.replace_synonym_all

--- app/models/sub_question.rb
+++ app/models/sub_question.rb
@@ -3,11 +3,15 @@ class SubQuestion < ApplicationRecord
   validates :question, presence: true
 
   before_validation do
-    Wakatifier.apply(question).tap do |wq|
+    wakatify_question.tap do |wq|
       if question_answer.try(:bot).try(:present?)
         wq = WordMapping.for_bot(question_answer.bot).decorate.replace_synonym(wq)
       end
       self.question_wakati = wq
     end
+  end
+
+  def wakatify_question
+    Wakatifier.apply(question)
   end
 end

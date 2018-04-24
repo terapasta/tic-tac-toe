4fdcc10d896e47139348c513e933e221f23b95ba
kozo yamagata <tune002@gmail.com>
2018-04-24 17:24:15 +0900

    Modify getting question_answers data to use question_answers table instead of learning_training_messages

--- learning/app/shared/datasource/database/question_answers.py
+++ learning/app/shared/datasource/database/question_answers.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from app.shared.datasource.database.database import Database
 from app.shared.base_cls import BaseCls
 
@@ -10,13 +11,36 @@ class QuestionAnswers(BaseCls):
         self.database = Database()
 
     def all(self):
-        return self.database.select(
-                "select * from learning_training_messages;",
-                params={}
-            )
+        question_answers = self.database.select(
+            "select * from question_answers;",
+            params={}
+        )
+        sub_questions = self.database.select(
+            "select * from sub_questions;",
+            params={}
+        )
+        return self.__make_data(question_answers, sub_questions)
 
     def by_bot(self, bot_id):
-        return self.database.select(
-                "select * from learning_training_messages where bot_id = %(bot_id)s;",
-                params={"bot_id": bot_id}
-            )
+        question_answers = self.database.select(
+            "select * from question_answers where bot_id = %(bot_id)s;",
+            params={"bot_id": bot_id}
+        )
+        sub_questions = self.database.select(
+            "select * from sub_questions where question_answer_id in (select question_answers.id from question_answers where question_answers.bot_id = %(bot_id)s);",
+            params={"bot_id": bot_id}
+        )
+        return self.__make_data(question_answers, sub_questions)
+
+
+    def __make_data(question_answers, sub_questions):
+        data1 = pd.DataFrame({
+            'question_id': question_answers['id'],
+            'question': question_answers['question_wakati']
+        })
+        data2 = pd.DataFrame({
+            'question_id': sub_questions['question_answer_id'],
+            'question': sub_questions['question_wakati']
+        })
+        result = pd.concat([data1, data2])
+        return result

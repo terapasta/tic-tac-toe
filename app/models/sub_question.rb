83cf21076727a5585c1d668601077d1199717eea
kozo yamagata <tune002@gmail.com>
2018-04-24 16:08:55 +0900

    Define before_validation callback in SubQuestion model

--- app/models/sub_question.rb
+++ app/models/sub_question.rb
@@ -1,4 +1,13 @@
 class SubQuestion < ApplicationRecord
   belongs_to :question_answer
   validates :question, presence: true
+
+  before_validation do
+    Wakatifier.apply(question).tap do |wq|
+      if question_answer.try(:bot).try(:present?)
+        wq = WordMapping.for_bot(question_answer.bot).decorate.replace_synonym(wq)
+      end
+      self.question_wakati = wq
+    end
+  end
 end

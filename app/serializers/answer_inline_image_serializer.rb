0762788b13f65524429b900ba4d9fedd6ecb74a6
kozo yamagata <tune002@gmail.com>
2018-05-09 16:46:58 +0900

    Add AnswerInlineImageSerializer#file method

--- app/serializers/answer_inline_image_serializer.rb
+++ app/serializers/answer_inline_image_serializer.rb
@@ -1,3 +1,11 @@
 class AnswerInlineImageSerializer < ActiveModel::Serializer
   attributes :file
+
+  def file
+    if Rails.env.development?
+      { url: 'http://localhost:3000' + object.file_url }
+    else
+      object.file
+    end
+  end
 end

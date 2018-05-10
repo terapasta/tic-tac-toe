646c330d70e1f08dbc7c6d74cf4943f830e458d6
kozo yamagata <tune002@gmail.com>
2018-05-10 17:02:55 +0900

    Fix bug of answer_failed_mailer

--- app/mailers/answer_failed_mailer.rb
+++ app/mailers/answer_failed_mailer.rb
@@ -6,6 +6,9 @@ class AnswerFailedMailer < ApplicationMailer
     @bot_message = message
     @chat = message.chat
     @guest_user = @chat.guest_user
+    if @guest_user.present?
+      @guest_name = " (#{[@guest_user.name, @guest_user.email].select(&:present?).join(' ')})"
+    end
     @guest_message = Chat.question_message(message.chat_id, message.id)
     @task = task
     unless @bot_users.length.zero?

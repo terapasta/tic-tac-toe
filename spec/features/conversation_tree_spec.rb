8ee597a93cf4ff376111805d7ef4cabe6d10f3f7
kozo yamagata <tune002@gmail.com>
2018-05-14 12:05:27 +0900

    Delete page.save_screenshot lines

--- spec/features/conversation_tree_spec.rb
+++ spec/features/conversation_tree_spec.rb
@@ -164,7 +164,6 @@ RSpec.describe 'ConversationTree', type: :feature, js: true do
 
   scenario 'filter by topic_tags' do
     find('.multiselect__tags').click
-    page.save_screenshot
     all('.multiselect__element').detect{ |it| it.text == topic_tags.first.name }.click
     expect(page).to have_content(question_answers.first.question)
     expect(page).to_not have_content(question_answers.second.question)

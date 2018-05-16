2b5734e7a48237f790dfea3aacf55698092f4c3f
chase0213 <chase0213+dev@gmail.com>
2018-05-16 15:48:53 +0900

    remove global variable and add phase indicator into context

--- learning/app/controllers/benchmark_controller.py
+++ learning/app/controllers/benchmark_controller.py
@@ -1,7 +1,7 @@
 from app.shared.logger import logger
 from app.shared.base_cls import BaseCls
 from app.shared.benchmark import Benchmark
-from app.shared.learning_phase import set_learning_phase
+from app.shared.constants import Constants
 from app.shared.document_generator import DocumentGenerator
 from app.controllers.learn_controller import LearnController
 from app.controllers.reply_controller import ReplyController
@@ -19,18 +19,20 @@ class BenchmarkController(BaseCls):
     def __init__(self, context):
         self.bot = context.current_bot
         self.factory = context.get_factory()
+
+        context.phase = Constants.PHASE_LEARNING
         self.learn_controller = LearnController(context)
+
+        context.phase = Constants.PHASE_REPLYING
         self.reply_controller = ReplyController(context)
 
     def perform(self):
         # 評価前に学習する
         logger.setLevel(logging.WARNING)
-        set_learning_phase(True)
         self.learn_controller.perform()
 
         logger.setLevel(logging.INFO)
         logger.info('load test data for evaluation')
-        set_learning_phase(False)
 
         expected, actual = self._load_test_data()
 

a98d3d61b124f4fd86ef644b2847e9a4663b147e
kozo yamagata <tune002@gmail.com>
2018-05-21 16:58:48 +0900

    Showing up zndesk alert if needed

--- app/serializers/question_answer_serializer.rb
+++ app/serializers/question_answer_serializer.rb
@@ -1,5 +1,5 @@
 class QuestionAnswerSerializer < ActiveModel::Serializer
-  attributes :id, :question, :answer
+  attributes :id, :question, :answer, :zendesk_article_id
   has_many :decision_branches
   has_many :sub_questions
   has_many :answer_files

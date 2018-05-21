aefb538c3aa3d4922b5c9fdd4790bcb2e9fd868d
kozo yamagata <tune002@gmail.com>
2018-05-21 16:19:16 +0900

    Modify to use uuid in AnswerInlineImage's file url

--- app/models/answer_inline_image.rb
+++ app/models/answer_inline_image.rb
@@ -2,6 +2,11 @@ class AnswerInlineImage < ApplicationRecord
   belongs_to :bot
 
   validates :file, presence: true
+  validates :uuid, presence: true
 
   mount_uploader :file, AnswerInlineImageUploader
+
+  before_validation do
+    self.uuid ||= SecureRandom.uuid
+  end
 end
--- app/serializers/answer_inline_image_serializer.rb
+++ app/serializers/answer_inline_image_serializer.rb
@@ -1,11 +1,13 @@
+require 'carrierwave/storage/fog'
+
 class AnswerInlineImageSerializer < ActiveModel::Serializer
   attributes :file
 
   def file
-    if Rails.env.development?
-      { url: 'http://localhost:3000' + object.file_url }
-    else
+    if CarrierWave::Uploader::Base.storage == CarrierWave::Storage::Fog
       object.file
+    else
+      { url: 'http://localhost:3000' + object.file_url }
     end
   end
 end

bd22b46b42f11afe96354b894f3f0cfd24a261d2
kozo yamagata <tune002@gmail.com>
2018-06-05 17:14:20 +0900

    Implement GuestMessagesChart

--- app/javascript/vue/GuestMessagesChart.vue
+++ app/javascript/vue/GuestMessagesChart.vue
@@ -1,15 +1,76 @@
-<template>
-  <div>
-    guest messages chart
-  </div>
-</template>
-
 <script>
-export default {
+import C3 from 'c3'
+import 'c3/c3.min.css'
 
+import dateFnsformatDate from 'date-fns/format'
+import parseDate from 'date-fns/parse'
+import addDays from 'date-fns/add_days'
+import jaLocale from 'date-fns/locale/ja'
+
+import max from 'lodash/max'
+
+const DateFormat = 'YYYY-MM-DD dd'
+const formatDate = date => dateFnsformatDate(date, DateFormat, { locale: jaLocale })
+
+export default {
+  props: {
+    columns: { type: Array, required: true }
+  },
+
+  mounted () {
+    this.$nextTick(() => {
+      this.renderChart()
+    })
+  },
+
+  computed: {
+    yAxisMax () {
+      return max(this.columns[1].slice(1)) + 10
+    }
+  },
+
+  methods: {
+    renderChart() {
+      this.chart = C3.generate({
+        bindto: this.$refs.chart,
+        size: {
+          height: 300
+        },
+        data: {
+          x: 'x',
+          columns: this.columns,
+          type: 'line',
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format (d) {
+                const date = parseDate(d)
+                return `${formatDate(date)} 〜 ${formatDate(addDays(date, 6))}`
+              }
+            }
+          },
+          y: {
+            padding: 0,
+            max: this.yAxisMax,
+            min: 0,
+          }
+        },
+        legend: {
+          show: false
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<template>
+  <div>
+    <div ref="chart" />
+  </div>
+</template>
 
+<style>
 </style>

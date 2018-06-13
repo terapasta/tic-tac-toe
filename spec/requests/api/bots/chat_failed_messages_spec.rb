5356b3b2f668c2e50218d56cd1975ea550bceb80
kozo yamagata <tune002@gmail.com>
2018-06-13 16:06:41 +0900

    Implement 'POST /api/bots/chat_failed_messages' API endpoint

---
+++ spec/requests/api/bots/chat_failed_messages_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe '/api/bots/:token/chat_messages', type: :request do
+  let!(:bot) do
+    create(:bot)
+  end
+
+  let!(:chat) do
+    create(:chat, bot_id: bot.id, guest_key: guest_key)
+  end
+
+  let(:guest_key) do
+    'example guest key'
+  end
+
+  let(:message) do
+    'example message'
+  end
+
+  describe 'POST /api/bots/:bot_token/chat_failed_messages' do
+    it 'always creates failed message' do
+      post "/api/bots/#{bot.token}/chat_failed_messages", params: {
+        guest_key: guest_key,
+        message: message
+      }
+
+      JSON.parse(response.body).tap do |json|
+        json['messages'][0].tap do |guest_message|
+          expect(guest_message['body']).to eq(message)
+        end
+        json['messages'][1].tap do |bot_message|
+          expect(bot_message['body']).to eq(DefinedAnswer.classify_failed_text)
+          expect(bot_message['answerFailed']).to be
+        end
+      end
+    end
+  end
+end

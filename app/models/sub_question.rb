bbcb47fd66aeec3eca3903b5363e9689c8d47c49
kozo yamagata <tune002@gmail.com>
2018-06-22 11:29:15 +0900

    Revert "Revert "[fixed #156791570] staffは形態素解析された文章をQ&Aで確認できる""
    This reverts commit 29f9c5a923798a129b07179b79f7bb856775cb77.

--- app/models/sub_question.rb
+++ app/models/sub_question.rb
@@ -5,4 +5,17 @@ class SubQuestion < ApplicationRecord
   def sub_question?
     true
   end
+
+  before_validation do
+    wakatify_question.tap do |wq|
+      if question_answer.try(:bot).try(:present?)
+        wq = WordMapping.for_bot(question_answer.bot).decorate.replace_synonym(wq)
+      end
+      self.question_wakati = wq
+    end
+  end
+
+  def wakatify_question
+    Wakatifier.apply(question)
+  end
 end
--- lib/tasks/bot.rake
+++ lib/tasks/bot.rake
@@ -2,6 +2,7 @@ namespace :bot do
   task learn_all: :environment do
     Bot.pluck(:id).each do |bot_id|
       LearnJob.perform_later(bot_id)
+      bot.update(learning_status: :processing)
       pp "Enqueued learn job for #{bot_id}"
     end
   end

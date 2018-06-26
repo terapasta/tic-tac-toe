d1bb3560d518388b1f9775c338b5771b7ff1bf63
kozo yamagata <tune002@gmail.com>
2018-06-26 11:29:39 +0900

    Implement generating access_token for ChatworkDecisionBranch

--- spec/factories/chatwork_decision_branches.rb
+++ spec/factories/chatwork_decision_branches.rb
@@ -1,7 +1,4 @@
 FactoryGirl.define do
   factory :chatwork_decision_branch do
-    chat_id 1
-    access_token "MyString"
-    decision_branch_id 1
   end
 end
--- spec/models/chatwork_decision_branch_spec.rb
+++ spec/models/chatwork_decision_branch_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe ChatworkDecisionBranch, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#generate_access_token' do
+    subject do
+      ChatworkDecisionBranch.new.generate_access_token
+    end
+
+    it { is_expected.to match /^[a-zA-Z0-9]{6}$/ }
+  end
+
+  describe '#set_access_token_if_needed' do
+    let(:chatwork_decision_branch) do
+      build(:chatwork_decision_branch)
+    end
+
+    subject do
+      chatwork_decision_branch.set_access_token_if_needed
+      chatwork_decision_branch.access_token
+    end
+
+    it { is_expected.to match /^[a-zA-Z0-9]{6}$/ }
+  end
 end

5a290e6c7a834d2f2bbda194528612c15eb6154e
kozo yamagata <kozo002@users.noreply.github.com>
2018-06-29 15:32:27 +0900

    Revert "[fixed #156791570] staffは形態素解析された文章をQ&Aで確認できる"

--- app/utils/wakatifier.rb
+++
@@ -1,5 +0,0 @@
-module Wakatifier
-  def self.apply(text)
-    Natto::MeCab.new('-Owakati').parse(text.to_s).sub(/\n$/, '')
-  end
-end
--- db/migrate/20180424070351_add_question_wakati_to_sub_questions.rb
+++
@@ -1,5 +0,0 @@
-class AddQuestionWakatiToSubQuestions < ActiveRecord::Migration[5.1]
-  def change
-    add_column :sub_questions, :question_wakati, :text
-  end
-end

ec230089f5372ead98f7d3b573091e83bda940db
kozo yamagata <tune002@gmail.com>
2018-07-26 10:17:16 +0900

    Show loader in conversation tree

--- app/javascript/vue/ConversationTree/components/Tree.vue
+++ app/javascript/vue/ConversationTree/components/Tree.vue
@@ -1,6 +1,7 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 import chunk from 'lodash/chunk'
+import BeatLoader from 'vue-spinner/src/BeatLoader'
 
 import QuestionNode from './QuestionNode'
 import classnames from 'classnames'
@@ -13,12 +14,14 @@ export default {
   },
 
   components: {
-    QuestionNode
+    QuestionNode,
+    BeatLoader
   },
 
   computed: {
     ...mapState([
-      'isOnlyShowHasDecisionBranchesNode'
+      'isOnlyShowHasDecisionBranchesNode',
+      'isLoading'
     ]),
 
     isActiveAddButton () {
@@ -47,20 +50,22 @@ export default {
 <template>
   <ol class="tree">
     <li class="tree__node" id="adding">
-      <label>
-        <input
-          type="checkbox"
-          :checked="isOnlyShowHasDecisionBranchesNode"
-          @change="handleCheckBoxChange"
-          id="toggleOnlyShowHasDecisionBranchesNode"
-        />
-        &nbsp;
-        <span class="icon-circle">
-          <i class="material-icons upside-down" title="選択肢" style="top:-1px">call_split</i>
-        </span>
-        ツリーのみ表示
-      </label>
-      <br />
+      <div class="d-flex justify-content-between narrow-box">
+        <label>
+          <input
+            type="checkbox"
+            :checked="isOnlyShowHasDecisionBranchesNode"
+            @change="handleCheckBoxChange"
+            id="toggleOnlyShowHasDecisionBranchesNode"
+          />
+          &nbsp;
+          <span class="icon-circle">
+            <i class="material-icons upside-down" title="選択肢" style="top:-1px">call_split</i>
+          </span>
+          ツリーのみ表示
+        </label>
+        <beat-loader v-if="isLoading" size="8px" color="#F36B30" />
+      </div>
       <div :class="addButtonClassName">
         <router-link
           id="AddQuestionAnswerButton"
@@ -74,3 +79,10 @@ export default {
     </template>
   </ol>
 </template>
+
+<style scoped>
+.narrow-box {
+  box-sizing: content-box;
+  width: calc(30% - 52px)
+}
+</style>

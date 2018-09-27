685a54ec79beaa1cb529a0769cce848a2b855b1a
chase0213 <chase0213+dev@gmail.com>
2018-09-28 03:45:11 +0900

    fix typo

--- app/models/bot/slack_credential.rb
+++ app/models/bot/slack_credential.rb
@@ -1,6 +1,6 @@
 class Bot::SlackCredential < ApplicationRecord
   belongs_to :bot
-  validate :team_id, presence: true
+  validates :team_id, presence: true
   validates :microsoft_app_id, presence: true
   validates :microsoft_app_password, presence: true
 end

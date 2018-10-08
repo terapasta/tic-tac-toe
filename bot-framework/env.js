33e05d655e3af29789e4e8a0fa07774a3eb89170
chase0213 <chase0213+dev@gmail.com>
2018-10-08 20:48:41 +0900

    add some environment variables and a function to load envfile

--- bot-framework/env.js
+++ bot-framework/env.js
@@ -1,16 +1,29 @@
 const path = require('path')
 const fs = require('fs')
 
+const getEnv = (path) => {
+  try {
+    // 指定された場所にファイルがあれば、そこから環境変数を読み込む
+    return fs.readFileSync(envFile, { encoding: 'UTF-8' })
+      .split("\n")
+      .filter(it => it !== '')
+      .map(it => it.split('='))
+      .reduce((acc, it) => { acc[it[0]] = it[1]; return acc }, {})
+  } catch (err) {
+    // ファイルが見つからない場合、空のオブジェクトを返す
+    return {}
+  }
+}
+
 const envFile = path.dirname(__dirname) + '/.env'
-const env = fs.readFileSync(envFile, { encoding: 'UTF-8' })
-  .split("\n")
-  .filter(it => it !== '')
-  .map(it => it.split('='))
-  .reduce((acc, it) => { acc[it[0]] = it[1]; return acc }, {})
+const env = getEnv(envFile)
 
 module.exports = Object.assign(env, {
   NODE_ENV: process.env.NODE_ENV || 'development',
   MYOPE_API_URL: process.env.MYOPE_API_URL || 'http://localhost:3000',
   AZURE_PROJECT_NAME: process.env.AZURE_PROJECT_NAME,
-  AZURE_PASSWORD: process.env.AZURE_PASSWORD,
+  AZURE_PROJECT_PASSWORD: process.env.AZURE_PROJECT_PASSWORD,
+  MicrosoftAppId: process.env.MicrosoftAppId,
+  MicrosoftAppPassword: process.env.MicrosoftAppPassword,
+  BOT_TOKEN: process.env.BOT_TOKEN,
 })

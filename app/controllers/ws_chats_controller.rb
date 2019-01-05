43c84e796dc5c37a48cbe895a648c5ff456a3762
kozo yamagata <tune002@gmail.com>
2019-01-05 17:09:48 +0900

    Make base of ws_chats#show page

--- app/controllers/ws_chats_controller.rb
+++ app/controllers/ws_chats_controller.rb
@@ -1,4 +1,24 @@
 class WsChatsController < ApplicationController
+  include ChatOperable
+
   def show
+    set_bot
+    set_guest_key
+    set_guest_user
+    set_warning_message
+    @chat = @bot.chats.find_or_create_by(
+      guest_key: guest_key,
+      is_staff: !!current_user.try(:staff?),
+      is_normal: !!current_user.try(:normal?)
+    )
+    authorize @chat
+    if need_password_view?
+      render_password_view and return
+    end
   end
+
+  private
+    def chat_path
+      ws_chat_path(@bot.token)
+    end
 end
---
+++ spec/features/chats_password_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'Chats Password', type: :feature, js: true do
+  let!(:bot) do
+    create(:bot, password: password)
+  end
+
+  let(:new_chat_page) do
+    "/embed/#{bot.token}/chats/new"
+  end
+
+  let(:password) do
+    'hoge'
+  end
+
+  feature 'when input correct password' do
+    scenario do
+      visit new_chat_page
+      expect(page).to have_content('パスワードを入力してください')
+      fill_in 'bot[password]', with: password
+      click_button '送信'
+      sleep 1
+      expect(page).to have_content(bot.name)
+
+      visit new_chat_page
+      expect(page).to_not have_content('パスワードを入力してください')
+      expect(page).to have_content(bot.name)
+    end
+  end
+
+  feature 'when input incorrect password' do
+    scenario do
+      visit new_chat_page
+      expect(page).to have_content('パスワードを入力してください')
+      fill_in 'bot[password]', with: 'incorrect'
+      click_button '送信'
+      sleep 1
+      expect(page).to_not have_content(bot.name)
+      expect(page).to have_content('パスワードを入力してください')
+    end
+  end
+end

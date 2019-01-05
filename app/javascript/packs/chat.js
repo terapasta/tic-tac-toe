63adb47839e8b4cc743b89faa9497aaf11389bc3
kozo yamagata <tune002@gmail.com>
2019-01-05 18:25:49 +0900

    Make websocket connection of chat

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -1,6 +1,32 @@
 import ActionCable from 'actioncable'
+import Cookies from 'js-cookie'
+
+const guestKey = Cookies.get().guest_key
+const botToken = window.location.pathname.split('/')[2]
 
 const cable = ActionCable.createConsumer()
 const channel = cable.subscriptions.create({
-  channel: 'ChatChannel', bot_token: 'hogehoge'
+  channel: 'ChatChannel',
+  bot_token: botToken,
+  guest_key: guestKey
+}, {
+  connected () {
+    console.log('connected')
+  },
+
+  disconnected () {
+    console.log('disconnected')
+  },
+
+  rejected () {
+    console.log('rejected')
+  },
+
+  received (...args) {
+    console.log(...args)
+  }
 })
+
+channel.consumer.connection.webSocket.onerror = err => {
+  console.log('an error occurred')
+}

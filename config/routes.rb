37242ec2f682752155600f590432216725d4d499
kozo yamagata <tune002@gmail.com>
2019-01-07 11:17:48 +0900

    Remove route of chat_messages#create

--- config/routes.rb
+++ config/routes.rb
@@ -171,7 +171,6 @@ Rails.application.routes.draw do
     resources :guest_users, only: [:show, :create, :update, :destroy], param: :guest_key
     resources :bots, param: :token do
       resources :chats, module: :bots, only: [:show, :create]
-      resources :chat_messages, module: :bots, only: [:create]
       resources :chat_messages, module: :bots, only: [:index, :create]
       resources :chat_choices, module: :bots, only: [:create]
       resources :chat_failed_messages, module: :bots, only: [:create]

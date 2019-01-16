33e53d8177fc6e53abef8a2ef763a5824699541b
Lynn Sakashita <lynn.sakashita@gmail.com>
2019-01-16 11:41:40 +0900

    Spec 修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,6 @@ FactoryGirl.define do
     password 'hogehoge'
     role :normal
     sequence(:confirmation_token) { |n| "hogehoge#{n}" }
-    notification_settings nil
 
     trait :staff do
       role :staff

af7a588a7285492da5773d648e8fea7887b04f29
chase0213 <chase0213+dev@gmail.com>
2019-01-24 04:27:14 +0900

    resolve conflict to develop

--- app/controllers/concerns/guest_key_usable.rb
+++ app/controllers/concerns/guest_key_usable.rb
@@ -1,13 +1,14 @@
 module GuestKeyUsable
   extend ActiveSupport::Concern
-  
+
   included do
     helper_method :guest_key
   end
 
   private
     def set_guest_key
-      if cookies[:guest_key]&.length.to_i > 255 || guest_key.blank?
+      # 不正な guest_key が渡された場合は空文字列が返る
+      if guest_key.blank?
         cookies[:guest_key] = {
           value: make_guest_key,
           path: '/',
@@ -17,10 +18,27 @@ module GuestKeyUsable
     end
 
     def guest_key
-      cookies[:guest_key]
+      # NOTE:
+      # guest_key はクエリパラメータ（params）、クッキー（cookies）、ヘッダ（headers）の順に評価する
+      gk = params[:guest_key] ||
+           cookies[:guest_key] ||
+           request.headers['X-Guest-Key']
+
+      # guest_key が正しいフォーマットの場合のみ guest_key として使用
+      valid_key?(gk) ? gk : ""
+    end
+
+    def valid_key?(key)
+      if key.present?
+        key.match(/^[0-9a-f]{128}$/).present?
+      else
+        false
+      end
     end
 
     def make_guest_key
+      # 2 * 64文字の 16進数
+      # https://docs.ruby-lang.org/ja/latest/class/SecureRandom.html#S_HEX
       SecureRandom.hex(64)[0...255]
     end
 end

cba6d33c932208f175358176f653d1493983a728
Lynn Sakashita <lynn.sakashita@gmail.com>
2019-02-05 13:06:05 +0900

    Remove white space

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_roles, :all
 set :linked_files, %w{.env .python-version learning/config/config.yml config/database.yml}
 set :linked_dirs, %w{log tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads public/packs learning/dumps learning/logs node_modules}
 
-set :bundle_jobs, 4 
+set :bundle_jobs, 4
 set :unicorn_pid, "/tmp/unicorn.pid"
 set :unicorn_config_path, 'config/unicorn.rb'
 

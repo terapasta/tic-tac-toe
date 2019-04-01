75f9dfee1fe639b34144c571c2010fcc4a3a9026
YukiNaito <naito.yuki127834@gmail.com>
2019-04-01 15:12:50 +0900

    復活 bundled-es2015.js

---
+++ app/assets/javascripts/bundled-es2015.js
@@ -0,0 +1,46 @@
+(function e(t, n, r) { function s(o, u) { if (!n[o]) { if (!t[o]) { var a = typeof require == "function" && require; if (!u && a) return a(o, !0); if (i) return i(o, !0); var f = new Error("Cannot find module '" + o + "'"); throw f.code = "MODULE_NOT_FOUND", f } var l = n[o] = { exports: {} }; t[o][0].call(l.exports, function (e) { var n = t[o][1][e]; return s(n ? n : e) }, l, l.exports, e, t, n, r) } return n[o].exports } var i = typeof require == "function" && require; for (var o = 0; o < r.length; o++)s(r[o]); return s })({
+  1: [function (require, module, exports) {
+    // import promiseMiddleware from 'redux-promise';
+
+    // import ChatApp from "./components/chat/app";
+    // import ChatAppReducers from "./components/chat/reducers";
+    // import MessageRatingButtons from "./components/message-rating-buttons";
+    // import ConversationTree from "./components/conversation-tree-app/app";
+    // import ConversationTreeReducers from "./components/conversation-tree-app/reducers";
+    // import BotResetForm from "./components/bot-reset-form";
+    // import mountComponent, { mountComponentWithRedux } from "./modules/mount-component";
+    // import Mixpanel from "./analytics/mixpanel";
+    // import LearningButton from "./components/learning-button";
+    // import QuestionAnswerTagForm from "./components/question-answer-tag-form";
+    // import AnswerTextArea from "./components/answer-text-area";
+    // import EmbedCodeGenerator from "./components/embed-code-generator";
+    // import WordMappings from './components/WordMappings';
+
+    // window.initMessageRatingButtons = () => {
+    //   MessageRatingButtons.mountComponentAll();
+    // };
+
+    // function init() {
+    // Mixpanel.initialize("3c53484fb604d6e20438b4fac8d2ea56");
+    // Mixpanel.listenEvents();
+
+    // window.initMessageRatingButtons();
+    // mountComponentWithRedux(ChatApp, ChatAppReducers, [promiseMiddleware]);
+    // mountComponentWithRedux(ConversationTree, ConversationTreeReducers);
+    // mountComponent(BotResetForm);
+    // mountComponent(LearningButton);
+    // mountComponent(QuestionAnswerTagForm);
+    // mountComponent(AnswerTextArea);
+    // mountComponent(EmbedCodeGenerator);
+    // mountComponent(WordMappings);
+    // }
+
+    // // if (document.readyState === "complete") {
+    // //   init();
+    // // } else {
+    // //   document.addEventListener("DOMContentLoaded", init);
+    // // }
+    "use strict";
+
+  }, {}]
+}, {}, [1]);

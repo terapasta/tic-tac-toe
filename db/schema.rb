b0475d7d9505d91e13f5b2be72f41b9c4addc872
YukiNaito <naito.yuki127834@gmail.com>
2019-04-16 16:08:33 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190305075245) do
+ActiveRecord::Schema.define(version: 20190416065106) do
 
   create_table "accuracy_test_cases", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "question_text"

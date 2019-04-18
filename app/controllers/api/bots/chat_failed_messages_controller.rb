12e282f54a2c04f16c0cb72e2dcf2ce47b9478d6
a.harada <redhornet96@gmail.com>
2019-04-18 17:04:16 +0900

    LINE連携時のどれにも該当しない選択時の処理を実装・共通化

--- app/controllers/api/bots/chat_failed_messages_controller.rb
+++ app/controllers/api/bots/chat_failed_messages_controller.rb
@@ -1,28 +1,9 @@
 class Api::Bots::ChatFailedMessagesController < Api::BaseController
   include ApiRespondable
+  include FailedMessageHandleable
 
   def create
-    guest_key = params.require(:guest_key)
-    token = params.require(:bot_token)
-    message = params.require(:message)
-
-    bot = Bot.find_by!(token: token)
-    chat = bot.chats.find_by!(guest_key: guest_key)
-
-    null_question_answer = NullQuestionAnswer.new(bot)
-
-    guest_message = chat.messages.create(
-      speaker: :guest,
-      body: message,
-      user_agent: request.user_agent
-    )
-    bot_message = chat.messages.create(
-      speaker: :bot,
-      body: null_question_answer.answer,
-      answer_failed: true,
-      question_answer_id: null_question_answer.id,
-      guest_message_id: guest_message.id
-    )
+    _, bot_message = *create_messages_for_failed
 
     respond_to do |format|
       format.json { render json: bot_message, adapter: :json }
--- app/controllers/chats/chat_failed_messages_controller.rb
+++ app/controllers/chats/chat_failed_messages_controller.rb
@@ -1,29 +1,8 @@
 class Chats::ChatFailedMessagesController < Api::BaseController
   include ApiRespondable
+  include FailedMessageHandleable
 
   def create
-    guest_key = params.require(:guest_key)
-    token = params.require(:token)
-    message = params.require(:message)
-
-    bot = Bot.find_by!(token: token)
-    chat = bot.chats.find_by!(guest_key: guest_key)
-
-    null_question_answer = NullQuestionAnswer.new(bot)
-
-    guest_message = chat.messages.create(
-      speaker: :guest,
-      body: message,
-      user_agent: request.user_agent
-    )
-    bot_message = chat.messages.create(
-      speaker: :bot,
-      body: null_question_answer.answer,
-      answer_failed: true,
-      question_answer_id: null_question_answer.id,
-      guest_message_id: guest_message.id
-    )
-
-    render_collection_json [guest_message, bot_message], status: :created
+    render_collection_json create_messages_for_failed, status: :created
   end
 end

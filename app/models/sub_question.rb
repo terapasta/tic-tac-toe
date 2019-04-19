88d171731e0dc9f676f482cdbce3c0849e0c7a02
Yuki Ito <y.ito.works@gmail.com>
2019-04-19 19:56:44 +0900

    バリデーションを追加

--- app/models/sub_question.rb
+++ app/models/sub_question.rb
@@ -1,8 +1,15 @@
 class SubQuestion < ApplicationRecord
   belongs_to :question_answer
   validates :question, presence: true
+  validate :check_uniq_question
 
   def sub_question?
     true
+  end
+
+  def check_uniq_question
+    unless QuestionAnswer.where(["bot_id = ? and question = ?", question_answer.bot_id, question]).blank?
+      errors.add(:question, "既に登録済み")
+    end
   end
 end

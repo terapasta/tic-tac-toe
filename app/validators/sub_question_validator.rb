4ff068cac99f39fdcfb85328966ed86bf110bec7
Yuki Ito <y.ito.works@gmail.com>
2019-04-22 15:38:02 +0900

    同一botでのサブクエスチョンの重複を制限

--- app/validators/sub_question_validator.rb
+++ app/validators/sub_question_validator.rb
@@ -1,7 +1,14 @@
 class SubQuestionValidator < ActiveModel::Validator
   def validate(record)
     unless QuestionAnswer.where(["bot_id = ? and question = ?", record.question_answer.bot_id, record.question]).blank?
-      record.errors.add(:sub_question, "はすでに存在しているか、同じ質問が存在しています。")
+      record.errors.add(:sub_question, "と同じ質問が存在しています。")
+    end
+
+    if SubQuestion.joins(:question_answer)
+           .where("question_answers.bot_id = ? and sub_questions.question = ?",
+               record.question_answer.bot_id,
+               record.question).present?
+      record.errors.add(:sub_question, "と同じサブ質問が存在しています。")
     end
   end
 end

bceb48205a1d06d197a32655759883beb87beba5
Yuki Ito <y.ito.works@gmail.com>
2019-05-20 18:40:32 +0900

    保存時に前後の空白・改行を削除する処理を追加

--- app/models/sub_question.rb
+++ app/models/sub_question.rb
@@ -3,6 +3,8 @@ class SubQuestion < ApplicationRecord
   validates :question, presence: true
   validates_with SubQuestionValidator
 
+  before_save { self.question = question.strip }
+
   def sub_question?
     true
   end

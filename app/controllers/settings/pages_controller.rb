4772cc8b68b3c07d38f91a9636ac6ca4d089db36
Lynn Sakashita <lynn.sakashita@gmail.com>
2019-05-31 15:23:31 +0900

    Add helper method to enable toggling chat widget position

--- app/controllers/settings/pages_controller.rb
+++ app/controllers/settings/pages_controller.rb
@@ -1,9 +1,27 @@
 class Settings::PagesController < ApplicationController
   include BotUsable
   before_action :set_bot
+  helper_method :embed_js
 
   private
     def set_bot
       @bot = bots.find(params[:bot_id])
+    end
+
+    def embed_js(embed_js_url, token, position)
+      %Q{<script type="text/javascript">
+        (function() {
+          var head = document.querySelector("head");
+          var src = "#{embed_js_url}";
+          var isAlreadyLoaded = document.querySelector("[src='" + src + "']");
+          if (isAlreadyLoaded) { return console.warn("[My-ope office] Duplicated loading scripts"); }
+          var js = document.createElement("script");
+          js.src = src;
+          js.async = true;
+          js.id = "MyOpeEmbedScript";
+          head.appendChild(js);
+        }).call(this);
+      </script>
+      <div id="MyOpeChatWidget" data-token="#{token}" data-position="#{position}"></div>}
     end
 end

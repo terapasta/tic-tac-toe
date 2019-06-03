080998301f86df19195bfa8b361ec98fe5be8938
Yuki Ito <y.ito.works@gmail.com>
2019-06-03 11:15:39 +0900

    add ios and android icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,10 +53,10 @@ module ApplicationHelper
     # https://www.pivotaltracker.com/story/show/162403437
     # 管理者画面とチャット画面に対応
     # 市民のミカタ（OEM)対応のため、暫定的な処置としてアドレスとトークンをベタ打ち
-    if current_user&.email == "milai-work@little-cloud.jp" || bot&.token == "cbdf74a20f064e84ec1c539ccddc60808b2eba13b5dd5595527b6e017e9b4be4"
-      "市民のミカタ - 問い合わせ専用AIチャットボット"
-    else
-      "My-ope office - 社内問い合わせ専用AIチャットボット"
-    end
+
+    default_title = "My-ope office - 社内問い合わせ専用AIチャットボット"
+    is_little_cloud = current_user&.email == "milai-work@little-cloud.jp" || bot&.token == "cbdf74a20f064e84ec1c539ccddc60808b2eba13b5dd5595527b6e017e9b4be4"
+
+    is_little_cloud ? "市民のミカタ - 問い合わせ専用AIチャットボット" : default_title
   end
 end

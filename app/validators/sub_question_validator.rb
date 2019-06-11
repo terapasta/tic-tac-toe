88e3cc15cb918ea6c7ff7dd8e4addcd490b4e26a
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-06-11 15:42:31 +0900

    新規登録の際に質問とサブ質問が重複しないようにvalidate

--- app/validators/sub_question_validator.rb
+++ app/validators/sub_question_validator.rb
@@ -1,8 +1,10 @@
 class SubQuestionValidator < ActiveModel::Validator
   def validate(record)
-    unless QuestionAnswer.where(["bot_id = ? and question = ?", record.question_answer.bot_id, record.question]).blank?
+    if record.question == record.question_answer.question
       record.errors.add(:sub_question, "と同じ質問が存在しています。")
     end
+
+    puts ("record #{record}")
 
     if SubQuestion.joins(:question_answer)
            .where({question_answers: { bot_id: record.question_answer.bot_id }, sub_questions: { question: record.question } })

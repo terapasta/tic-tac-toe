9a787f0c6df84c8cf1530a172486889bd6ed52d0
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-06-12 10:41:22 +0900

    新規登録時にサブ質問が重複した場合に登録できない処理を追加

--- app/validators/sub_question_validator.rb
+++ app/validators/sub_question_validator.rb
@@ -3,8 +3,12 @@ class SubQuestionValidator < ActiveModel::Validator
     if record.question == record.question_answer.question
       record.errors.add(:sub_question, "と同じ質問が存在しています。")
     end
+    sub_questions = record.question_answer.sub_questions.map{|rec| rec.question }
 
-    puts ("record #{record}")
+    unless sub_questions.uniq.map{|e| [e, sub_questions.count(e)]}.select{|_, i| i > 1}.empty?
+      record.errors.add(:sub_question, "と同じサブ質問が存在しています。")
+    end
+
 
     if SubQuestion.joins(:question_answer)
            .where({question_answers: { bot_id: record.question_answer.bot_id }, sub_questions: { question: record.question } })

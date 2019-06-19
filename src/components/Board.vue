f7df24f1fe0cf4734e1e4505abb54b7dc922ac74
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-06-19 11:43:08 +0900

    データをProps経由で渡す

---
+++ src/components/Board.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="board-row">
+      <square value="1"></square>
+      <square value="2"></square>
+      <square value="3"></square>
+    </div>
+    <div class="board-row">
+      <square value="4"></square>
+      <square value="5"></square>
+      <square value="6"></square>
+    </div>
+    <div class="board-row">
+      <square value="7"></square>
+      <square value="8"></square>
+      <square value="9"></square>
+    </div>
+  </div>
+</template>
+
+<script>
+import Square from './Square'
+export default {
+  components: { Square },
+
+}
+</script>
+
+<style>
+.board-row:after {
+  clear: both;
+  content: "";
+  display: table;
+}
+</style>
---
+++ src/components/Game.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="game">
+    <div class="game-board">
+      <board></board>
+    </div>
+  </div>
+</template>
+
+<script>
+import Board from './Board'
+export default {
+  components: { Board },
+}
+</script>
